c3305d55f7f8007841c045b77feb1f79e703932b
Max Neuvians <max@neuvians.net>
2019-07-03 20:10:31 -0400

    No test new feature commit

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,7 @@ from app.clients.email.aws_ses import AwsSesClient
 from app.clients.sms.firetext import FiretextClient
 from app.clients.sms.loadtesting import LoadtestingClient
 from app.clients.sms.mmg import MMGClient
+from app.clients.sms.aws_sns import AwsSnsClient
 from app.clients.performance_platform.performance_platform_client import PerformancePlatformClient
 from app.encryption import Encryption
 
@@ -48,6 +49,7 @@ firetext_client = FiretextClient()
 loadtest_client = LoadtestingClient()
 mmg_client = MMGClient()
 aws_ses_client = AwsSesClient()
+aws_sns_client = AwsSnsClient()
 encryption = Encryption()
 zendesk_client = ZendeskClient()
 statsd_client = StatsdClient()
@@ -81,13 +83,14 @@ def create_app(application):
     firetext_client.init_app(application, statsd_client=statsd_client)
     loadtest_client.init_app(application, statsd_client=statsd_client)
     mmg_client.init_app(application, statsd_client=statsd_client)
+    aws_sns_client.init_app(application, statsd_client=statsd_client)
     aws_ses_client.init_app(application.config['AWS_REGION'], statsd_client=statsd_client)
     notify_celery.init_app(application)
     encryption.init_app(application)
     redis_store.init_app(application)
     performance_platform_client.init_app(application)
     document_download_client.init_app(application)
-    clients.init_app(sms_clients=[firetext_client, mmg_client, loadtest_client], email_clients=[aws_ses_client])
+    clients.init_app(sms_clients=[firetext_client, mmg_client, aws_sns_client, loadtest_client], email_clients=[aws_ses_client])
 
     register_blueprint(application)
     register_v2_blueprints(application)
---
+++ app/clients/sms/aws_sns.py
@@ -0,0 +1,38 @@
+import boto3
+import botocore
+from time import monotonic
+from app.clients.sms import SmsClient
+
+class AwsSnsClient(SmsClient):
+    '''
+    AwsSns sms client
+    '''
+
+    def init_app(self, current_app, statsd_client, *args, **kwargs):
+        self._client = boto3.client('sns', region_name=current_app.config["AWS_REGION"])
+        super(SmsClient, self).__init__(*args, **kwargs)
+        self.current_app = current_app
+        self.name = 'sns'
+        self.statsd_client = statsd_client
+
+    def get_name(self):
+        return self.name
+
+    def send_sms(self, to, content, reference, multi=True, sender=None):
+        try:
+            start_time = monotonic()
+            response = self._client.publish(
+                PhoneNumber=to,
+                Message=content
+            )
+        except botocore.exceptions.ClientError as e:
+            self.statsd_client.incr("clients.sns.error")
+        except Exception as e:
+            self.statsd_client.incr("clients.sns.error")
+        finally:
+            elapsed_time = monotonic() - start_time
+            current_app.logger.info("AWS SNS request finished in {}".format(elapsed_time))
+            self.statsd_client.timing("clients.sns.request-time", elapsed_time)
+            self.statsd_client.incr("clients.sns.success")
+
+        return response['MessageId']
