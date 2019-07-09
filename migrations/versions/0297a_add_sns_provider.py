7bb4e54618c5f7181a19d47ef173e53eb354dcd5
Max <max@neuvians.net>
2019-07-09 09:31:38 -0400

    Insert uuid

--- migrations/versions/0297a_add_sns_provider.py
+++ migrations/versions/0297a_add_sns_provider.py
@@ -7,16 +7,16 @@ Create Date: 2019-07-09 08:49:20.630174
 """
 from alembic import op
 import sqlalchemy as sa
-
+import uuid
 
 revision = '0297a_add_sns_provider'
 down_revision = '0297_template_redacted_fix'
 
 
 def upgrade():
-    op.execute("""
-        INSERT INTO provider_details (display_name, identifier, priority, notification_type, active, version) 
-        VALUES ('AWS SNS', 'sns', 0, 'sms', true, 1)
+    op.execute(f"""
+        INSERT INTO provider_details (id, display_name, identifier, priority, notification_type, active, version) 
+        VALUES ('{uuid.uuid4()}', 'AWS SNS', 'sns', 0, 'sms', true, 1)
     """)
 
 
