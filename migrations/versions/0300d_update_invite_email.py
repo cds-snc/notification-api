d463fd2e6289fbe7ed3092059c9591347775b0e1
Max <max@neuvians.net>
2019-07-30 14:55:29 -0400

    Migrations to remove default email branding

---
+++ migrations/versions/0300d_update_invite_email.py
@@ -0,0 +1,49 @@
+"""
+
+Revision ID: 0300d_update_invite_email
+Revises: 0300c_remove_email_branding
+Create Date: 2019-07-29 16:18:27.467361
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '0300d_update_invite_email'
+down_revision = '0300c_remove_email_branding'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+
+    op.execute("""
+            UPDATE
+                templates
+            SET
+                content = 
+                  REPLACE(
+                    content,
+                    'create an account on Notification',
+                    'accept the invitation'
+                  )
+            WHERE
+                id = '4f46df42-f795-4cc4-83bb-65ca312f49cc'
+        """)
+
+    op.execute("""
+            UPDATE
+                templates_history
+            SET
+                content = 
+                  REPLACE(
+                    content,
+                    'create an account on Notification',
+                    'accept the invitation'
+                  )
+            WHERE
+                id = '4f46df42-f795-4cc4-83bb-65ca312f49cc'
+        """)
+
+
+def downgrade():
+    pass
