0a74f5879688dce37f8d46ada00bf69ce3cdaebb
Max <max@neuvians.net>
2019-09-04 10:53:38 -0400

    Add merge

---
+++ migrations/versions/0305a_merge.py
@@ -0,0 +1,23 @@
+"""
+
+Revision ID: 0305a_merge
+Revises: 0305_add_gp_org_type, 0304a_merge
+Create Date: 2019-09-04 16:18:27.467361
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '0305a_merge'
+down_revision = ('0305_add_gp_org_type', '0304a_merge')
+branch_labels = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
