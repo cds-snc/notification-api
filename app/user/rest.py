abd2ab0d00986e46417cf37d98172e4de3fa4cdc
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2019-09-30 16:13:05 -0700

    missed endpoint route

--- app/user/rest.py
+++ app/user/rest.py
@@ -438,7 +438,7 @@ def send_support_email(user_id):
 
     return jsonify({}), 204
 
-
+@user_blueprint.route('/<uuid:user_id>/branding-request', methods=['POST'])
 def send_branding_request(user_id):
     to, errors = branding_request_data_schema.load(request.get_json())
     template = dao_get_template_by_id(current_app.config['CONTACT_US_TEMPLATE_ID'])
