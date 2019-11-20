b1f8abe53aacdb7df803fbe19154d32a15f8c975
Tim Arney <tim@line37.com>
2019-11-20 11:05:19 -0500

    fix imports

--- migrations/versions/0305e_account_change_type.py
+++ migrations/versions/0305e_account_change_type.py
@@ -7,8 +7,6 @@ Create Date: 2019-11-20 16:07:22.019759
 """
 from flask import current_app
 from alembic import op
-import sqlalchemy as sa
-
 
 revision = '0305e_account_change_type'
 down_revision = '0305d_block_users'
