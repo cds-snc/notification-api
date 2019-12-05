44e17411f7e5d528b4464981a7200dcf2901f509
Max <max@neuvians.net>
2019-12-05 14:52:49 -0500

    Adds sender email to contact form

--- app/user/rest.py
+++ app/user/rest.py
@@ -440,7 +440,8 @@ def send_support_email(user_id):
         service=service,
         personalisation={
             'user': to['email'],
-            'message': to['message']
+            'message': to['message'],
+            'sender_email': to["sender"]
         },
         notification_type=template.template_type,
         api_key_id=None,
