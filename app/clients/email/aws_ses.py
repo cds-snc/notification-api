29e00c474396244db700b935324e5fc059c231b4
Tim Arney <tim@line37.com>
2020-02-14 09:49:29 -0500

    fix char count

--- app/clients/email/aws_ses.py
+++ app/clients/email/aws_ses.py
@@ -73,7 +73,7 @@ class AwsSesClient(EmailClient):
                 to_addresses = [to_addresses]
 
             source = source.encode('idna').decode("utf-8")
-            source = source[0:200]
+            source = source[0:63]
 
             reply_to_addresses = [reply_to_address] if reply_to_address else []
 
