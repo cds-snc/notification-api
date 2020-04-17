480a0d48f257c9bb94327de35726571ba43af762
Max Neuvians <max@neuvians.net>
2020-04-17 10:04:46 -0400

    Add newrelic agent (#769)
    * Add newrelic agent
    
    * Fix bleach
    
    * Better bleach;

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,8 @@
-import os
-import sys
-import traceback
+import newrelic.agent  # See https://bit.ly/2xBVKBH
+newrelic.agent.initialize()
+import os  # noqa: E402
+import sys  # noqa: E402
+import traceback  # noqa: E402
 
 workers = 4
 worker_class = "eventlet"
