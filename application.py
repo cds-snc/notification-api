a9a0ac6fde1fe97031739fa658b4d27c71f3ab6f
Max Neuvians <max@neuvians.net>
2020-04-22 09:17:51 -0400

    Inject git sha into container (#794)

--- application.py
+++ application.py
@@ -16,7 +16,8 @@ load_dotenv()
 
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_URL', ''),
-    integrations=[FlaskIntegration()]
+    integrations=[FlaskIntegration()],
+    release=os.environ.get('GIT_SHA', '')
 )
 
 application = Flask('app')
