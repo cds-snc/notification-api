84a5f2527ac23a338928fa3d4f0ecc80b9a23403
Max Neuvians <max@neuvians.net>
2020-04-22 14:02:02 -0400

    Sentry release (#795)
    * Fix unused import
    
    * Fix unused import

--- application.py
+++ application.py
@@ -17,7 +17,7 @@ load_dotenv()
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_URL', ''),
     integrations=[FlaskIntegration()],
-    release=os.environ.get('GIT_SHA', '')
+    release="notify-api@" + os.environ.get('GIT_SHA', '')
 )
 
 application = Flask('app')
