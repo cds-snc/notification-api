0e95b9872c2e91f63096231a051f409f56b06e34
Max Neuvians <max@neuvians.net>
2020-04-22 09:55:20 -0400

    Fix unused import

--- application.py
+++ application.py
@@ -5,7 +5,6 @@ import os
 import sentry_sdk
 
 from flask import Flask
-from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app import create_app
@@ -17,7 +16,7 @@ load_dotenv()
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_URL', ''),
     integrations=[FlaskIntegration()],
-    release=os.environ.get('GIT_SHA', '')
+    release="notify-api@" + os.environ.get('GIT_SHA', '')
 )
 
 application = Flask('app')
