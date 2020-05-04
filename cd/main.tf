06c9d156d64070b809d4771f549d726fbb80f8a7
saman.moshafi <saman.moshafi@thoughtworks.com>
2020-05-04 16:30:35 -0400

    [T323] terraform to create ecs cluster using fargate
    Co-authored-by: Marisa Hoenig <Marisa.Hoenig@thoughtworks.com>
    Co-authored-by: Punit Lad <plad@thoughtworks.com>

--- cd/main.tf
+++ cd/main.tf
@@ -9,3 +9,14 @@ terraform {
     region = "us-east-2"
   }
 }
+resource "aws_vpc" "ecs-vpc" {
+  cidr_block = "10.0.0.0/24"
+}
+resource "aws_subnet" "ecs-subnet" {
+  vpc_id     = "aws_vpc.ecs-vpc.id"
+  cidr_block = "10.0.0.0/28"
+}
+resource "aws_ecs_cluster" "ecs-cluster" {
+  name = "notify-fargate-cluster"
+  capacity_providers = ["FARGATE"]
+}
