bfc347ba32a02a97514aba866d5b64c021498ffc
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2020-05-06 13:03:50 -0600

    reference column name?

--- migrations/versions/0306_branding_variables_rename.py
+++ migrations/versions/0306_branding_variables_rename.py
@@ -13,14 +13,14 @@ down_revision = '0305m_add_pinpoint'
 
 
 def upgrade():
-    op.execute("INSERT INTO branding_type VALUES ('custom_logo'), ('custom_logo_with_banner')")
+    op.execute("INSERT INTO branding_type (name) VALUES ('custom_logo'), ('custom_logo_with_banner')")
     op.execute("UPDATE email_branding SET brand_type = 'custom_logo' WHERE brand_type = 'org'")
     op.execute("UPDATE email_branding SET brand_type = 'custom_logo_with_banner' WHERE brand_type = 'org_banner'")
     op.execute("DELETE FROM branding_type WHERE name = 'org_banner'")
     op.execute("DELETE FROM branding_type WHERE name = 'org'")
 
 def downgrade():
-    op.execute("INSERT INTO branding_type VALUES ('org'), ('org_banner')")
+    op.execute("INSERT INTO branding_type (name) VALUES ('org'), ('org_banner')")
     op.execute("UPDATE email_branding SET brand_type = 'org' WHERE brand_type = 'custom_logo'")
     op.execute("UPDATE email_branding SET brand_type = 'org_banner' WHERE brand_type = 'custom_logo_with_banner'")
     op.execute("DELETE FROM branding_type WHERE name = 'custom_logo'")
