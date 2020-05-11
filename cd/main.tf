8fbff0d9c5914397335fa184a2c3f08d98056d56
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-05-11 16:51:51 -0400

    [#6] creates route table to replace main route table and uses with the endpoints
    Co-authored-by: Saman Moshafi <saman.moshafi@thoughtworks.com>

--- cd/main.tf
+++ cd/main.tf
@@ -134,7 +134,7 @@ resource "aws_vpc_endpoint" "vpc_endpoint_s3" {
   vpc_id            = aws_vpc.ecs-vpc.id
   service_name      = "com.amazonaws.us-east-2.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids = [aws_vpc.ecs-vpc.main_route_table_id]
+  route_table_ids = [aws_route_table.notification_route_table.id]
 }
 
 data "template_file" "notification-api" {
@@ -155,10 +155,19 @@ resource "aws_cloudwatch_log_group" "notification-log-group" {
   name = "notification-log-group"
 }
 
+resource "aws_route_table" "notification_route_table" {
+  vpc_id = aws_vpc.ecs-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.notification_internet_gateway.id
+  }
+}
+
 resource "aws_route_table_association" "vpc_route_table_association" {
   count = 2
   subnet_id      = element(aws_subnet.ecs-subnet.*.id, count.index)
-  route_table_id = aws_vpc.ecs-vpc.main_route_table_id
+  route_table_id = aws_route_table.notification_route_table.id
 }
 
 resource "aws_internet_gateway" "notification_internet_gateway" {
