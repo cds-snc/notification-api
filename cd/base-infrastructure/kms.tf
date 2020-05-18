823de4a5d0510ed45ceb54bd04f887acd4e54e64
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-05-18 11:51:34 -0400

    [#49] creates KMS key for notification
    Co-authored-by: Saman Moshafi <saman.moshafi@thoughtworks.com>
    Co-authored-by: Punit Lad <plad@thoughtworks.com>

---
+++ cd/base-infrastructure/kms.tf
@@ -0,0 +1,89 @@
+resource "aws_kms_key" "notification" {
+  description             = "Notification KMS Key"
+  deletion_window_in_days = 7
+  policy                  = data.aws_iam_policy_document.notification.json
+}
+
+data "aws_iam_policy_document" "notification" {
+  statement {
+    sid = "Allow access for Key Administrators"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = ["arn:aws:iam::437518843863:role/notification-deploy-role"]
+      type = "AWS"
+    }
+
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid = "Allow use of the key"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = ["arn:aws:iam::437518843863:role/notification-deploy-role"]
+      type = "AWS"
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid = "Allow attachment of persistent resources"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = ["arn:aws:iam::437518843863:role/notification-deploy-role"]
+      type = "AWS"
+    }
+
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant"
+    ]
+
+    resources = [
+      "*"
+    ]
+
+    condition = {
+      test = "Bool"
+      values = ["true"]
+      variable = "kms:GrantIsForAWSResource"
+    }
+  }
+}
