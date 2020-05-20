ab8dd3fafd853b8adc8b1150bf4553efade76816
Punit Lad <plad@thoughtworks.com>
2020-05-20 09:41:27 -0400

    [#49] allow role to fetch secrets
    Co-authored-by: Marisa Hoenig <marisa.hoenig@thoughtworks.com>
    Co-authored-by: Punit Lad <plad@thoughtworks.com>

--- cd/application-infrastructure/roles.tf
+++ cd/application-infrastructure/roles.tf
@@ -22,3 +22,33 @@ resource "aws_iam_role_policy_attachment" "notification_ecs_task_execution" {
   role       = aws_iam_role.notification_ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "notification_ecs_task_ssm_fetch" {
+  role = aws_iam_role.notification_ecs_task_execution.name
+  policy_arn = aws_iam_policy.notification_ecs_task_secrets_fetch.arn
+}
+
+resource "aws_iam_policy" "notification_ecs_task_secrets_fetch" {
+  name = "notification-ecs-task-secrets-fetch"
+  policy = data.aws_iam_policy_document.ssm_parameter_fetch.json
+}
+
+data "aws_iam_policy_document" "ssm_parameter_fetch" {
+  statement {
+    sid = "FetchNotificationSecrets"
+
+    actions = [
+      "ssm:GetParametersByPath",
+      "ssm:GetParameters",
+      "ssm:GetParameter",
+      "ssm:DescribeParameters"
+    ]
+
+    resources = [
+      data.aws_ssm_parameter.database_uri.arn,
+      data.aws_ssm_parameter.twilio_account_sid.arn,
+      data.aws_ssm_parameter.twilio_auth_token.arn,
+      data.aws_ssm_parameter.twilio_from_number.arn
+    ]
+  }
+}
