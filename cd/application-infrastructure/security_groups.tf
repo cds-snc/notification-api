ffba2f2c2f5f8070999040ce57a1a378d2224438
Punit Lad <plad@thoughtworks.com>
2020-05-19 22:02:03 -0400

    [#50] moving notification-db-access back to database state
    Co-authored-by: Punit Lad <plad@thoughtworks.com>

--- cd/application-infrastructure/security_groups.tf
+++ cd/application-infrastructure/security_groups.tf
@@ -18,17 +18,3 @@ resource "aws_security_group" "ecs_task_outbound_access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group" "notification_db_access" {
-  name_prefix = "notification-db-access-"
-  description = "For access to the Notification Database"
-  vpc_id      = data.aws_vpc.notification.id
-}
-resource "aws_security_group_rule" "allow_db_ingress" {
-  type                     = "ingress"
-  from_port                = data.terraform_remote_state.application_db.outputs.database_port
-  to_port                  = data.terraform_remote_state.application_db.outputs.database_port
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.notification_db_access.id
-  security_group_id        = data.terraform_remote_state.application_db.outputs.database_security_group
-}
