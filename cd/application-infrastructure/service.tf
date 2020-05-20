714330ed7f17b8cc3e14775d8efef76270974ff2
Punit Lad <plad@thoughtworks.com>
2020-05-19 22:59:57 -0400

    [#50] allow service access to db
    Co-authored-by: Punit Lad <plad@thoughtworks.com>

--- cd/application-infrastructure/service.tf
+++ cd/application-infrastructure/service.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_service" "notification_api" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    security_groups  = [aws_security_group.ecs_task_outbound_access.id]
+    security_groups  = [aws_security_group.ecs_task_outbound_access.id, data.terraform_remote_state.application_db.outputs.notification_db_access_security_group_id]
     subnets          = [data.aws_subnet.private_az_a.id, data.aws_subnet.private_az_b.id]
     assign_public_ip = false
   }
