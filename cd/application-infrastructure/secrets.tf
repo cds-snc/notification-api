7d353c439f011bbaecf061cede4508aa8ba778c0
saman.moshafi <saman.moshafi@thoughtworks.com>
2020-06-04 16:01:03 -0400

    [#63] create SSM parameters for admin client secret, secret key and dangerous salt
    Co-authored-by: Phil Herbert <pherbert@thoughtworks.com>

---
+++ cd/application-infrastructure/secrets.tf
@@ -0,0 +1,35 @@
+resource "random_password" "admin_client_secret" {
+  length = 16
+}
+
+resource "aws_ssm_parameter" "admin_client_secret" {
+  name        = "/dev/notification-api/admin-client-secret"
+  description = "The notification api URL"
+  type        = "SecureString"
+  value       = random_password.admin_client_secret.result
+  tags        = var.default_tags
+}
+
+resource "random_password" "secret_key" {
+  length = 16
+}
+
+resource "aws_ssm_parameter" "secret_key" {
+  name        = "/dev/notification-api/secret-key"
+  description = "The notification api URL"
+  type        = "SecureString"
+  value       = random_password.secret_key.result
+  tags        = var.default_tags
+}
+
+resource "random_password" "dangerous_salt" {
+  length = 16
+}
+
+resource "aws_ssm_parameter" "dangerous_salt" {
+  name        = "/dev/notification-api/dangerous-salt"
+  description = "The notification api URL"
+  type        = "SecureString"
+  value       = random_password.dangerous_salt.result
+  tags        = var.default_tags
+}
