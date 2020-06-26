82927305ab923f5ffd5d08233283d09da321d2cf
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-06-26 14:37:05 -0400

    [#51] removes aws_vpc data from database variables
    Co-authored-by: Saman Moshafi <saman.moshafi@thoughtworks.com>
    Co-authored-by: Shefali Nayak <shefali.nayak@thoughtworks.com>

--- cd/application-database/variables.tf
+++ cd/application-database/variables.tf
@@ -7,10 +7,6 @@ variable "database_name" {
   default = "notification_api"
 }
 
-data "aws_vpc" "notification" {
-  cidr_block = "10.0.0.0/24"
-}
-
 data "aws_subnet" "private_az_a" {
   cidr_block = "10.0.0.64/26"
 }
