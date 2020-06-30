c6723bced02868ec7705eeb2d7ca4e9ae12ad2c6
Phil Herbert <phil.a.herbert@gmail.com>
2020-06-30 11:37:13 -0400

    [#101] creates s3 bucket and object for va logo
    Co-authored-by: Filip Fafara <ffafara@thoughtworks.com>

---
+++ cd/application-infrastructure/s3.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "assets" {
+  bucket  = var.environment_prefix == "prod" ? "notifications-va-gov-assets" : "${var.environment_prefix}-notifications-va-gov-assets"
+  acl = "public-read"
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = local.default_tags
+}
+
+resource "aws_s3_bucket_object" "va_logo" {
+  bucket = aws_s3_bucket.assets.id
+  key    = "header-logo.png"
+  source = "assets/header-logo.png"
+
+  tags = local.default_tags
+}
