b0a5c7b88735d47cb35fd4727bad241dd23ec525
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-07-10 16:33:43 -0400

    [#79] tags docker image at the end of staging CI

--- scripts/push_git_tag.sh
+++ scripts/push_git_tag.sh
@@ -2,9 +2,13 @@
 set -e
 
 create_git_tag () {
-  version=`echo $1 | cut -f2 -d'-'`
+  removed_refs=`echo $1 | cut -f3 -d'/'`
+  version=`echo $removed_refs | cut -f2 -d'-'`
   git tag $version
   git push origin $version
 }
 
 create_git_tag "$1"
+
+echo "::set-env name=STAGING_TAG::$removed_refs"
+echo "::set-env name=TAG::$version"
