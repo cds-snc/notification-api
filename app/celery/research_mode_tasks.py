c4b057567152b203a1a5bb90f6d04d73b26d87d1
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2020-07-14 12:44:40 -0600

    Revert "try disabling ses callback in test sends (#1028)"
    This reverts commit b599479be94cd80fa2449cb22f836df1c23f6182.

--- app/celery/research_mode_tasks.py
+++ app/celery/research_mode_tasks.py
@@ -10,8 +10,8 @@ from notifications_utils.s3 import s3upload
 from app import notify_celery
 from app.aws.s3 import file_exists
 from app.models import SMS_TYPE
-# from app.config import QueueNames
-# from app.celery.process_ses_receipts_tasks import process_ses_results
+from app.config import QueueNames
+from app.celery.process_ses_receipts_tasks import process_ses_results
 
 temp_fail = "7700900003"
 perm_fail = "7700900002"
@@ -44,9 +44,6 @@ def send_sms_response(provider, reference, to):
 
 
 def send_email_response(reference, to):
-    return
-
-    """
     if to == perm_fail_email:
         body = ses_hard_bounce_callback(reference)
     elif to == temp_fail_email:
@@ -55,7 +52,6 @@ def send_email_response(reference, to):
         body = ses_notification_callback(reference)
 
     process_ses_results.apply_async([body], queue=QueueNames.RESEARCH_MODE)
-    """
 
 
 def make_request(notification_type, provider, data, headers):
