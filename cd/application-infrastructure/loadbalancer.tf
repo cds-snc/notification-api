576c823a1db02c2caf1d76f45c18a5abdb38e978
Phil Herbert <phil.a.herbert@gmail.com>
2020-07-16 16:55:52 -0400

    [#94] adds ingress on port 443 for https access to load balancer

--- cd/application-infrastructure/loadbalancer.tf
+++ cd/application-infrastructure/loadbalancer.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "notification_api" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
