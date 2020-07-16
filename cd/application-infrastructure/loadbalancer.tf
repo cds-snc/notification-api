98eace9d582d40ae4d03d24c9fa4f7dd5d49729d
Phil Herbert <phil.a.herbert@gmail.com>
2020-07-16 15:43:18 -0400

    [#94] creates route53 record, certificate, and https listener

--- cd/application-infrastructure/loadbalancer.tf
+++ cd/application-infrastructure/loadbalancer.tf
@@ -16,6 +16,18 @@ resource "aws_alb_listener" "notification_api" {
   }
 }
 
+resource "aws_alb_listener" "notification_api_https" {
+  load_balancer_arn = aws_alb.notification_api.id
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn = aws_acm_certificate.cert.arn
+
+  default_action {
+    target_group_arn = aws_alb_target_group.notification_api.id
+    type             = "forward"
+  }
+}
+
 resource "aws_ssm_parameter" "api_host_name" {
   name        = "/${var.environment_prefix}/notification-api/api-host-name"
   description = "The notification api URL"
