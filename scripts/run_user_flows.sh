75271d93b8e597d815c542b1da9693d6068af079
Sarah Hutchins <shutchin@thoughtworks.com>
2020-07-21 13:59:12 -0500

    Adding user flows

---
+++ scripts/run_user_flows.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Run project tests
+#
+# NOTE: This script expects to be run from the project root with
+# ./scripts/run_tests.sh
+
+set -o pipefail
+
+function display_result {
+  RESULT=$1
+  EXIT_STATUS=$2
+  TEST=$3
+
+  if [ $RESULT -ne 0 ]; then
+    echo -e "\033[31m$TEST failed\033[0m"
+    exit $EXIT_STATUS
+  else
+    echo -e "\033[32m$TEST passed\033[0m"
+  fi
+}
+
+# run with four concurrent threads
+py.test --disable-pytest-warnings -s user_flows/ --junitxml=user_flow_results.xml
+display_result $? 2 "User Flows"
