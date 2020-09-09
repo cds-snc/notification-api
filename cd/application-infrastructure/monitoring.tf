f4d0e9b94f9793c2e6cce6c69432ac6c93ee6475
Phil Herbert <phil.a.herbert@gmail.com>
2020-09-09 17:48:09 -0400

    [#122] adds log group for datadog containers
    Co-authored-by: Filip Fafara <ffafara@thoughtworks.com>

--- cd/application-infrastructure/monitoring.tf
+++ cd/application-infrastructure/monitoring.tf
@@ -3,3 +3,9 @@ resource "aws_cloudwatch_log_group" "notification" {
   retention_in_days = var.log_retention_in_days
   tags              = local.default_tags
 }
+
+resource "aws_cloudwatch_log_group" "datadog" {
+  name              = "${var.environment_prefix}-notification-api-datadog-log-group"
+  retention_in_days = var.log_retention_in_days
+  tags              = local.default_tags
+}
