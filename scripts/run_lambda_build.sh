cb4a886983acf028611299511af7d47e743af41e
Ling Tran <lxiutran@gmail.com>
2020-10-07 14:37:20 -0400

    [#228] Initial commit for Lambda Functions pipeline workflow

---
+++ scripts/run_lambda_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Test script for initial lambda workflow job
+#
+# NOTE: This script expects to be run from the project root with
+# ./scripts/run_tests.sh
+
+set -o pipefail
+
+echo -e "Hello world!"
