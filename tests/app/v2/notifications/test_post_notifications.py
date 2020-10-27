545f61a1d4cc1b604ed556f7a53dda76b9c30806
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-10-27 15:41:44 -0400

    [#190] test post_notifications returns 501 when feature flag disabled and recipient identifiers present

--- tests/app/v2/notifications/test_post_notifications.py
+++ tests/app/v2/notifications/test_post_notifications.py
@@ -11,7 +11,8 @@ from app.models import (
     SCHEDULE_NOTIFICATIONS,
     SMS_TYPE,
     UPLOAD_DOCUMENT,
-    INTERNATIONAL_SMS_TYPE)
+    INTERNATIONAL_SMS_TYPE,
+    VA_PROFILE_ID)
 from flask import json, current_app
 
 from app.models import Notification
@@ -870,3 +871,24 @@ def test_post_notification_returns_400_when_get_json_throws_exception(client, sa
         data="[",
         headers=[('Content-Type', 'application/json'), auth_header])
     assert response.status_code == 400
+
+
+def test_post_notification_returns_501_when_recipient_identifiers_present_and_feature_flag_disabled(
+        client,
+        mocker,
+        sample_email_template
+):
+    mocker.patch(
+        'app.v2.notifications.post_notifications.accept_recipient_identifiers_enabled',
+        return_value=False
+    )
+    data = {
+        "template_id": sample_email_template.id,
+        "recipient_identifier": {'id_type': VA_PROFILE_ID, 'id_value': "foo"}
+    }
+    auth_header = create_authorization_header(service_id=sample_email_template.service_id)
+    response = client.post(
+        path="v2/notifications/email",
+        data=json.dumps(data),
+        headers=[('Content-Type', 'application/json'), auth_header])
+    assert response.status_code == 501
