1bc9a8be76d1297728ba566200f82b819f209aa8
Ling Tran <ltran@thoughtworks.com>
2020-11-03 15:16:02 -0500

    [#228] Refactor user flows lambda pytest.main argument syntax

--- lambda_functions/user_flows/user_flows_lambda.py
+++ lambda_functions/user_flows/user_flows_lambda.py
@@ -1,10 +1,13 @@
+import os
 import pytest
 
 
 def user_flows_handler(event, context):
     environment = event['environment']
 
-    return pytest.main(
-        "-p no:cacheprovider -s --verbose --environment {env} ./test_retrieve_everything.py"
-        .format(env=environment)
-    )
+    return pytest.main([
+        "-p", "no:cacheprovider",
+        "-s", "--verbose",
+        "--environment", environment,
+        "./test_retrieve_everything.py"
+    ])
