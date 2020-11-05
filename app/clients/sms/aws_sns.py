a11b4d5cbf2867dd309c7b4d31effb24c50edde7
Filip Fafara <ffafara@thoughtworks.com>
2020-11-05 01:48:10 -0500

    [#236] Remove phone number formatting from SNS client as it is already formatted when passed to the client.

--- app/clients/sms/aws_sns.py
+++ app/clients/sms/aws_sns.py
@@ -1,12 +1,12 @@
 import boto3
 import botocore
-import phonenumbers
 from time import monotonic
 from app.clients.sms import SmsClient, SmsClientResponseException
 
 
 class SnsClientException(SmsClientResponseException):
     pass
+
 
 class AwsSnsClient(SmsClient):
     '''
@@ -24,35 +24,24 @@ class AwsSnsClient(SmsClient):
         return self.name
 
     def send_sms(self, to, content, reference, multi=True, sender=None):
-        matched = False
-
-        for match in phonenumbers.PhoneNumberMatcher(to, "US"):
-            matched = True
-            to = phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164)
-
-            try:
-                start_time = monotonic()
-                response = self._client.publish(
-                    PhoneNumber=to,
-                    Message=content,
-                    MessageAttributes={
-                        'AWS.SNS.SMS.SMSType': {
-                            'DataType': 'String',
-                            'StringValue': 'Transactional'
-                        }
+        try:
+            start_time = monotonic()
+            response = self._client.publish(
+                PhoneNumber=to,
+                Message=content,
+                MessageAttributes={
+                    'AWS.SNS.SMS.SMSType': {
+                        'DataType': 'String',
+                        'StringValue': 'Transactional'
                     }
-                )
-            except (botocore.exceptions.ClientError, Exception) as e:
-                self.statsd_client.incr("clients.sns.error")
-                raise SnsClientException(str(e))
-            else:
-                elapsed_time = monotonic() - start_time
-                self.logger.info(f"AWS SNS request finished in {elapsed_time}")
-                self.statsd_client.timing("clients.sns.request-time", elapsed_time)
-                self.statsd_client.incr("clients.sns.success")
-                return response['MessageId']
-
-        if not matched:
+                }
+            )
+        except (botocore.exceptions.ClientError, Exception) as e:
             self.statsd_client.incr("clients.sns.error")
-            self.logger.error(f"No valid numbers found in {to}")
-            raise ValueError("No valid numbers found for SMS delivery")
+            raise SnsClientException(str(e))
+        else:
+            elapsed_time = monotonic() - start_time
+            self.logger.info(f"AWS SNS request finished in {elapsed_time}")
+            self.statsd_client.timing("clients.sns.request-time", elapsed_time)
+            self.statsd_client.incr("clients.sns.success")
+            return response['MessageId']
--- tests/app/clients/test_aws_sns.py
+++ tests/app/clients/test_aws_sns.py
@@ -1,16 +1,16 @@
-import pytest
 from app import aws_sns_client
 
 
 def test_send_sms_successful_returns_aws_sns_response(notify_api, mocker):
     boto_mock = mocker.patch.object(aws_sns_client, '_client', create=True)
     mocker.patch.object(aws_sns_client, 'statsd_client', create=True)
+    boto_mock.publish.return_value = {'MessageId': 'some-identifier'}
 
-    to = "6135555555"
+    to = "+16135555555"
     content = reference = 'foo'
 
     with notify_api.app_context():
-        aws_sns_client.send_sms(to, content, reference)
+        response = aws_sns_client.send_sms(to, content, reference)
 
     boto_mock.publish.assert_called_once_with(
         PhoneNumber="+16135555555",
@@ -18,15 +18,4 @@ def test_send_sms_successful_returns_aws_sns_response(notify_api, mocker):
         MessageAttributes={'AWS.SNS.SMS.SMSType': {'DataType': 'String', 'StringValue': 'Transactional'}}
     )
 
-
-def test_send_sms_returns_raises_error_if_there_is_no_valid_number_is_found(notify_api, mocker):
-    mocker.patch.object(aws_sns_client, '_client', create=True)
-    mocker.patch.object(aws_sns_client, 'statsd_client', create=True)
-
-    to = ""
-    content = reference = 'foo'
-
-    with pytest.raises(ValueError) as excinfo:
-        aws_sns_client.send_sms(to, content, reference)
-
-    assert 'No valid numbers found for SMS delivery' in str(excinfo.value)
+    assert response == 'some-identifier'
