6e8793d5666a76296f2b80567ce2317056d5300a
Ling Tran <ltran@thoughtworks.com>
2020-11-10 14:57:10 -0500

    [#228] Add back in notification status assertions in user flows tests lambda

--- lambda_functions/user_flows/test_retrieve_everything.py
+++ lambda_functions/user_flows/test_retrieve_everything.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from steps import get_notification_url
 from steps import get_api_health_status
 from steps import get_authenticated_request
@@ -8,6 +9,8 @@ from steps import get_user_id
 from steps import get_new_service_api_key
 from steps import get_service_jwt
 from steps import send_email
+from steps import get_notification_id
+from steps import get_notification_status
 
 
 @pytest.fixture(scope="function")
@@ -78,12 +81,14 @@ def test_send_email(environment, notification_url, service_id, template_id, user
     service_jwt = get_service_jwt(service_key, service_id)
     email_response = send_email(notification_url, service_jwt, template_id)
     assert email_response.status_code == 201
-    # notification_id = get_notification_id(email_response)
-    # time_count = 0
-    # notification_status = ""
-    # while notification_status != "sending" and time_count < 30:
-    #     notification_status_response = get_notification_status(notification_id, notification_url, service_jwt)
-    #     assert notification_status_response.status_code == 200
-    #     notification_status = notification_status_response.json()['status']
-    #     time.sleep(1)
-    # assert notification_status == 'sending'
+    notification_id = get_notification_id(email_response)
+    time_count = 0
+    notification_status = ""
+    while notification_status != "sending" and time_count < 30:
+        service_jwt = get_service_jwt(service_key, service_id)
+        notification_status_response = get_notification_status(notification_id, notification_url, service_jwt)
+        assert notification_status_response.status_code == 200
+        notification_status = notification_status_response.json()['status']
+        time.sleep(1)
+        time_count = time_count + 1
+    assert notification_status == 'sending'
