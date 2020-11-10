ca46d2412c7ad108764c6ef368375c2c07beaf82
Filip Fafara <ffafara@thoughtworks.com>
2020-11-10 00:56:44 -0500

    [#236] Refactor aws sns client tests to not depend on app context

--- tests/app/clients/test_aws_sns.py
+++ tests/app/clients/test_aws_sns.py
@@ -1,16 +1,30 @@
-from app import aws_sns_client
+import pytest
+from app.clients.sms.aws_sns import AwsSnsClient
 
 
-def test_send_sms_successful_returns_aws_sns_response(notify_api, mocker):
+@pytest.fixture(scope='function')
+def aws_sns_client(notify_api, mocker):
+    with notify_api.app_context():
+        aws_sns_client = AwsSnsClient()
+        statsd_client = mocker.Mock()
+        logger = mocker.Mock()
+        aws_sns_client.init_app("some-aws-region", statsd_client, logger)
+        return aws_sns_client
+
+
+@pytest.fixture(scope='function')
+def boto_mock(aws_sns_client, mocker):
     boto_mock = mocker.patch.object(aws_sns_client, '_client', create=True)
-    mocker.patch.object(aws_sns_client, 'statsd_client', create=True)
+    return boto_mock
+
+
+def test_send_sms_successful_returns_aws_sns_response(aws_sns_client, boto_mock):
     boto_mock.publish.return_value = {'MessageId': 'some-identifier'}
 
     to = "+16135555555"
     content = reference = 'foo'
 
-    with notify_api.app_context():
-        response = aws_sns_client.send_sms(to, content, reference)
+    response = aws_sns_client.send_sms(to, content, reference)
 
     boto_mock.publish.assert_called_once_with(
         PhoneNumber="+16135555555",
