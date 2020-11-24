024406635cd41fd5bd8dab5198e089b9063dd241
Filip Fafara <ffafara@thoughtworks.com>
2020-11-24 18:04:29 -0500

    [#192] Add cert and key path variables to task definition as they cannot be exported from inside the script. Make sure that script presevers certifiate line breaks when saving to file
    Co-authored-by: Filip Fafara <ffafara@thoughtworks.com>
    Co-authored-by: Phil Herbert <pherbert@thoughtworks.com>
    Co-authored-by: Saman Moshafi <saman.moshafi@thoughtworks.com>

--- scripts/save_certificate.sh
+++ scripts/save_certificate.sh
@@ -2,11 +2,7 @@
 set -e
 
 echo "Writing SSL certificate and key to files"
-
-export VANOTIFY_SSL_CERT_PATH="/app/certs/vanotify_ssl.cert"
-export VANOTIFY_SSL_KEY_PATH="/app/certs/vanotify_ssl.key"
-
-echo $VANOTIFY_SSL_CERT > $VANOTIFY_SSL_CERT_PATH
-echo $VANOTIFY_SSL_KEY > $VANOTIFY_SSL_KEY_PATH
+echo "$VANOTIFY_SSL_CERT" > $VANOTIFY_SSL_CERT_PATH
+echo "$VANOTIFY_SSL_KEY" > $VANOTIFY_SSL_KEY_PATH
 
 exec "$@"
