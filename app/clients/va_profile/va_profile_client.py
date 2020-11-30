8c9b18e3cbc0e8e1bd17f3d55af6a9a73bb3501a
Phil Herbert <phil.a.herbert@gmail.com>
2020-11-30 10:44:07 -0500

    [#192] fixes lint issues

--- app/clients/va_profile/va_profile_client.py
+++ app/clients/va_profile/va_profile_client.py
@@ -21,15 +21,13 @@ class VAProfileClient:
         )
         return self._parse_response(response)
 
-
     def _parse_response(self, response):
         if response.status_code == 200:
             response_dict = response.json()
             if response_dict['status'] == 'COMPLETED_SUCCESS':
-                email_address_text = self._fetch_email_from_bios(response_dict) # [0]['emailAddressText']
+                email_address_text = self._fetch_email_from_bios(response_dict)
                 current_app.logger.info(f"Did VAProfile send email address? {email_address_text is not None}")
                 return email_address_text
-
 
     def _fetch_email_from_bios(self, response_dict):
         bios = response_dict.get('bios')
