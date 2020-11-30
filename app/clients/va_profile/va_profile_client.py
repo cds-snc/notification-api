86dd480033337965c1a8181f9477d631e59d1557
Phil Herbert <phil.a.herbert@gmail.com>
2020-11-30 11:24:17 -0500

    [#192] refactors va profile client to move error throwing around, and uses try-except to parse email

--- app/clients/va_profile/va_profile_client.py
+++ app/clients/va_profile/va_profile_client.py
@@ -25,24 +25,24 @@ class VAProfileClient:
             f"{self.va_profile_url}/contact-information-hub/cuf/contact-information/v1/{va_profile_id}/emails",
             cert=(self.ssl_cert_path, self.ssl_key_path)
         )
-        return self._parse_response(response)
 
-    def _parse_response(self, response):
         if response.status_code != 200:
             raise VAProfileClientException(f"VA Profile responded with HTTP {response.status_code}")
 
-        response_dict = response.json()
+        return self._parse_response(response)
 
-        if response_dict['status'] != 'COMPLETED_SUCCESS':
-            raise VAProfileClientException(f"VA Profile responded with status {response_dict['status']}")
+    def _parse_response(self, response):
+        response_json = response.json()
 
-        email_address_text = self._fetch_email_from_bios(response_dict)
+        if response_json['status'] != 'COMPLETED_SUCCESS':
+            raise VAProfileClientException(f"VA Profile responded with status {response_json['status']}")
+
+        email_address_text = self._fetch_email_from_bios(response_json)
         self.logger.info(f"Did VAProfile send email address? {email_address_text is not None}")
         return email_address_text
 
-    def _fetch_email_from_bios(self, response_dict):
-        bios = response_dict.get('bios')
-        if bios and len(bios) > 0:
-            return bios[0]['emailAddressText']
-        else:
-            raise VAProfileClientException("No email in VA Profile response")
+    def _fetch_email_from_bios(self, response_json):
+        try:
+            return response_json['bios'][0]['emailAddressText']
+        except KeyError as e:
+            raise VAProfileClientException("No email in VA Profile response") from e
