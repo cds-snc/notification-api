202e221d615c058ae9d47f9cacb4d1ea525d9f82
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-12-07 10:03:47 +0100

    Update task name

--- tests/app/v2/notifications/test_post_notifications.py
+++ tests/app/v2/notifications/test_post_notifications.py
@@ -101,7 +101,7 @@ def test_post_sms_notification_uses_inbound_number_reply_to_as_sender(client, no
     service = create_service_with_inbound_number(inbound_number='6502532222')
 
     template = create_template(service=service, content="Hello (( Name))\nYour thing is due soon")
-    mocked = mocker.patch('app.celery.provider_tasks.deliver_sms.apply_async')
+    mocked = mocker.patch('app.celery.provider_tasks.deliver_throttled_sms.apply_async')
     data = {
         'phone_number': '+16502532222',
         'template_id': str(template.id),
@@ -156,7 +156,7 @@ def test_post_sms_notification_uses_sms_sender_id_reply_to(
         client, sample_template_with_placeholders, mocker
 ):
     sms_sender = create_service_sms_sender(service=sample_template_with_placeholders.service, sms_sender='6502532222')
-    mocked = mocker.patch('app.celery.provider_tasks.deliver_sms.apply_async')
+    mocked = mocker.patch('app.celery.provider_tasks.deliver_throttled_sms.apply_async')
     data = {
         'phone_number': '+16502532222',
         'template_id': str(sample_template_with_placeholders.id),
