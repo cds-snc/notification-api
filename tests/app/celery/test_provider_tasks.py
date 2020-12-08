96026f7906b62486bd5d0e9df632d8cfec8c16de
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-12-08 11:12:03 +0100

    Test both SMS methods

--- tests/app/celery/test_provider_tasks.py
+++ tests/app/celery/test_provider_tasks.py
@@ -10,50 +10,69 @@ from app.clients.email.aws_ses import AwsSesClientException
 from app.exceptions import NotificationTechnicalFailureException
 
 
+sms_methods = [
+    (deliver_sms, 'deliver_sms'),
+    (deliver_throttled_sms, 'deliver_throttled_sms'),
+]
+
+
 def test_should_have_decorated_tasks_functions():
     assert deliver_sms.__wrapped__.__name__ == 'deliver_sms'
     assert deliver_throttled_sms.__wrapped__.__name__ == 'deliver_throttled_sms'
     assert deliver_email.__wrapped__.__name__ == 'deliver_email'
 
 
+@pytest.mark.parametrize("sms_method,sms_method_name", sms_methods)
 def test_should_call_send_sms_to_provider_from_deliver_sms_task(
-        sample_notification,
-        mocker):
+    sample_notification,
+    mocker,
+    sms_method,
+    sms_method_name,
+):
     mocker.patch('app.delivery.send_to_providers.send_sms_to_provider')
 
-    deliver_sms(sample_notification.id)
+    sms_method(sample_notification.id)
     app.delivery.send_to_providers.send_sms_to_provider.assert_called_with(sample_notification)
 
 
+@pytest.mark.parametrize("sms_method,sms_method_name", sms_methods)
 def test_sms_tasks_should_call_same_method(
     sample_notification,
     mocker,
+    sms_method,
+    sms_method_name,
 ):
     private_task = mocker.patch('app.celery.provider_tasks._deliver_sms')
 
-    deliver_sms(sample_notification.id)
-    assert private_task.call_count == 1
-
-    deliver_throttled_sms(sample_notification.id)
-    assert private_task.call_count == 2
+    sms_method(sample_notification.id)
+    private_task.assert_called_once()
 
 
+@pytest.mark.parametrize("sms_method,sms_method_name", sms_methods)
 def test_should_add_to_retry_queue_if_notification_not_found_in_deliver_sms_task(
-        notify_db_session,
-        mocker):
+    notify_db_session,
+    mocker,
+    sms_method,
+    sms_method_name,
+):
     mocker.patch('app.delivery.send_to_providers.send_sms_to_provider')
-    mocker.patch('app.celery.provider_tasks.deliver_sms.retry')
+    mocker.patch(f'app.celery.provider_tasks.{sms_method_name}.retry')
 
     notification_id = app.create_uuid()
 
-    deliver_sms(notification_id)
+    sms_method(notification_id)
     app.delivery.send_to_providers.send_sms_to_provider.assert_not_called()
-    app.celery.provider_tasks.deliver_sms.retry.assert_called_with(queue="retry-tasks", countdown=0)
+
+    getattr(app.celery.provider_tasks, sms_method_name).retry.assert_called_with(
+        queue="retry-tasks",
+        countdown=0
+    )
 
 
 def test_should_call_send_email_to_provider_from_deliver_email_task(
-        sample_notification,
-        mocker):
+    sample_notification,
+    mocker,
+):
     mocker.patch('app.delivery.send_to_providers.send_email_to_provider')
 
     deliver_email(sample_notification.id)
@@ -73,15 +92,21 @@ def test_should_add_to_retry_queue_if_notification_not_found_in_deliver_email_ta
 
 # DO THESE FOR THE 4 TYPES OF TASK
 
-def test_should_go_into_technical_error_if_exceeds_retries_on_deliver_sms_task(sample_notification, mocker):
+@pytest.mark.parametrize("sms_method,sms_method_name", sms_methods)
+def test_should_go_into_technical_error_if_exceeds_retries_on_deliver_sms_task(
+    sample_notification,
+    mocker,
+    sms_method,
+    sms_method_name,
+):
     mocker.patch('app.delivery.send_to_providers.send_sms_to_provider', side_effect=Exception("EXPECTED"))
-    mocker.patch('app.celery.provider_tasks.deliver_sms.retry', side_effect=MaxRetriesExceededError())
+    mocker.patch(f'app.celery.provider_tasks.{sms_method_name}.retry', side_effect=MaxRetriesExceededError())
 
     with pytest.raises(NotificationTechnicalFailureException) as e:
-        deliver_sms(sample_notification.id)
+        sms_method(sample_notification.id)
     assert str(sample_notification.id) in str(e.value)
 
-    provider_tasks.deliver_sms.retry.assert_called_with(queue="retry-tasks", countdown=0)
+    getattr(provider_tasks, sms_method_name).retry.assert_called_with(queue="retry-tasks", countdown=0)
 
     assert sample_notification.status == 'technical-failure'
 
@@ -126,17 +151,20 @@ def test_should_retry_and_log_exception(sample_notification, mocker):
     assert sample_notification.status == 'created'
 
 
+@pytest.mark.parametrize("sms_method,sms_method_name", sms_methods)
 def test_send_sms_should_not_switch_providers_on_non_provider_failure(
     sample_notification,
-    mocker
+    mocker,
+    sms_method,
+    sms_method_name,
 ):
     mocker.patch(
         'app.delivery.send_to_providers.send_sms_to_provider',
         side_effect=Exception("Non Provider Exception")
     )
     switch_provider_mock = mocker.patch('app.delivery.send_to_providers.dao_toggle_sms_provider')
-    mocker.patch('app.celery.provider_tasks.deliver_sms.retry')
+    mocker.patch(f'app.celery.provider_tasks.{sms_method_name}.retry')
 
-    deliver_sms(sample_notification.id)
+    sms_method(sample_notification.id)
 
     assert switch_provider_mock.called is False
