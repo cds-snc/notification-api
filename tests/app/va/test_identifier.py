b90ee701c9609cfaff901972c3318e85e5fbb458
Phil Herbert <pherbert@thoughtworks.com>
2020-12-23 11:14:19 -0500

    [#282] adds more test cases to is_fhir_format method
    examples taken from this comment: https://github.com/department-of-veterans-affairs/notification-api/issues/200#issuecomment-724203027

--- tests/app/va/test_identifier.py
+++ tests/app/va/test_identifier.py
@@ -6,8 +6,12 @@ from app.va import is_fhir_format
 @pytest.mark.parametrize(
     "identifier_value, expected_is_fhir_format",
     [
-        ("123", False),
-        ("12323423^BRLS^200^USDVA", True)
+        ("12323423^BRLS^200^USDVA", True),
+        ("12323423", False),
+        ("123456^PI^200CORP^USVBA", True),
+        ("12345^PI^200CRNR^USVHA^A", True),
+        ("1008533405V377263^NI^200M^USVHA", True),
+        ("1008533405V377263", False),
     ]
 )
 def test_is_fhir_format(identifier_value, expected_is_fhir_format):
