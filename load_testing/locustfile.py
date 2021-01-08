48947c2225dda2aef1c23b60174c8b33a302cf26
Phil Herbert <pherbert@thoughtworks.com>
2021-01-08 16:40:54 -0500

    [#287] read load testing config from SSM if it's not in environment variables

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -1,36 +1,56 @@
 import time
 import os
 import jwt
+import boto3
 from locust import HttpUser, task
 
 
 class SendEmail(HttpUser):
+
+    def on_start(self):
+        print(os.getenv("LOAD_TESTING_template_id"))
+        self.template_id = self.read_configuration('template_id')
+        self.service_id = self.read_configuration('service_id')
+        self.api_key = self.read_configuration('api_key')
+
+    def read_configuration(self, key: str) -> str:
+        if f"LOAD_TESTING_{key}" in os.environ:
+            return os.getenv(f"LOAD_TESTING_{key}")
+        else:
+            return self.read_from_ssm(key)
+
+    def read_from_ssm(self, key: str) -> str:
+        if not hasattr(self, 'boto_client'):
+            self.boto_client = boto3.client('ssm')
+
+        resp = self.boto_client.get_parameter(
+            Name=f"/utility/locust/{key}",
+            WithDecryption=True
+        )
+        return resp["Parameter"]["Value"]
 
     @task
     def send_email(self):
         headers = {
             'Authorization': f"Bearer {self._get_jwt().decode('utf-8')}"
         }
-        template_id = os.getenv('LOAD_TESTING_TEMPLATE_ID')
         payload = {
-            'template_id': template_id,
-            'email_address': 'foo@bar.com'
+            'template_id': self.template_id,
+            'email_address': 'pherbert@thoughtworks.com'
         }
         self.client.post('/v2/notifications/email', json=payload, headers=headers)
 
-    def _get_jwt(self):
-        service_id = os.getenv('LOAD_TESTING_SERVICE_ID')
-        jwtSecret = os.getenv('LOAD_TESTING_API_KEY')
+    def _get_jwt(self) -> bytes:
         header = {'typ': 'JWT', 'alg': 'HS256'}
         combo = {}
         currentTimestamp = int(time.time())
         data = {
-            'iss': service_id,
+            'iss': self.service_id,
             'iat': currentTimestamp,
             'exp': currentTimestamp + 30,
             'jti': 'jwt_nonce'
         }
         combo.update(data)
         combo.update(header)
-        encoded_jwt = jwt.encode(combo, jwtSecret, algorithm='HS256')
+        encoded_jwt = jwt.encode(combo, self.api_key, algorithm='HS256')
         return encoded_jwt
