aee6b9484af5812e52ac9536fa586b58af78f90d
Jimmy Royer <jimleroyer@gmail.com>
2021-01-27 14:42:57 -0500

    fix: trying something else to setup db initialization for test

---
+++ .github/workflows/initdb/notify-db-test-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+
+###################################################################
+# This script will get executed *once* the Docker container has 
+# been built. Commands that need to be executed with all available
+# tools and the filesystem mount enabled should be located here. 
+#
+# The PostgreSQL Docker image has an extension mechanism that does 
+# not necessitate to override the entrypoint or main command. One
+# simply has to copy a shell script into the 
+# /docker-entrypoint-initdb.d/ initialization folder. 
+###################################################################
+
+# Create reader user meant for testing writer/reader replica functionality.
+createuser reader -w -D -R -h 127.0.0.1 -p 5432 -U postgres
