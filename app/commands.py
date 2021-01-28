d27e60b24b40dd21e17d427e89b95d8adf41f707
Jimmy Royer <jimleroyer@gmail.com>
2021-01-28 18:51:40 -0500

    fix: Fixed a SQL query that was modified by random keystrokes

--- app/commands.py
+++ app/commands.py
@@ -427,7 +427,7 @@ def migrate_data_to_ft_billing(start_date, end_date):
                 group by bst_date, template_id, service_id, notification_type, provider, rate_multiplier, international,
                     sms_rate, letter_rate, postage, created_at
                 order by bst_date
-            on conflict on constraint ft_billing_pkey do updat
+            on conflict on constraint ft_billing_pkey do update set
              billable_units = excluded.billable_units,
              notifications_sent = excluded.notifications_sent,
              rate = excluded.rate,
