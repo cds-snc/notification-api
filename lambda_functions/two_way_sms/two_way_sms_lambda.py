fbcc0c9240f7bbabed18f3dd5333420e15c8cb37
Ling Tran <ltran@thoughtworks.com>
2021-01-29 17:59:12 -0500

    [#333] Modify sms lambda to opt-in number via sns client

--- lambda_functions/two_way_sms/two_way_sms_lambda.py
+++ lambda_functions/two_way_sms/two_way_sms_lambda.py
@@ -6,6 +6,7 @@ import os
 region = os.getenv("AWS_REGION")
 pinpoint = boto3.client('pinpoint', region_name=region)
 pinpoint_project_id = os.getenv("AWS_PINPOINT_APP_ID")
+sns = boto3.client('sns')
 start_keyword = os.getenv("START_KEYWORD")
 supported_keywords = os.getenv("SUPPORTED_KEYWORDS")
 default_response_message = os.getenv("DEFAULT_RESPONSE_MESSAGE")
@@ -19,20 +20,21 @@ def two_way_sms_handler(event, context):
             recipient_number = parsed_message["originationNumber"]
             sender = parsed_message["destinationNumber"]
 
-            if start_keyword in text_response:
-                # Assumes recipient number is prefixed with '+'
-                endpoint_id = recipient_number[1:]
-                pinpoint.update_endpoint(
-                    ApplicationId=pinpoint_project_id,
-                    EndpointId=endpoint_id,
-                    EndpointRequest={
-                        "Address": recipient_number,
-                        "ChannelType": "SMS",
-                        "OptOut": "NONE"
-                    }
+            if start_keyword in text_response.upper():
+                response = sns.opt_in_phone_number(
+                    phoneNumber=recipient_number
                 )
-            elif text_response not in supported_keywords:
-                pinpoint.send_messages(
+                parsed_response = {
+                    'RequestId': response['ResponseMetadata']['RequestId'],
+                    'StatusCode': response['ResponseMetadata']['HTTPStatusCode'],
+                    'DeliveryStatus': response['MessageResponse'][recipient_number]['DeliveryStatus'],
+                    'StatusMessage': response['MessageResponse'][recipient_number]['DeliveryStatus']
+
+                }
+                logging.info(f"Handler successfully with response {parsed_response}")
+                return parsed_response
+            elif text_response.upper() not in supported_keywords:
+                response = pinpoint.send_messages(
                     ApplicationId=pinpoint_project_id,
                     MessageRequest={
                         "Addresses": {
@@ -49,6 +51,10 @@ def two_way_sms_handler(event, context):
                         }
                     }
                 )
+
+                response_body = response['MessageResponse']['Result'][recipient_number]
+                logging.info(f"Handler successfully sent message with message {response_body}")
+                return response_body
 
     except Exception as error:
         logging.error(f"Handler error when processing sms response: {error}")
