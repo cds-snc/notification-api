8161fc84f250dfb2bd5ad1860599f565deca63fb
saman.moshafi <saman.moshafi@thoughtworks.com>
2021-02-05 11:58:56 -0500

    [#300] test that message is delivered

--- tests/lambda_functions/ses_callback/test_ses_callback_lambda.py
+++ tests/lambda_functions/ses_callback/test_ses_callback_lambda.py
@@ -8,6 +8,8 @@ from lambda_functions.ses_callback.ses_callback_lambda import (
     ROUTING_KEY
 )
 
+CALLBACK_LAMBDA_BOTO = 'lambda_functions.ses_callback.ses_callback_lambda.boto3'
+
 
 def test_lambda_handler(mocker):
     mock_queue = mocker.Mock()
@@ -18,7 +20,7 @@ def test_lambda_handler(mocker):
     mock_boto = mocker.Mock()
     mock_boto.resource.return_value = mock_sqs
 
-    mocker.patch('lambda_functions.ses_callback.ses_callback_lambda.boto3', new=mock_boto)
+    mocker.patch(CALLBACK_LAMBDA_BOTO, new=mock_boto)
 
     event = {
         "Records": [
@@ -82,7 +84,7 @@ def test_lambda_handler_queue_name(mocker):
     mock_boto = mocker.Mock()
     mock_boto.resource.return_value = mock_sqs
 
-    mocker.patch('lambda_functions.ses_callback.ses_callback_lambda.boto3', new=mock_boto)
+    mocker.patch(CALLBACK_LAMBDA_BOTO, new=mock_boto)
 
     event = {
         "Records": [
@@ -93,5 +95,106 @@ def test_lambda_handler_queue_name(mocker):
             }
         ]
     }
+
+    lambda_handler(event, mocker.Mock())
+
+
+def test_lambda_handler_queue_name_with_message(mocker):
+    queue_name_prefix = os.environ['NOTIFICATION_QUEUE_PREFIX'] = 'test-notification-'
+    expected_queue_name = f"{queue_name_prefix}{ROUTING_KEY}"
+    mock_queue = mocker.Mock()
+
+    some_recipient = "somebody@some.organization.com"
+    some_source_email = "test@notifications.va.gov"
+    event = {
+        "Records": [
+            {
+                "Sns": {
+                    "Message": {
+                        "delivery": {
+                            "timestamp": "2021-01-27T21:10:19.584Z",
+                            "processingTimeMillis": 3409,
+                            "recipients": [
+                                some_recipient
+                            ],
+                            "smtpResponse": "250 2.0.0 OK  1611781819 b3si3790733pgk.485 - gsmtp",
+                            "reportingMTA": "d210-3.smtp-out.us-us-east-1.amazonses.com"
+                        },
+                        "mail": {
+                            "timestamp": "2021-01-27T21:10:16.175Z",
+                            "source": some_source_email,
+                            "sourceArn": "arn:aws-us-gov:ses:us-gov-west-1:171875617347:identity/notifications.va.gov",
+                            "sendingAccountId": "171875617347",
+                            "messageId": "010a017745aebf6f-aaddaeab-8091-4930-8c9e-34279e23950c-000000",
+                            "destination": [
+                                some_recipient
+                            ],
+                            "headersTruncated": False,
+                            "headers": [
+                                {
+                                    "name": "From",
+                                    "value": some_source_email
+                                },
+                                {
+                                    "name": "To",
+                                    "value": some_recipient
+                                },
+                                {
+                                    "name": "Subject",
+                                    "value": "test2"
+                                },
+                                {
+                                    "name": "MIME-Version",
+                                    "value": "1.0"
+                                },
+                                {
+                                    "name": "Content-Type",
+                                    "value": "text/plain; charset=UTF-8"
+                                },
+                                {
+                                    "name": "Content-Transfer-Encoding",
+                                    "value": "7bit"
+                                }
+                            ],
+                            "commonHeaders": {
+                                "from": [
+                                    some_source_email
+                                ],
+                                "to": [
+                                    some_recipient
+                                ],
+                                "messageId": "010a017745aebf6f-aaddaeab-8091-4930-8c9e-34279e23950c-000000",
+                                "subject": "test2"
+                            }
+                        },
+                        "eventType": "Delivery"
+                    }
+                }
+            }
+        ]
+    }
+
+    # noinspection PyPep8Naming
+    def mocked_queue_name(QueueName):  # NOSONAR
+        assert QueueName == expected_queue_name
+        return mock_queue
+
+    # noinspection PyPep8Naming
+    def mocked_send_message(MessageBody):  # NOSONAR
+        assert MessageBody
+        envelope = json.loads(base64.b64decode(MessageBody))
+        assert envelope['properties']['delivery_info']['routing_key'] == ROUTING_KEY
+        message_body = json.loads(base64.b64decode(envelope['body']))
+        assert message_body['args'][0]['Message'] == event['Records'][0]['Sns']['Message']
+
+    mock_queue.send_message.side_effect = mocked_send_message
+
+    mock_sqs = mocker.Mock()
+    mock_sqs.get_queue_by_name.side_effect = mocked_queue_name
+
+    mock_boto = mocker.Mock()
+    mock_boto.resource.return_value = mock_sqs
+
+    mocker.patch(CALLBACK_LAMBDA_BOTO, new=mock_boto)
 
     lambda_handler(event, mocker.Mock())
