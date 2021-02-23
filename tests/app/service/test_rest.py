c81493f33fe0ecfe4d026ee9aaa68e925cd4ea4a
Ren-David Dimen <ren-david.dimen@cds-snc.ca>
2021-02-23 14:08:48 -0500

    update string formating style and go live test case

--- tests/app/service/test_rest.py
+++ tests/app/service/test_rest.py
@@ -2140,6 +2140,8 @@ def test_update_service_calls_send_notification_as_service_becomes_live(notify_d
         template_id='618185c6-3636-49cd-b7d2-6f6f5eb3bdde',
         personalisation={
             'service_name': restricted_service.name,
+            'contact_us_url': f"{current_app.config['ADMIN_BASE_URL']}/contact",
+            'signin_url': f"{current_app.config['ADMIN_BASE_URL']}/contact",
             'message_limit_en': '1,000',
             'message_limit_fr': '1 000'
         },
