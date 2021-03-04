76040d700ce0f912f91692cc7e4898f11b0bb3e1
Phil Herbert <pherbert@thoughtworks.com>
2021-03-04 13:58:51 -0500

    refactors GA pixel test imports
    to avoid duplication and renaming

--- tests/app/googleanalytics/test_pixels.py
+++ tests/app/googleanalytics/test_pixels.py
@@ -1,13 +1,15 @@
-import app.googleanalytics.pixels as gapixels
-import urllib
+from urllib.parse import quote_plus
+
 from flask import current_app
+
+from app.googleanalytics.pixels import build_ga_pixel_url
 
 
 def test_build_ga_pixel_url_contains_expected_parameters(
         sample_notification_model_with_organization,
         mock_email_client
 ):
-    img_src_url = gapixels.build_ga_pixel_url(sample_notification_model_with_organization, mock_email_client)
+    img_src_url = build_ga_pixel_url(sample_notification_model_with_organization, mock_email_client)
 
     assert img_src_url is not None
 
@@ -31,15 +33,15 @@ def test_build_ga_pixel_url_contains_expected_parameters(
 
 
 def test_build_ga_pixel_url_is_escaped(sample_notification_model_with_organization, mock_email_client):
-    escaped_provider_name = urllib.parse.quote_plus(mock_email_client.get_name())
-    escaped_template_name = urllib.parse.quote_plus(sample_notification_model_with_organization.template.name)
-    escaped_service_name = urllib.parse.quote_plus(sample_notification_model_with_organization.service.name)
-    escaped_organization_name = urllib.parse.quote_plus(
+    escaped_provider_name = quote_plus(mock_email_client.get_name())
+    escaped_template_name = quote_plus(sample_notification_model_with_organization.template.name)
+    escaped_service_name = quote_plus(sample_notification_model_with_organization.service.name)
+    escaped_organization_name = quote_plus(
         sample_notification_model_with_organization.service.organisation.name
     )
-    escaped_subject_name = urllib.parse.quote_plus(sample_notification_model_with_organization.subject)
+    escaped_subject_name = quote_plus(sample_notification_model_with_organization.subject)
 
-    img_src_url = gapixels.build_ga_pixel_url(sample_notification_model_with_organization, mock_email_client)
+    img_src_url = build_ga_pixel_url(sample_notification_model_with_organization, mock_email_client)
 
     ga_tid = current_app.config['GOOGLE_ANALYTICS_TID']
     assert 'v=1' in img_src_url
@@ -65,8 +67,8 @@ def test_build_ga_pixel_url_without_organization(sample_notification_model_with_
 
     sample_notification_model_with_organization.service.organisation = None
 
-    img_src_url = gapixels.build_ga_pixel_url(sample_notification_model_with_organization, mock_email_client)
+    img_src_url = build_ga_pixel_url(sample_notification_model_with_organization, mock_email_client)
 
     service_name = sample_notification_model_with_organization.service.name
     template_name = sample_notification_model_with_organization.template.name
-    assert urllib.parse.quote_plus(f"/email/vanotify/{service_name}/{template_name}") in img_src_url
+    assert quote_plus(f"/email/vanotify/{service_name}/{template_name}") in img_src_url
