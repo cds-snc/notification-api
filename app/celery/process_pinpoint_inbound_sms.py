02fb3a0bdf2f6f8581d8c80b8a28500a29bedcb2
Phil Herbert <pherbert@thoughtworks.com>
2021-03-19 12:27:43 -0400

    [#353] tries to fetch service for inbound sms, throws if not found

--- app/celery/process_pinpoint_inbound_sms.py
+++ app/celery/process_pinpoint_inbound_sms.py
@@ -1,13 +1,38 @@
+import base64
+import json
+
 from flask import current_app
+from typing_extensions import TypedDict
 from notifications_utils.statsd_decorators import statsd
 
 from app import notify_celery
 from app.feature_flags import FeatureFlag, is_feature_enabled
+from app.notifications.receive_notifications import fetch_potential_service
 
 
-@notify_celery.task(bind=True, name="process-pinpoint-inbound-sms", max_retries=5, default_retry_delay=300)
-@statsd(namespace="tasks")
-def process_pinpoint_inbound_sms(self, event):
+class PinpointInboundSmsMessage(TypedDict):
+    messageBody: str
+    destinationNumber: str
+    originationNumber: str
+
+
+class CeleryEvent(TypedDict):
+    Message: str
+
+
+class NoSuitableServiceForInboundSms(Exception):
+    pass
+
+
+@notify_celery.task(bind=True, name='process-pinpoint-inbound-sms', max_retries=5, default_retry_delay=300)
+@statsd(namespace='tasks')
+def process_pinpoint_inbound_sms(self, event: CeleryEvent):
     if not is_feature_enabled(FeatureFlag.PINPOINT_INBOUND_SMS_ENABLED):
         current_app.logger.info('Pinpoint inbound SMS toggle is disabled, skipping task')
         return True
+
+    pinpoint_message: PinpointInboundSmsMessage = json.loads(base64.b64decode(event['Message']))
+
+    service = fetch_potential_service(pinpoint_message['destinationNumber'], 'pinpoint')
+    if not service:
+        raise NoSuitableServiceForInboundSms
--- tests/app/celery/test_process_pinpoint_inbound_sms.py
+++ tests/app/celery/test_process_pinpoint_inbound_sms.py
@@ -1,4 +1,10 @@
-from app.celery.process_pinpoint_inbound_sms import process_pinpoint_inbound_sms
+import base64
+import json
+
+import pytest
+
+from app.celery.process_pinpoint_inbound_sms import process_pinpoint_inbound_sms, CeleryEvent, \
+    PinpointInboundSmsMessage, NoSuitableServiceForInboundSms
 from app.feature_flags import FeatureFlag
 
 
@@ -8,3 +14,30 @@ def test_passes_if_toggle_disabled(mocker, db_session):
     process_pinpoint_inbound_sms(event={})
 
     mock_toggle.assert_called_with(FeatureFlag.PINPOINT_INBOUND_SMS_ENABLED)
+
+
+def test_fails_if_no_matching_service(mocker, db_session):
+    mocker.patch('app.celery.process_pinpoint_inbound_sms.is_feature_enabled', return_value=True)
+
+    mock_fetch_potential_service = mocker.patch(
+        'app.celery.process_pinpoint_inbound_sms.fetch_potential_service',
+        return_value=False
+    )
+
+    destination_number = "1234"
+
+    with pytest.raises(NoSuitableServiceForInboundSms):
+        process_pinpoint_inbound_sms(event=_pinpoint_inbound_sms_event(destination_number))
+
+    mock_fetch_potential_service.assert_called_with(destination_number, 'pinpoint')
+
+
+def _pinpoint_inbound_sms_event(destination_number: str) -> CeleryEvent:
+    pinpoint_message: PinpointInboundSmsMessage = {
+        "messageBody": 'foo',
+        "destinationNumber": destination_number,
+        "originationNumber": '5678'
+    }
+    return {
+        'Message': base64.b64encode(bytes(json.dumps(pinpoint_message), 'utf-8')).decode('utf-8')
+    }
