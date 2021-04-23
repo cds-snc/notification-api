b7a4ea4cf2c978eb8f9d3b94b5ae3d1f8d47cfbe
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-04-23 09:13:02 -0400

    feat: add tests when generating callbacks

---
+++ tests/app/notifications/test_callbacks.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+from app import encryption, DATETIME_FORMAT
+from app.notifications.callbacks import (
+    create_complaint_callback_data,
+    create_delivery_status_callback_data,
+)
+from tests.app.db import (
+    create_service_callback_api,
+    create_complaint,
+)
+from tests.app.conftest import sample_notification as create_sample_notification
+
+
+def test_create_delivery_status_callback_data(
+    notify_db,
+    notify_db_session,
+    sample_email_template,
+):
+    notification = create_sample_notification(
+        notify_db,
+        notify_db_session,
+        template=sample_email_template,
+        status='sending',
+        sent_at=datetime.utcnow()
+    )
+    callback_api = create_service_callback_api(service=sample_email_template.service, url="https://original_url.com")
+
+    assert encryption.decrypt(
+        create_delivery_status_callback_data(notification, callback_api)
+    ) == {
+        'notification_client_reference': notification.client_reference,
+        'notification_created_at': notification.created_at.strftime(DATETIME_FORMAT),
+        'notification_id': str(notification.id),
+        'notification_provider_response': notification.provider_response,
+        'notification_sent_at': notification.sent_at.strftime(DATETIME_FORMAT),
+        'notification_status': notification.status,
+        'notification_to': notification.to,
+        'notification_type': notification.notification_type,
+        'notification_updated_at': notification.updated_at,
+        'service_callback_api_bearer_token': callback_api.bearer_token,
+        'service_callback_api_url': callback_api.url
+    }
+
+
+def test_create_complaint_callback_data(
+    notify_db,
+    notify_db_session,
+    sample_email_template,
+):
+    notification = create_sample_notification(
+        notify_db,
+        notify_db_session,
+        template=sample_email_template,
+        status='delivered',
+        sent_at=datetime.utcnow()
+    )
+    complaint = create_complaint(notification=notification, service=notification.service)
+    callback_api = create_service_callback_api(service=sample_email_template.service, url="https://original_url.com")
+
+    assert encryption.decrypt(
+        create_complaint_callback_data(complaint, notification, callback_api, "recipient@example.com")
+    ) == {
+        "complaint_id": str(complaint.id),
+        "notification_id": str(notification.id),
+        "reference": notification.client_reference,
+        "to": "recipient@example.com",
+        "complaint_date": complaint.complaint_date.strftime(DATETIME_FORMAT),
+        "service_callback_api_url": callback_api.url,
+        "service_callback_api_bearer_token": callback_api.bearer_token,
+    }
