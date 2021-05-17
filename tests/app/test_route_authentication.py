6dbe04e6ad5e77ac1206b02824f3afd0103a85a0
Phil Herbert <pherbert@thoughtworks.com>
2021-05-17 14:37:53 -0400

    refactors test that verifies all routes have authentication
    since we'll be moving some authentication to decorators, which won't be detected by checking before_request_funcs

--- tests/app/test_route_authentication.py
+++ tests/app/test_route_authentication.py
@@ -1,12 +1,50 @@
-def test_all_routes_have_authentication(client):
-    # This tests that each blueprint registered on the application has a before_request function registered.
-    # The None row is removed from the comparison as that is not blueprint specific but app specific.
-    before_req_funcs = set(x for x in client.application.before_request_funcs if x is not None)
+import uuid
 
-    blueprint_names = set(client.application.blueprints.keys())
-    assert blueprint_names == before_req_funcs
+from flask import url_for
+from werkzeug.routing import UUIDConverter, UnicodeConverter, IntegerConverter, PathConverter
 
-    # The static route is always available by default for a Flask app to serve anything in the static folder.
-    routes_blueprint_names = set(
-        [x.split('.')[0] for x in client.application.view_functions.keys() if x.split('.')[0] != 'static'])
-    assert sorted(blueprint_names) == sorted(routes_blueprint_names)
+
+def test_all_routes_have_authentication(client, db_session):
+    routes_without_authentication = set()
+
+    for rule in client.application.url_map.iter_rules():
+
+        if rule.endpoint == 'static':
+            continue
+
+        methods = [method for method in rule.methods if method != 'OPTIONS']
+        for method in methods:
+            make_request = getattr(client, method.lower())
+
+            response = make_request(_build_url(rule))
+
+            if response.status_code not in [401, 403]:
+                routes_without_authentication.add(str(rule))
+
+    expected_routes_without_authentication = {
+        '/',
+        '/_status',
+        '/_status/live-service-and-organisation-counts',
+        '/authorize',
+        '/login',
+        '/notifications/govdelivery',
+        '/redeem-token'
+    }
+
+    assert routes_without_authentication == expected_routes_without_authentication
+
+
+def _build_url(rule):
+    example_path_variables = {
+        UUIDConverter: uuid.uuid4(),
+        UnicodeConverter: 'example-string',
+        IntegerConverter: 1,
+        PathConverter: '/'
+    }
+
+    params = {
+        path_variable_name: example_path_variables[type(converter)]
+        for path_variable_name, converter in rule._converters.items()
+    }
+
+    return url_for(rule.endpoint, **params)
