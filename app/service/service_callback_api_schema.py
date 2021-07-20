b295f0417cefd5590068f3ab2a4db6397662fc0c
Emily Wu <ewu@thoughtworks.com>
2021-07-20 11:46:13 -0400

    [#504] Remove bearer_token from required fields in ServiceCallback schema. Remove unused create_service_inbound_api_schema
    Co-authored-by: Vikram Kohli <vikram.kohli@thoughtworks.com>

--- app/service/service_callback_api_schema.py
+++ app/service/service_callback_api_schema.py
@@ -1,19 +1,6 @@
 from app.models import NOTIFICATION_STATUS_TYPES_COMPLETED
 from app.schema_validation.definitions import uuid, https_url
 
-create_service_inbound_api_schema = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "description": "POST service callback/inbound api schema",
-    "type": "object",
-    "title": "Create service callback/inbound api",
-    "properties": {
-        "url": https_url,
-        "bearer_token": {"type": "string", "minLength": 10},
-        "updated_by_id": uuid
-    },
-    "required": ["url", "bearer_token", "updated_by_id"]
-}
-
 update_service_inbound_api_schema = {
     "$schema": "http://json-schema.org/draft-04/schema#",
     "description": "POST service callback/inbound api schema",
@@ -49,7 +36,7 @@ create_service_callback_api_request_schema = {
         },
         "updated_by_id": uuid
     },
-    "required": ["updated_by_id", "url", "bearer_token"]
+    "required": ["updated_by_id", "url"]
 }
 
 update_service_callback_api_request_schema = {
