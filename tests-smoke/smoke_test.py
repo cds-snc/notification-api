529a0c0b125ce142b6fb991885b16d7f583464ce
Stephen Astels <sastels@gmail.com>
2021-07-23 18:27:59 -0400

    admin sms one-off

--- tests-smoke/smoke_test.py
+++ tests-smoke/smoke_test.py
@@ -1,7 +1,8 @@
+from test_admin_email_one_off import test_admin_email_one_off
+from test_admin_sms_one_off import test_admin_sms_one_off
 from test_api_email import test_api_email
 from test_api_sms import test_api_sms
 from test_api_email_bulk import test_api_email_bulk
-from test_admin_email_one_off import test_admin_email_one_off
 from test_api_sms_bulk import test_api_sms_bulk
 
 if __name__ == "__main__":
@@ -11,3 +12,4 @@ if __name__ == "__main__":
     test_api_sms_bulk()
 
     test_admin_email_one_off()
+    test_admin_sms_one_off()
