bd080ff8b04cbb9d10e76c755fdd28a2523603ce
Stephen Astels <sastels@gmail.com>
2021-07-29 12:55:10 -0400

    email csv test

--- tests-smoke/test_admin_email_csv.py
+++ tests-smoke/test_admin_email_csv.py
@@ -1,13 +1,19 @@
+import csv
+
 from dotenv import load_dotenv
+from io import StringIO
+import itertools
 import json
 from notifications_python_client.authentication import create_jwt_token
 import os
+from notifications_utils.s3 import s3upload as utils_s3upload
 import requests
 import time
 import uuid
-
+from boto3 import resource
 
 # NOTE: this doesn't work yet
+
 
 load_dotenv()
 
@@ -17,8 +23,66 @@ USER_ID = os.environ.get("USER_ID")
 SERVICE_ID = os.environ.get("SERVICE_ID")
 ADMIN_CLIENT_SECRET = os.environ.get("ADMIN_CLIENT_SECRET")
 ADMIN_CLIENT_USER_NAME = os.environ.get("ADMIN_CLIENT_USER_NAME")
-EMAIL_SEND_TO = os.environ.get("EMAIL_SEND_TO")
+EMAIL_TO = os.environ.get("EMAIL_TO")
 API_HOST_NAME = os.environ.get("API_HOST_NAME")
+CSV_UPLOAD_BUCKET_NAME = os.environ.get("CSV_UPLOAD_BUCKET_NAME")
+AWS_REGION = os.environ.get("AWS_REGION")
+
+
+def rows_to_csv(rows):
+    output = StringIO()
+    writer = csv.writer(output)
+    writer.writerows(rows)
+    return output.getvalue()
+
+
+def job_line(number_of_lines):
+    return list(itertools.repeat([EMAIL_TO, "test"], number_of_lines))
+
+
+# from admin app/s3_client/c3_csv_client.py ---------
+
+FILE_LOCATION_STRUCTURE = "service-{}-notify/{}.csv"
+
+
+def get_csv_location(service_id, upload_id):
+    return (
+        CSV_UPLOAD_BUCKET_NAME,
+        FILE_LOCATION_STRUCTURE.format(service_id, upload_id),
+    )
+
+
+def s3upload(service_id, data):
+    upload_id = str(uuid.uuid4())
+    bucket_name, file_location = get_csv_location(service_id, upload_id)
+    utils_s3upload(
+        filedata=data,
+        region=AWS_REGION,
+        bucket_name=bucket_name,
+        file_location=file_location,
+    )
+    return upload_id
+
+
+def set_metadata_on_csv_upload(service_id, upload_id, **kwargs):
+    get_csv_upload(service_id, upload_id).copy_from(
+        CopySource="{}/{}".format(*get_csv_location(service_id, upload_id)),
+        ServerSideEncryption="AES256",
+        Metadata={key: str(value) for key, value in kwargs.items()},
+        MetadataDirective="REPLACE",
+    )
+
+
+def get_csv_upload(service_id, upload_id):
+    return get_s3_object(*get_csv_location(service_id, upload_id))
+
+
+def get_s3_object(bucket_name, filename):
+    s3 = resource("s3")
+    return s3.Object(bucket_name, filename)
+
+
+# --------------------------------------------------
 
 
 def pretty_print(data):
@@ -28,16 +92,32 @@ def pretty_print(data):
 def test_admin_email_csv():
     print("test_admin_csv... ", end="", flush=True)
 
+    data = rows_to_csv([["email address", "name"], *job_line(1)])
+
+    upload_id = s3upload(SERVICE_ID, data)
+    metadata_kwargs = {
+        "notification_count": 1,
+        "template_id": str(EMAIL_TEMPLATE_ID),
+        "valid": True,
+        "original_file_name": "smoke_test.csv",
+    }
+
+    set_metadata_on_csv_upload(SERVICE_ID, upload_id, **metadata_kwargs)
+
+    print(f"upload id: {upload_id}")
+    print(f"file: {get_csv_location(SERVICE_ID, upload_id)}")
+
     token = create_jwt_token(ADMIN_CLIENT_SECRET, client_id=ADMIN_CLIENT_USER_NAME)
 
     response = requests.post(
-        f"{API_HOST_NAME}/services/{SERVICE_ID}/send/{TEMPLATE_ID}/csv",
-        json={},
+        f"{API_HOST_NAME}/service/{SERVICE_ID}/job",
+        json={"id": upload_id, "created_by": USER_ID},
         headers={"Authorization": "Bearer {}".format(token)},
     )
-
     status_code = response.status_code
     body = response.json()
+
+    print(f"status: {status_code}")
     pretty_print(body)
 
     if status_code != 201:
@@ -45,26 +125,21 @@ def test_admin_email_csv():
         pretty_print(body)
         return
 
-    notification_id = body["id"]
+    uri = f"{API_HOST_NAME}/service/{SERVICE_ID}/job/{upload_id}"
+
     for _ in range(20):
         time.sleep(1)
-        response = requests.get(
-            f"{API_HOST_NAME}/service/{SERVICE_ID}/notifications/{notification_id}",
-            headers={"Authorization": "Bearer {}".format(token)},
-        )
+        response = requests.get(uri, headers={"Authorization": "Bearer {}".format(token)})
         status_code = response.status_code
-        body = response.json()
-        if status_code != 200:
-            print("FAILED: couldn't get notification status")
-            pretty_print(body)
-            return
-        if body["status"] == "sending":
+        data = response.json()["data"]
+        if status_code == 200 and data.get("job_status") == "finished":
             break
 
-    if body["status"] != "sending":
-        print("FAILED: email not sent successfully")
-        pretty_print(body)
+    if status_code != 200 or data.get("job_status") != "finished":
+        print("FAILED: job didn't finish successfully")
+        pretty_print(response.json())
         return
+
     print("Success")
 
 
