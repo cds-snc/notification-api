58ca6b943ef039d18ca8e5f015eb6152b95b4daa
Phil Herbert <pherbert@thoughtworks.com>
2021-08-06 13:53:32 -0400

    [#507] fixes test that checks validation error message

--- tests/app/v2/test_errors.py
+++ tests/app/v2/test_errors.py
@@ -99,7 +99,7 @@ def test_validation_error(app_for_test):
             assert error['status_code'] == 400
             assert len(error['errors']) == 2
             assert {'error': 'ValidationError',
-                    'message': "{template_id: bad_uuid} is not valid under any of the given schemas"} in error['errors']
+                    'message': "Please provide either a phone number or a recipient identifier"} in error['errors']
             assert {'error': 'ValidationError',
                     'message': "template_id is not a valid UUID"} in error['errors']
 
