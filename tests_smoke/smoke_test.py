b7dbffff41c79f6a5965a0501699324d7e39a2ac
Stephen Astels <sastels@gmail.com>
2021-08-20 13:12:54 -0400

    test sending files as links

--- tests_smoke/smoke_test.py
+++ tests_smoke/smoke_test.py
@@ -4,6 +4,7 @@ from smoke.test_admin_one_off import test_admin_one_off
 from smoke.test_api_bulk import test_api_bulk
 from smoke.test_api_one_off import test_api_one_off
 from smoke.test_api_one_off_file_attached import test_api_one_off_file_attached
+from smoke.test_api_one_off_file_link import test_api_one_off_file_link
 
 if __name__ == "__main__":
     for notification_type in [Notification_type.EMAIL, Notification_type.SMS]:
@@ -12,3 +13,4 @@ if __name__ == "__main__":
         test_api_one_off(notification_type)
         test_api_bulk(notification_type)
     test_api_one_off_file_attached()
+    test_api_one_off_file_link()
