43e108d5f63342f2b247e2415e00f5f10c2ab07f
Stephen Astels <sastels@gmail.com>
2021-08-23 09:48:31 -0400

    merge tests

--- tests_smoke/smoke/test_api_one_off.py
+++ tests_smoke/smoke/test_api_one_off.py
@@ -1,22 +1,57 @@
+from typing import Any, Dict
+
 import requests
 
-from .common import Config, Notification_type, pretty_print, single_succeeded
+from .common import (
+    Attachment_type,
+    Config,
+    Notification_type,
+    pretty_print,
+    single_succeeded,
+)
 
 
-def test_api_one_off(notification_type: Notification_type):
-    print(f"test_api_oneoff ({notification_type.value})... ", end="", flush=True)
+def test_api_one_off(notification_type: Notification_type, attachment_type: Attachment_type = Attachment_type.NONE):
+    if attachment_type is Attachment_type.NONE:
+        print(f"test_api_oneoff ({notification_type.value})... ", end="", flush=True)
+    else:
+        print(f"test_api_oneoff ({notification_type.value}, file {attachment_type.value}ed)... ", end="", flush=True)
 
+    if notification_type is Notification_type.SMS and attachment_type is not Attachment_type.NONE:
+        print("Error: can't use files with sms")
+        return
+
+    data: Dict[str, Any]
     if notification_type == Notification_type.EMAIL:
         data = {
             "email_address": Config.EMAIL_TO,
             "template_id": Config.EMAIL_TEMPLATE_ID,
-            "personalisation": {"var": "var"},
         }
     else:
         data = {
             "phone_number": Config.SMS_TO,
             "template_id": Config.SMS_TEMPLATE_ID,
-            "personalisation": {"var": "var"},
+        }
+    if attachment_type is Attachment_type.ATTACHED:
+        data["personalisation"] = {
+            "var": "var",
+            "application_file": {
+                "file": "aGkgdGhlcmU=",
+                "filename": "test_file.txt",
+                "sending_method": f"{attachment_type.value}",
+            },
+        }
+    elif attachment_type is Attachment_type.LINK:
+        data["personalisation"] = {
+            "var": {
+                "file": "aGkgdGhlcmU=",
+                "filename": "test_file.txt",
+                "sending_method": f"{attachment_type.value}",
+            }
+        }
+    else:
+        data["personalisation"] = {
+            "var": "var",
         }
 
     response = requests.post(
--- tests_smoke/smoke_test.py
+++ tests_smoke/smoke_test.py
@@ -1,10 +1,8 @@
-from smoke.common import Notification_type
+from smoke.common import Attachment_type, Notification_type
 from smoke.test_admin_csv import test_admin_csv
 from smoke.test_admin_one_off import test_admin_one_off
 from smoke.test_api_bulk import test_api_bulk
 from smoke.test_api_one_off import test_api_one_off
-from smoke.test_api_one_off_file_attached import test_api_one_off_file_attached
-from smoke.test_api_one_off_file_link import test_api_one_off_file_link
 
 if __name__ == "__main__":
     for notification_type in [Notification_type.EMAIL, Notification_type.SMS]:
@@ -12,5 +10,5 @@ if __name__ == "__main__":
         test_admin_csv(notification_type)
         test_api_one_off(notification_type)
         test_api_bulk(notification_type)
-    test_api_one_off_file_attached()
-    test_api_one_off_file_link()
+    test_api_one_off(Notification_type.EMAIL, Attachment_type.ATTACHED)
+    test_api_one_off(Notification_type.EMAIL, Attachment_type.LINK)
