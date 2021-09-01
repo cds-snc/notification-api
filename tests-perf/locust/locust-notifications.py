72412c8a0c09eea5e44b787cf1c7af8012170e57
iokpala <ikenna.okpala@cds-snc.ca>
2021-09-01 15:23:02 -0200

    Define load tests for one off email
    This commit seeks to define load tests to test the one off email
    endpoint[1].
    
    Prior to this commit one example load tests already exists. This effort
    expands the one off email enpoint load test to cover attachment and link.
    
    This choice is influenced by the already existing smoke tests for one off email.
    
    Locust insists on have a task defined[2], which it uses to simulate the to run the task,
    this can be weighted which yields probable selection based on the weight.
    
    The developers understand, would be that of isolated tasks that would run on thier own greenlet(thread),
    hence this has led to the definition of seperate class in order to achieve the aforementioned.
    
    [1] <domain>/v2/notifications/email
    [2] https://docs.locust.io/en/stable/writing-a-locustfile.html

--- tests-perf/locust/locust-notifications.py
+++ tests-perf/locust/locust-notifications.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from locust import HttpUser, constant_pacing, task
+from locust import HttpUser, constant_pacing, task, tag
 
 load_dotenv()
 AUTH_HEADER = os.getenv("TEST_AUTH_HEADER")
@@ -12,13 +12,50 @@ class NotifyApiUser(HttpUser):
     wait_time = constant_pacing(60)
     host = "https://api.staging.notification.cdssandbox.xyz"
 
-    @task
-    def send_notifications(self):
-        headers = {"Authorization": AUTH_HEADER}
-        json = {
+    def __init__(self, *args, **kwargs):
+        super(NotifyApiUser, self).__init__(*args, **kwargs)
+
+        self.headers = {"Authorization": AUTH_HEADER}
+        self.json = {
             "email_address": "success@simulator.amazonses.com",
             "template_id": "9c17633c-126a-4ad3-ad2f-b14c3a85314a",
             "personalisation": {"colour": "Fulvous"},
         }
 
-        self.client.post("/v2/notifications/email", json=json, headers=headers)
+    @task
+    def task(self):
+        pass
+
+class OneOffEmailMessage(NotifyApiUser):
+    @tag("one-off-email")
+    @task
+    def send_one_off_email_notifications(self):
+        self.client.post("/v2/notifications/email", json=self.json, headers=self.headers)
+
+class OneOffEmailWithFileAttachmentMessage(NotifyApiUser):
+    @tag("one-off-email-attached")
+    @task
+    def send_one_off_email_with_attachment_notifications(self):
+        self.json["personalisation"] = {
+            "application_file": {
+                "file": "Q29udGVudCBvZiBBdHRhY2hlZCBmaWxl",
+                "filename": "attached_file.txt",
+                "sending_method": "attach",
+            }
+        }
+
+        self.client.post("/v2/notifications/email", json=self.json, headers=self.headers)
+
+class OneOffEmailWithLinkMessage(NotifyApiUser):
+    @tag("one-off-email-link")
+    @task
+    def send_one_off_email_with_link_notifications(self):
+        self.json["personalisation"] = {
+            "var": {
+                "file": "Q29udGVudCBvZiBBdHRhY2hlZCBmaWxl",
+                "filename": "attached_file.txt",
+                "sending_method": "link",
+            }
+        }
+
+        self.client.post("/v2/notifications/email", json=self.json, headers=self.headers)
