86833383fd0bea8fc4aca85c5f1e10b6955a8038
iokpala <ikenna.okpala@cds-snc.ca>
2021-09-10 18:32:05 -0200

    Define bulk email load test
    This commits appends a bulk email load test to the existing
    one off email load tests

--- tests-perf/locust/locust-notifications.py
+++ tests-perf/locust/locust-notifications.py
@@ -1,7 +1,10 @@
-import os
+import sys, os
+sys.path.append(os.path.abspath(os.path.join("..", "tests_smoke")))
 
+from datetime import datetime
 from dotenv import load_dotenv
 from locust import HttpUser, constant_pacing, task
+from tests_smoke.smoke.common import job_line, rows_to_csv
 
 load_dotenv()
 AUTH_HEADER = os.getenv("TEST_AUTH_HEADER")
@@ -49,3 +52,23 @@ class NotifyApiUser(HttpUser):
         }
 
         self.client.post("/v2/notifications/email", json=self.json, headers=self.headers)
+
+    @task(8)
+    def send_bulk_notifications(self):
+        self.json = {
+            "name": f"My bulk name {datetime.utcnow().isoformat()}",
+            "template_id": "5ebee3b7-63c0-4052-a8cb-387b818df627",
+            "csv": rows_to_csv([["email address", "var"], *job_line(self.json["email_address"], 2)])
+        }
+
+        self.client.post("/v2/notifications/bulk", json=self.json, headers=self.headers)
+
+    @task(12)
+    def send_sms_notifications(self):
+        self.json = {
+            "phone_number": 12897682684,
+            "template_id": "d8234b4d-4def-4ad6-aafe-526a24ee5f19",
+            "var": "var"
+        }
+
+        self.client.post("/v2/notifications/sms", json=self.json, headers=self.headers)
