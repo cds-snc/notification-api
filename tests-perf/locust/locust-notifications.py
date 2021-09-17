a1ff3d52c0b36b6338b88dcca68bf46d05c2852f
iokpala <ikenna.okpala@cds-snc.ca>
2021-09-17 18:55:15 -0200

    Extraction request payload to private method
    This commit updates the NotifyApiUser class with a private method
    
    that is meant to help swtch personalisation and template id when
    required

--- tests-perf/locust/locust-notifications.py
+++ tests-perf/locust/locust-notifications.py
@@ -19,63 +19,64 @@ class NotifyApiUser(HttpUser):
         super(NotifyApiUser, self).__init__(*args, **kwargs)
 
         self.headers = {"Authorization": AUTH_HEADER}
-        self.template_id = "5ebee3b7-63c0-4052-a8cb-387b818df627"
         self.email = "success@simulator.amazonses.com"
-        self.json = {
-            "email_address": self.email,
-            "template_id": self.template_id,
-            "personalisation": {},
-        }
+        self.template_id = "5ebee3b7-63c0-4052-a8cb-387b818df627"
 
     @task(1)
     def send_email_notifications(self):
-        self.json = {
-            "email_address": self.email,
-            "template_id": "a59b313d-8de2-4973-ac2f-66de7ec0b239",
-            "personalisation": {},
-        }
+        json = self.__email_json("a59b313d-8de2-4973-ac2f-66de7ec0b239")
 
-        self.client.post("/v2/notifications/email", json=self.json, headers=self.headers)
+        self.client.post("/v2/notifications/email", json=json, headers=self.headers)
 
     @task(2)
     def send_email_with_attachment_notifications(self):
-        self.json["personalisation"] = {
+        personalisation = {
             "application_file": {
                 "file": "Q29udGVudCBvZiBBdHRhY2hlZCBmaWxl",
                 "filename": "attached_file.txt",
                 "sending_method": "attach",
             }
         }
+        json = self.__email_json(self.template_id, personalisation)
 
-        self.client.post("/v2/notifications/email", json=self.json, headers=self.headers)
+        self.client.post("/v2/notifications/email", json=json, headers=self.headers)
 
     @task(4)
     def send_email_with_link_notifications(self):
-        self.json["personalisation"] = {
+        personalisation = {
             "application_file": {
                 "file": "Q29udGVudCBvZiBBdHRhY2hlZCBmaWxl",
                 "filename": "attached_file.txt",
                 "sending_method": "link",
             }
         }
+        json = self.__email_json(self.template_id, personalisation)
 
-        self.client.post("/v2/notifications/email", json=self.json, headers=self.headers)
+
+        self.client.post("/v2/notifications/email", json=json, headers=self.headers)
 
     @task(8)
     def send_bulk_notifications(self):
-        self.json = {
+        json = {
             "name": f"My bulk name {datetime.utcnow().isoformat()}",
-            "template_id": "5ebee3b7-63c0-4052-a8cb-387b818df627",
+            "template_id": self.template_id,
             "csv": rows_to_csv([["email address", "application_file"], *job_line(self.email, 2)])
         }
 
-        self.client.post("/v2/notifications/bulk", json=self.json, headers=self.headers)
+        self.client.post("/v2/notifications/bulk", json=json, headers=self.headers)
 
     @task(16)
     def send_sms_notifications(self):
-        self.json = {
+        json = {
             "phone_number": "12897682684",
             "template_id": "83d01f06-a818-4134-bd69-ce90a2949280"
         }
 
-        self.client.post("/v2/notifications/sms", json=self.json, headers=self.headers)
+        self.client.post("/v2/notifications/sms", json=json, headers=self.headers)
+
+    def __email_json(self, template_id, personalisation = {}):
+        return {
+            "email_address": self.email,
+            "template_id": template_id,
+            "personalisation": personalisation,
+        }
