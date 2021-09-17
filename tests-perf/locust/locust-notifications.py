240352af670a460e36884fbada475699adf4aa8f
iokpala <ikenna.okpala@cds-snc.ca>
2021-09-17 11:35:54 -0300

    Correction from Cloudwatch debug
    These are a collection of changes based on Cloudwatch logs

--- tests-perf/locust/locust-notifications.py
+++ tests-perf/locust/locust-notifications.py
@@ -19,8 +19,9 @@ class NotifyApiUser(HttpUser):
         super(NotifyApiUser, self).__init__(*args, **kwargs)
 
         self.headers = {"Authorization": AUTH_HEADER}
+        self.email = "success@simulator.amazonses.com"
         self.json = {
-            "email_address": "success@simulator.amazonses.com",
+            "email_address": self.email,
             "template_id": "5ebee3b7-63c0-4052-a8cb-387b818df627",
             "personalisation": {},
         }
@@ -44,7 +45,7 @@ class NotifyApiUser(HttpUser):
     @task(4)
     def send_email_with_link_notifications(self):
         self.json["personalisation"] = {
-            "var": {
+            "application_file": {
                 "file": "Q29udGVudCBvZiBBdHRhY2hlZCBmaWxl",
                 "filename": "attached_file.txt",
                 "sending_method": "link",
@@ -58,7 +59,7 @@ class NotifyApiUser(HttpUser):
         self.json = {
             "name": f"My bulk name {datetime.utcnow().isoformat()}",
             "template_id": "5ebee3b7-63c0-4052-a8cb-387b818df627",
-            "csv": rows_to_csv([["email address", "var"], *job_line(self.json["email_address"], 2)])
+            "csv": rows_to_csv([["email address", "var"], *job_line(self.email, 2)])
         }
 
         self.client.post("/v2/notifications/bulk", json=self.json, headers=self.headers)
@@ -66,9 +67,8 @@ class NotifyApiUser(HttpUser):
     @task(16)
     def send_sms_notifications(self):
         self.json = {
-            "phone_number": 12897682684,
-            "template_id": "d8234b4d-4def-4ad6-aafe-526a24ee5f19",
-            "var": "var"
+            "phone_number": "12897682684",
+            "template_id": "d8234b4d-4def-4ad6-aafe-526a24ee5f19"
         }
 
         self.client.post("/v2/notifications/sms", json=self.json, headers=self.headers)
