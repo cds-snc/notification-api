a8501ca0b35083225e6b86f014791adab0cae86c
iokpala <ikenna.okpala@cds-snc.ca>
2021-09-24 16:25:45 -0200

    Define and apply load test ENV VARS
    Following feedback from the pull request, it became imperative that the key configurable
    attributes are identified, grouped and sensible defaults applied through these environment
    variables.
    
    Given the ever-changing nature of Amazon test variables, these changes should potentially help
    save costs as cost-efficient email and phone numbers could be used for these potentially large number
    of emails and SMS tests that may be required
    
    An advantage also at play here is that the test class would require to change for
    limited reasons and variable templates, emails, and phone numbers could be used.

--- tests-perf/locust/locust-notifications.py
+++ tests-perf/locust/locust-notifications.py
@@ -1,11 +1,12 @@
 """ locust-notifications.py
-   isort:skip_file
+    isort:skip_file
 """
 # flake8: noqa
 
 import os
 import sys
 from datetime import datetime
+from dataclasses import make_dataclass
 
 sys.path.append(os.path.abspath(os.path.join("..", "tests_smoke")))
 
@@ -14,6 +15,13 @@ from locust import HttpUser, constant_pacing, task
 from tests_smoke.smoke.common import job_line, rows_to_csv  # type: ignore
 
 load_dotenv()
+NotifyApiUserTemplateGroup = make_dataclass('NotifyApiUserTemplateGroup', [
+    'bulk_email_id',
+    'email_id',
+    'email_with_attachment_id',
+    'email_with_link_id',
+    'sms_id',
+])
 
 
 class NotifyApiUser(HttpUser):
@@ -25,26 +33,32 @@ class NotifyApiUser(HttpUser):
         super(NotifyApiUser, self).__init__(*args, **kwargs)
 
         self.headers = {"Authorization": os.getenv("TEST_AUTH_HEADER")}
-        self.email = "success@simulator.amazonses.com"
-        self.template_id = "5ebee3b7-63c0-4052-a8cb-387b818df627"
+        self.email = os.getenv("LOAD_TEST_EMAIL", "success@simulator.amazonses.com")
+        self.phone_number = os.getenv("LOAD_TEST_PHONE_NUMBER", "12897682684")
+        self.template_group = NotifyApiUserTemplateGroup(
+            bulk_email_id=os.getenv("LOAD_TEST_BULK_EMAIL_TEMPLATE_ID", "5ebee3b7-63c0-4052-a8cb-387b818df627"),
+            email_id=os.getenv("LOAD_TEST_EMAIL_TEMPLATE_ID", "a59b313d-8de2-4973-ac2f-66de7ec0b239"),
+            email_with_attachment_id=os.getenv("LOAD_TEST_EMAIL_WITH_ATTACHMENT_TEMPLATE_ID", "a59b313d-8de2-4973-ac2f-66de7ec0b239"),
+            email_with_link_id=os.getenv("LOAD_TEST_EMAIL_WITH_LINK_TEMPLATE_ID", "5ebee3b7-63c0-4052-a8cb-387b818df627"),
+            sms_id=os.getenv("LOAD_TEST_SMS_TEMPLATE_ID", "83d01f06-a818-4134-bd69-ce90a2949280"),
+        )
 
     @task(1)
     def send_email_notifications(self):
-        json = self.__email_json("a59b313d-8de2-4973-ac2f-66de7ec0b239")
+        json = self.__email_json(self.template_group.email_id)
 
         self.client.post("/v2/notifications/email", json=json, headers=self.headers)
 
     @task(2)
     def send_email_with_attachment_notifications(self):
         personalisation = {
-            "application_file": "arbitrary text for this variable",
             "attached_file": {
                 "file": "Q29udGVudCBvZiBBdHRhY2hlZCBmaWxl",
                 "filename": "attached_file.txt",
                 "sending_method": "attach",
             }
         }
-        json = self.__email_json(self.template_id, personalisation)
+        json = self.__email_json(self.template_group.email_with_attachment_id, personalisation)
 
         self.client.post("/v2/notifications/email", json=json, headers=self.headers)
 
@@ -57,7 +71,7 @@ class NotifyApiUser(HttpUser):
                 "sending_method": "link",
             }
         }
-        json = self.__email_json(self.template_id, personalisation)
+        json = self.__email_json(self.template_group.email_with_link_id, personalisation)
 
         self.client.post("/v2/notifications/email", json=json, headers=self.headers)
 
@@ -65,7 +79,7 @@ class NotifyApiUser(HttpUser):
     def send_bulk_notifications(self):
         json = {
             "name": f"My bulk name {datetime.utcnow().isoformat()}",
-            "template_id": self.template_id,
+            "template_id": self.template_group.bulk_email_id,
             "csv": rows_to_csv([["email address", "application_file"], *job_line(self.email, 2)])
         }
 
@@ -74,8 +88,8 @@ class NotifyApiUser(HttpUser):
     @task(16)
     def send_sms_notifications(self):
         json = {
-            "phone_number": "12897682684",
-            "template_id": "83d01f06-a818-4134-bd69-ce90a2949280"
+            "phone_number": self.phone_number,
+            "template_id": self.template_group.sms_id
         }
 
         self.client.post("/v2/notifications/sms", json=json, headers=self.headers)
