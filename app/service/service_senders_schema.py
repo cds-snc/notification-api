dc497b1b2eeb88572b32e7953168bc7145e34b64
Emily Wu <ewu@thoughtworks.com>
2021-09-29 13:06:46 -0400

    [#208] Make rate limit nullable
    Co-authored-by: Ling Tran <ltran@thoughtworks.com>

--- app/service/service_senders_schema.py
+++ app/service/service_senders_schema.py
@@ -35,7 +35,7 @@ add_service_sms_sender_request = {
         "sms_sender": {"type": "string"},
         "is_default": {"type": "boolean"},
         "inbound_number_id": uuid,
-        "rate_limit": {"type": "integer", "minimum": 1}
+        "rate_limit": {"type": ["integer", "null"], "minimum": 1}
     },
     "required": ["sms_sender", "is_default"]
 }
@@ -49,7 +49,7 @@ update_service_sms_sender_request = {
     "properties": {
         "sms_sender": {"type": "string"},
         "is_default": {"type": "boolean"},
-        "rate_limit": {"type": "integer", "minimum": 1},
+        "rate_limit": {"type": ["integer", "null"], "minimum": 1},
         "inbound_number_id": uuid
     }
 }
