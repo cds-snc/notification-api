25892dd4145178c22fd8d9859cea493f9841ad13
Filip Fafara <ffafara@thoughtworks.com>
2021-10-19 12:56:36 -0400

    Add CLI script to send a number of smses from locust instance

---
+++ load_testing/send_sms_cli.py
@@ -0,0 +1,35 @@
+import click
+import boto3
+from notifications_python_client.notifications import NotificationsAPIClient
+
+
+def read_from_ssm(key: str) -> str:
+    boto_client = boto3.client('ssm')
+    resp = boto_client.get_parameter(
+        Name=f"/utility/locust/perf/{key}",
+        WithDecryption=True
+    )
+    return resp["Parameter"]["Value"]
+
+
+@click.command()
+@click.option('--count', default=1, help='Number of messages to send.')
+@click.option('--phone', help='Phone number to send messages to')
+def send_sms(count, phone):
+    sms_template_id = read_from_ssm('sms_template_id')
+    service_id = read_from_ssm('service_id')
+    sms_sender_id = read_from_ssm('sms_sender_id')
+    _api_key = read_from_ssm('api_key')
+    api_key = f'some_key-{service_id}-{_api_key}'
+
+    notifications_client = NotificationsAPIClient(api_key)
+    for x in range(count):
+        notifications_client.send_sms_notification(
+            phone_number=phone,
+            template_id=sms_template_id,
+            sms_sender_id=sms_sender_id
+        )
+
+
+if __name__ == '__main__':
+    send_sms()
