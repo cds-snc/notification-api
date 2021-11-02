7201bd1fd1c84849bc9a0a17617ee37bb2ef9241
Phil Herbert <pherbert@thoughtworks.com>
2021-11-02 14:45:51 -0400

    [#574] copies Store from CDS'es document-download-api
    and renames "document" to "attachment"
    and adds type hints
    
    see https://github.com/cds-snc/notification-document-download-api

---
+++ app/attachments/store.py
@@ -0,0 +1,84 @@
+import os
+import uuid
+from typing_extensions import TypedDict, Literal
+
+import boto3
+from botocore.exceptions import ClientError as BotoClientError
+from botocore.response import StreamingBody
+
+SendingMethod = Literal['attach', 'link']
+
+
+class AttachmentStoreError(Exception):
+    pass
+
+
+class AttachmentStore:
+    def __init__(self, bucket=None):
+        self.s3 = boto3.client("s3")
+        self.bucket = bucket
+
+    def init_app(self, app):
+        self.bucket = app.config['ATTACHMENTS_BUCKET']
+
+    def put(
+            self,
+            service_id: uuid.UUID,
+            attachment_stream,
+            sending_method: SendingMethod,
+            mimetype: str = 'application/pdf'
+    ) -> TypedDict('PutReturn', {'id': uuid.UUID, 'encryption_key': bytes}):
+
+        encryption_key = self.generate_encryption_key()
+        attachment_id = uuid.uuid4()
+
+        self.s3.put_object(
+            Bucket=self.bucket,
+            Key=self.get_attachment_key(service_id, attachment_id, sending_method),
+            Body=attachment_stream,
+            ContentType=mimetype,
+            SSECustomerKey=encryption_key,
+            SSECustomerAlgorithm='AES256'
+        )
+
+        return {
+            'id': attachment_id,
+            'encryption_key': encryption_key
+        }
+
+    def get(
+            self,
+            service_id: uuid.UUID,
+            attachment_id: uuid.UUID,
+            decryption_key: bytes,
+            sending_method: SendingMethod
+    ) -> TypedDict('GetReturn', {'body': StreamingBody, 'mimetype': str, 'size': int}):
+        try:
+            attachment = self.s3.get_object(
+                Bucket=self.bucket,
+                Key=self.get_attachment_key(service_id, attachment_id, sending_method),
+                SSECustomerKey=decryption_key,
+                SSECustomerAlgorithm='AES256'
+            )
+
+        except BotoClientError as e:
+            raise AttachmentStoreError(e.response['Error'])
+
+        return {
+            'body': attachment['Body'],
+            'mimetype': attachment['ContentType'],
+            'size': attachment['ContentLength']
+        }
+
+    @staticmethod
+    def generate_encryption_key() -> bytes:
+        return os.urandom(32)
+
+    @staticmethod
+    def get_attachment_key(
+            service_id: uuid.UUID,
+            attachment_id: uuid.UUID,
+            sending_method: SendingMethod = None
+    ) -> str:
+        key_prefix = 'tmp/' if sending_method == 'attach' else ''
+        return f"{key_prefix}{service_id}/{attachment_id}"
---
+++ tests/app/attachments/test_store.py
@@ -0,0 +1,131 @@
+import uuid
+from unittest import mock
+
+import pytest
+from botocore.exceptions import ClientError as BotoClientError
+
+from tests.conftest import set_config, Matcher
+
+from app.attachments.store import AttachmentStore, AttachmentStoreError
+
+
+@pytest.fixture
+def store(mocker):
+    mock_boto = mocker.patch('app.attachments.store.boto3')
+    mock_boto.client.return_value.get_object.return_value = {
+        'Body': mock.Mock(),
+        'ContentType': 'application/pdf',
+        'ContentLength': 100
+    }
+    store = AttachmentStore(bucket='test-bucket')
+    return store
+
+
+def test_attachment_store_init_app(notify_api, store):
+    with set_config(notify_api, 'ATTACHMENTS_BUCKET', 'test-bucket-2'):
+        store.init_app(notify_api)
+
+    assert store.bucket == 'test-bucket-2'
+
+
+def test_attachment_key_with_uuid(store):
+    service_id = uuid.uuid4()
+    attachment_id = uuid.uuid4()
+
+    assert store.get_attachment_key(service_id, attachment_id) == "{}/{}".format(str(service_id), str(attachment_id))
+
+
+def test_put_attachment(store):
+    service_id = uuid.uuid4()
+    ret = store.put(service_id, mock.Mock(), sending_method='link')
+
+    assert ret == {
+        'id': Matcher('UUID length match', lambda x: len(str(x)) == 36),
+        'encryption_key': Matcher('32 bytes', lambda x: len(x) == 32 and isinstance(x, bytes))
+    }
+
+    store.s3.put_object.assert_called_once_with(
+        Body=mock.ANY,
+        Bucket='test-bucket',
+        ContentType='application/pdf',
+        Key=Matcher(
+            'attachment key',
+            lambda attachment_key:
+                attachment_key.startswith(f"{service_id}/") and len(attachment_key.split('/')[-1]) == 36
+        ),
+        SSECustomerKey=ret['encryption_key'],
+        SSECustomerAlgorithm='AES256'
+    )
+
+
+def test_put_attachment_attach_tmp_dir(store):
+    service_id = uuid.uuid4()
+    ret = store.put(service_id, mock.Mock(), sending_method='attach')
+
+    assert ret == {
+        'id': Matcher('UUID length match', lambda x: len(str(x)) == 36),
+        'encryption_key': Matcher('32 bytes', lambda x: len(x) == 32 and isinstance(x, bytes))
+    }
+
+    store.s3.put_object.assert_called_once_with(
+        Body=mock.ANY,
+        Bucket='test-bucket',
+        ContentType='application/pdf',
+        Key=Matcher(
+            'attachment key',
+            lambda attachment_key:
+                attachment_key.startswith(f"tmp/{service_id}/") and len(attachment_key.split('/')[-1]) == 36
+        ),
+        SSECustomerKey=ret['encryption_key'],
+        SSECustomerAlgorithm='AES256'
+    )
+
+
+def test_get_attachment(store):
+    service_id = uuid.uuid4()
+    attachment_id = uuid.uuid4()
+
+    assert store.get(service_id, attachment_id, bytes(32), sending_method='link') == {
+        'body': mock.ANY,
+        'mimetype': 'application/pdf',
+        'size': 100,
+    }
+
+    store.s3.get_object.assert_called_once_with(
+        Bucket='test-bucket',
+        Key=f"{service_id}/{attachment_id}",
+        SSECustomerAlgorithm='AES256',
+        # 32 null bytes
+        SSECustomerKey=bytes(32),
+    )
+
+
+def test_get_attachment_attach_tmp_dir(store):
+    service_id = uuid.uuid4()
+    attachment_id = uuid.uuid4()
+
+    assert store.get(service_id, attachment_id, bytes(32), sending_method='attach') == {
+        'body': mock.ANY,
+        'mimetype': 'application/pdf',
+        'size': 100,
+    }
+
+    store.s3.get_object.assert_called_once_with(
+        Bucket='test-bucket',
+        Key=f"tmp/{service_id}/{attachment_id}",
+        SSECustomerAlgorithm='AES256',
+        # 32 null bytes
+        SSECustomerKey=bytes(32),
+    )
+
+
+def test_get_attachment_with_boto_error(store):
+    store.s3.get_object = mock.Mock(side_effect=BotoClientError({
+        'Error': {
+            'Code': 'Error code',
+            'Message': 'Error message'
+        }
+    }, 'GetObject'))
+
+    with pytest.raises(AttachmentStoreError):
+        store.get(uuid.uuid4(), uuid.uuid4(), b'0f0f0f', sending_method='link')
