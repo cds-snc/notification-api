f6942299792502a7804ef1698c965dc81457dca0
Ikenna <983625+iokpala@users.noreply.github.com>
2021-11-10 16:47:06 -0400

    Publish performance test results (#1395)
    * Publish performance test results
    
    Within this commit, we have defined a script that setups the
    performance test environment executes the performance test an
    publishes it to S3 bucket.
    
    The run time duration has also been set to 5 minutes to discourage endless
    run of the performance tests
    
    The s3 bucket is managed with a terraform script as defined here [1] and
    will be used to hold the performance test results per environment
    (production or staging).
    
    [1] github.com/cds-snc/notification-terraform/pull/317
    
    * Change to early exit post failure
    
    This changes to an early exit post failure.
    
    * COSMETIC: Change LOAD_TEST to PERF_TEST (#1397)
    
    This is makes an update from LOAD_TEST to PERF_TEST.
    
    * FIXUP: Typograhiccal error
    
    Typo fix
    
    Co-authored-by: Jimmy Royer <jimleroyer@gmail.com>

--- tests-perf/locust/locust-notifications.py
+++ tests-perf/locust/locust-notifications.py
@@ -27,20 +27,20 @@ NotifyApiUserTemplateGroup = make_dataclass('NotifyApiUserTemplateGroup', [
 class NotifyApiUser(HttpUser):
 
     wait_time = constant_pacing(60)
-    host = os.getenv("LOAD_TEST_DOMAIN", "https://api.staging.notification.cdssandbox.xyz")
+    host = os.getenv("PERF_TEST_DOMAIN", "https://api.staging.notification.cdssandbox.xyz")
 
     def __init__(self, *args, **kwargs):
         super(NotifyApiUser, self).__init__(*args, **kwargs)
 
         self.headers = {"Authorization": os.getenv("TEST_AUTH_HEADER")}
-        self.email = os.getenv("LOAD_TEST_EMAIL", "success@simulator.amazonses.com")
-        self.phone_number = os.getenv("LOAD_TEST_PHONE_NUMBER", "16132532222")
+        self.email = os.getenv("PERF_TEST_EMAIL", "success@simulator.amazonses.com")
+        self.phone_number = os.getenv("PERF_TEST_PHONE_NUMBER", "16132532222")
         self.template_group = NotifyApiUserTemplateGroup(
-            bulk_email_id=os.getenv("LOAD_TEST_BULK_EMAIL_TEMPLATE_ID", "5ebee3b7-63c0-4052-a8cb-387b818df627"),
-            email_id=os.getenv("LOAD_TEST_EMAIL_TEMPLATE_ID", "a59b313d-8de2-4973-ac2f-66de7ec0b239"),
-            email_with_attachment_id=os.getenv("LOAD_TEST_EMAIL_WITH_ATTACHMENT_TEMPLATE_ID", "a59b313d-8de2-4973-ac2f-66de7ec0b239"),
-            email_with_link_id=os.getenv("LOAD_TEST_EMAIL_WITH_LINK_TEMPLATE_ID", "5ebee3b7-63c0-4052-a8cb-387b818df627"),
-            sms_id=os.getenv("LOAD_TEST_SMS_TEMPLATE_ID", "83d01f06-a818-4134-bd69-ce90a2949280"),
+            bulk_email_id=os.getenv("PERF_TEST_BULK_EMAIL_TEMPLATE_ID", "5ebee3b7-63c0-4052-a8cb-387b818df627"),
+            email_id=os.getenv("PERF_TEST_EMAIL_TEMPLATE_ID", "a59b313d-8de2-4973-ac2f-66de7ec0b239"),
+            email_with_attachment_id=os.getenv("PERF_TEST_EMAIL_WITH_ATTACHMENT_TEMPLATE_ID", "a59b313d-8de2-4973-ac2f-66de7ec0b239"),
+            email_with_link_id=os.getenv("PERF_TEST_EMAIL_WITH_LINK_TEMPLATE_ID", "5ebee3b7-63c0-4052-a8cb-387b818df627"),
+            sms_id=os.getenv("PERF_TEST_SMS_TEMPLATE_ID", "83d01f06-a818-4134-bd69-ce90a2949280"),
         )
 
     @task(16)
