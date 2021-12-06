dd0e31e9d95fb601a6a22ba91a233d6c27ac3e6a
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2021-12-06 13:42:17 -0500

    fix

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -7,11 +7,12 @@ from notifications_python_client.notifications import NotificationsAPIClient
 
 API_KEY = os.getenv("TF_VAR_heartbeat_api_key")
 # BASE_URL is a list of api urls - k8s and lambda.
-BASE_URL = os.getenv("TF_VAR_heartbeat_base_url", [])
+BASE_URL = os.getenv("TF_VAR_heartbeat_base_url")
 EMAIL_ADDRESS = "success@simulator.amazonses.com"
 TEMPLATE_ID = os.getenv("TF_VAR_heartbeat_template_id")
 
 if __name__ == "__main__":
-    for base_url in BASE_URL:
-        notifications_client = NotificationsAPIClient(API_KEY, base_url=base_url)
-        response = notifications_client.send_email_notification(email_address=EMAIL_ADDRESS, template_id=TEMPLATE_ID)
+    if BASE_URL:
+        for base_url in BASE_URL:
+            notifications_client = NotificationsAPIClient(API_KEY, base_url=base_url)
+            response = notifications_client.send_email_notification(email_address=EMAIL_ADDRESS, template_id=TEMPLATE_ID)
