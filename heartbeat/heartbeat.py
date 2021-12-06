6b1af33e0e03ece0f3c96a238035ea2845ae9ed9
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2021-12-06 12:47:15 -0500

    Change code to take list of base_url

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -6,11 +6,12 @@ import os
 from notifications_python_client.notifications import NotificationsAPIClient
 
 API_KEY = os.getenv("TF_VAR_heartbeat_api_key")
-BASE_URL = os.getenv("TF_VAR_base_url")
+# BASE_URL is a list of api urls - k8s and lambda.
+BASE_URL = os.getenv("TF_VAR_heartbeat_base_url")
 EMAIL_ADDRESS = "success@simulator.amazonses.com"
-TEMPLATE_ID = os.getenv("TF_VAR_template_id")
-
-notifications_client = NotificationsAPIClient(API_KEY, base_url=BASE_URL)
+TEMPLATE_ID = os.getenv("TF_VAR_heartbeat_template_id")
 
 if __name__ == "__main__":
-    response = notifications_client.send_email_notification(email_address=EMAIL_ADDRESS, template_id=TEMPLATE_ID)
+    for base_url in BASE_URL:
+        notifications_client = NotificationsAPIClient(API_KEY, base_url=base_url)
+        response = notifications_client.send_email_notification(email_address=EMAIL_ADDRESS, template_id=TEMPLATE_ID)
