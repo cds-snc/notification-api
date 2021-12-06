ee46ce0ddd9715e0dc940b3e0e2553ec10e52f0c
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2021-12-06 17:04:10 -0500

    PR changes

--- bin/execute_heartbeat.sh
+++ bin/execute_heartbeat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python heartbeat/heartbeat.py
+python heartbeat.py
--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -2,17 +2,23 @@
 Code to keep the lambda function alive.
 """
 import os
+import uuid
 
 from notifications_python_client.notifications import NotificationsAPIClient
 
-API_KEY = os.getenv("TF_VAR_heartbeat_api_key")
+API_KEY: str = os.getenv("TF_VAR_heartbeat_api_key")
 # BASE_URL is a list of api urls - k8s and lambda.
-BASE_URL = os.getenv("TF_VAR_heartbeat_base_url")
+BASE_URL: [] = os.getenv("TF_VAR_heartbeat_base_url")
 EMAIL_ADDRESS = "success@simulator.amazonses.com"
-TEMPLATE_ID = os.getenv("TF_VAR_heartbeat_template_id")
+TEMPLATE_ID: uuid.UUID = os.getenv("TF_VAR_heartbeat_template_id")
 
 if __name__ == "__main__":
-    if BASE_URL:
-        for base_url in BASE_URL:
-            notifications_client = NotificationsAPIClient(API_KEY, base_url=base_url)
-            response = notifications_client.send_email_notification(email_address=EMAIL_ADDRESS, template_id=TEMPLATE_ID)
+    if not BASE_URL:
+        print("Variable BASE_URL is missing")
+    if not API_KEY:
+        print("Variable API_KEY is missing")
+    if not TEMPLATE_ID:
+        print("Variable TEMPLATE_ID is missing")
+    for base_url in BASE_URL:
+        notifications_client = NotificationsAPIClient(API_KEY, base_url=base_url)
+        response = notifications_client.send_email_notification(email_address=EMAIL_ADDRESS, template_id=TEMPLATE_ID)
