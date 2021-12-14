b6cf09a4072a94e5776cd1d0e688e537f3f881e8
Jimmy Royer <jimleroyer@gmail.com>
2021-12-14 18:05:09 -0500

    Added template ID on deleted template validation error msg (#1417)
    * Added template ID on deleted template validation error msg
    
    * Fix test
    
    * Fix test
    
    * Fix test
    
    * Fix test

--- tests/app/notifications/test_validators.py
+++ tests/app/notifications/test_validators.py
@@ -269,8 +269,8 @@ def test_check_template_is_active_fails(sample_template):
     with pytest.raises(BadRequestError) as e:
         check_template_is_active(sample_template)
     assert e.value.status_code == 400
-    assert e.value.message == "Template has been deleted"
-    assert e.value.fields == [{"template": "Template has been deleted"}]
+    assert e.value.message == f"Template {sample_template.id} has been deleted"
+    assert e.value.fields == [{"template": f"Template {sample_template.id} has been deleted"}]
 
 
 @pytest.mark.parametrize("key_type", ["test", "normal"])
