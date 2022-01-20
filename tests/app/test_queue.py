82539cf50bb01617dc6cfa372a4d7e10e1c55af4
Jimmy Royer <jimleroyer@gmail.com>
2022-01-19 20:22:30 -0500

    Trying out pytest-mock-resources with redis

---
+++ tests/app/test_queue.py
@@ -0,0 +1,18 @@
+from pytest_mock_resources import create_redis_fixture
+
+redis = create_redis_fixture()
+
+def test_pytest_mock_redis(redis):
+    colorSet = "Colors"
+    redis.sadd(colorSet, "Red")
+    redis.sadd(colorSet, "Orange")
+    redis.sadd(colorSet, "Yellow")
+    redis.sadd(colorSet, "Green")
+    redis.sadd(colorSet, "Blue")
+    redis.sadd(colorSet, "Indigo")
+    redis.sadd(colorSet, "violet")
+
+    print("Cardinality of the Redis set:")
+    print(redis.scard(colorSet))
+    print("Contents of the Redis set:")
+    print(redis.smembers(colorSet))
