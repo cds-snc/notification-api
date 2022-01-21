ab0820f55349c5dbac5b545aef1fd63bcadd0da5
Jimmy Royer <jimleroyer@gmail.com>
2022-01-21 15:19:18 -0500

    Removed test around pytest_mock_resource

--- tests/app/test_queue.py
+++ tests/app/test_queue.py
@@ -13,21 +13,6 @@ redis = create_redis_fixture()
 
 
 class TestRedisQueue:
-    def test_pytest_mock_redis(self, redis):
-        colorSet = "Colors"
-        redis.sadd(colorSet, "Red")
-        redis.sadd(colorSet, "Orange")
-        redis.sadd(colorSet, "Yellow")
-        redis.sadd(colorSet, "Green")
-        redis.sadd(colorSet, "Blue")
-        redis.sadd(colorSet, "Indigo")
-        redis.sadd(colorSet, "violet")
-
-        print("Cardinality of the Redis set:")
-        print(redis.scard(colorSet))
-        print("Contents of the Redis set:")
-        print(redis.smembers(colorSet))
-
     def test_polling_messages_from_queue(self, redis):
         pass
 
