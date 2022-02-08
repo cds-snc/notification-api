192358996cd5183e7db3437be347fa99c5713fd3
Stephen Astels <sastels@gmail.com>
2022-02-08 10:07:18 -0500

    cleanup

--- app/queue.py
+++ app/queue.py
@@ -114,10 +114,6 @@ class RedisQueue(Queue):
         if expired:
             current_app.logger.warning(f"Moved inflights {expired} back to inbox {self._inbox}")
 
-        # for key in self._redis_client.scan_iter(f"{Buffer.IN_FLIGHT.inflight_prefix(self._suffix)}*"):
-        #     if self._redis_client.object("idletime", key) > self._expire_inflight_after_seconds:
-        #         self.__move_from_inflight(key)
-
     def acknowledge(self, receipt: UUID):
         inflight_name = Buffer.IN_FLIGHT.inflight_name(receipt, self._suffix)
         self._redis_client.delete(inflight_name)
@@ -129,11 +125,6 @@ class RedisQueue(Queue):
         results = self.scripts[self.LUA_MOVE_TO_INFLIGHT](args=[self._inbox, in_flight_key, count])
         decoded = [result.decode("utf-8") for result in results]
         return decoded
-
-    # def __move_from_inflight(self, in_flight_key: str):
-    #     self.scripts[self.LUA_MOVE_FROM_INFLIGHT](args=[in_flight_key, self._inbox])
-    #     current_app.logger.warning(f"Moved inflight {in_flight_key} back to inbox {self._inbox}")
-    #     self._redis_client.delete(in_flight_key)
 
     def __register_scripts(self):
         self.scripts[self.LUA_MOVE_TO_INFLIGHT] = self._redis_client.register_script(
@@ -162,32 +153,6 @@ class RedisQueue(Queue):
             return all
             """
         )
-
-        # self.scripts[self.LUA_MOVE_FROM_INFLIGHT] = self._redis_client.register_script(
-        #     """
-        #     local DEFAULT_CHUNK = 99
-
-        #     local source        = ARGV[1]
-        #     local destination   = ARGV[2]
-        #     local count         = tonumber(redis.call("LLEN", source))
-
-        #     local chunk_size    = math.min(math.max(0, count-1), DEFAULT_CHUNK)
-        #     local current       = 0
-        #     local all           = {}
-
-        #     while current < count do
-        #         local elements = redis.call("LRANGE", source, 0, chunk_size)
-        #         redis.call("LPUSH", destination, unpack(elements))
-        #         redis.call("LTRIM", source, chunk_size+1, -1)
-        #         for i=1,#elements do all[#all+1] = elements[i] end
-
-        #         current    = current + chunk_size+1
-        #         chunk_size = math.min((count-1) - current, DEFAULT_CHUNK)
-        #     end
-
-        #     return all
-        #     """
-        # )
 
         self.scripts[self.LUA_EXPIRE_INFLIGHTS] = self._redis_client.register_script(
             """
