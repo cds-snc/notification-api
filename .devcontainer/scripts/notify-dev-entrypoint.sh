8a44e741fc5d596edf5925016f3a848a7bfbbab1
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-09 20:50:24 +0000

    fix: creation of devcontainer aliases

--- .devcontainer/scripts/notify-dev-entrypoint.sh
+++ .devcontainer/scripts/notify-dev-entrypoint.sh
@@ -7,18 +7,14 @@ set -ex
 # tools and the filesystem mount enabled should be located here. 
 ###################################################################
 
-# We want to enable broadcast message which by default is disabled.
-sed '/mesg/d' ~/.profile > ~/.profile.bak && mv ~/.profile.bak ~/.profile
-echo -e "\ntest -t 0 && mesg n" >> ~/.profile
-
 # Define aliases
-echo -e "\n\n# User's Aliases" >> ~/.profile
-echo -e "alias fd=fdfind" >> ~/.profile
-echo -e "alias l='ls -al --color'" >> ~/.profile
-echo -e "alias ls='exa'" >> ~/.profile
-echo -e "alias l='exa -alh'" >> ~/.profile
-echo -e "alias ll='exa -alh@ --git'" >> ~/.profile
-echo -e "alias lt='exa -al -T -L 2'" >> ~/.profile
+echo -e "\n\n# User's Aliases" >> ~/.bashrc
+echo -e "alias fd=fdfind" >> ~/.bashrc
+echo -e "alias l='ls -al --color'" >> ~/.bashrc
+echo -e "alias ls='exa'" >> ~/.bashrc
+echo -e "alias l='exa -alh'" >> ~/.bashrc
+echo -e "alias ll='exa -alh@ --git'" >> ~/.bashrc
+echo -e "alias lt='exa -al -T -L 2'" >> ~/.bashrc
 
 cd /workspace 
 
@@ -32,5 +28,3 @@ pip3 install -r requirements_for_test.txt
 
 # Upgrade schema of the notification_api database.
 flask db upgrade
-
-wall "The dev container entrypoint setup is complete!"
