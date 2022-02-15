0f42c9068ed832facd690bce2abda28227ffff2d
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-02-15 16:32:22 -0500

    wip: debugging test mocks

--- app/aws/metrics.py
+++ app/aws/metrics.py
@@ -3,16 +3,19 @@ from aws_embedded_metrics.config import get_config
 from botocore.exceptions import ClientError
 from flask import current_app
 
+from app.config import Config
 from app.queue import RedisQueue
 
-Config = get_config()
-Config.service_name = "BatchSaving"
-Config.service_type = "Redis"
-Config.log_group_name = "BatchSaving"
+Metrics_Config = get_config()
+Metrics_Config.agent_endpoint = Config.CLOUDWATCH_AGENT_ENDPOINT
+Metrics_Config.service_name = "BatchSaving"
+Metrics_Config.service_type = "Redis"
+Metrics_Config.log_group_name = "BatchSaving"
 
 
 @metric_scope
 def put_batch_saving_metric(queue: RedisQueue, metrics):
+    import pdb; pdb.set_trace()
     try:
         metrics.set_namespace("BatchSaving")
         metrics.set_dimensions({"inbox": queue._inbox})
--- tests/app/aws/test_metrics.py
+++ tests/app/aws/test_metrics.py
@@ -1,12 +1,20 @@
 
+from unittest import mock
+metrics_logger_mock = mock.patch("aws_embedded_metrics.logger.metrics_logger_factory.create_metrics_logger").start()
+metric_scope_mock = mock.patch("app.aws.metrics.metric_scope").start()
 
+# metrics_logger_mock = mock.patch("aws_embedded_metrics.logger").start()
 from app.aws.metrics import put_batch_saving_metric
 
+
 def test_put_batch_metric(mocker):
-    metric_scope_mock = mocker.patch("app.aws.metrics.metric_scope")
     redis_queue = mocker.MagicMock()
+    # metrics_logger_mock = mock.patch("aws_embedded_metrics.logger.metrics_logger.MetricsLogger").start()
+    # metrics_logger_mock = mocker.patch("aws_embedded_metrics.logger")
     redis_queue._inbox = "foo"
     redis_queue._expire_inflight_after_seconds = 100
     put_batch_saving_metric(redis_queue)
-    metric_scope_mock.set_dimensions().assert_called_with({"inbox": "foo"})
-    metric_scope_mock.put_metric().assert_called_with("published", 1, "Count")
+    import pdb; pdb.set_trace()
+    # print(metric_scope_mock.mock_calls)
+    metrics_logger_mock.set_dimensions().assert_called_with({"inbox": "foo"})
+    metrics_logger_mock.put_metric().assert_called_with("published", 1, "Count")
