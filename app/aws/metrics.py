405268fde9543eacd4e2ef67bbe43dcf0afae317
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2022-02-21 16:26:04 -0500

    feat: scaffold custom metrics (#1465)
    * wip: publish custom batch metrics to cloudwatch
    
    * wip: debugging test mocks
    
    * fix: tests working mock
    
    * chore: modify how type checking is setup
    
    * chore: remove unnecessary pass
    
    * wip
    
    * Add tests for metrics usage and functionality
    
    * chore: pin Werkzeug to prevent LocalProxy breaking change
    
    * chore: updated requirements file
    
    * chore: pin marksafe due to new version introducing breaking changes
    
    * Moved tests into its own class
    
    Co-authored-by: Jumana Bahrainwala <jzbahrai@uwaterloo.ca>

---
+++ app/aws/metrics.py
@@ -0,0 +1,99 @@
+from __future__ import annotations  # PEP 563 -- Postponed Evaluation of Annotations
+
+from typing import TYPE_CHECKING
+
+from aws_embedded_metrics import metric_scope  # type: ignore
+from aws_embedded_metrics.config import get_config  # type: ignore
+from botocore.exceptions import ClientError
+from flask import current_app
+
+from app.config import Config
+
+if TYPE_CHECKING:  # A special Python 3 constant that is assumed to be True by 3rd party static type checkers
+    from aws_embedded_metrics.logger.metrics_logger import MetricsLogger  # type: ignore
+
+    from app.queue import RedisQueue
+
+metrics_config = get_config()
+metrics_config.agent_endpoint = Config.CLOUDWATCH_AGENT_ENDPOINT
+metrics_config.service_name = "BatchSaving"
+metrics_config.service_type = "Redis"
+metrics_config.log_group_name = "BatchSaving"
+
+
+@metric_scope
+def put_batch_saving_metric(queue: RedisQueue, count: int, metrics: MetricsLogger):
+    """
+    Metric to calculate how many items are put in an INBOX
+
+    Args:
+        queue (RedisQueue): Implementation of queue.RedisQueue for BatchSaving
+        count (int): default: 1, count of an item added to the INBOX.
+        metrics (MetricsLogger): Submit metric to cloudwatch
+    """
+    try:
+        metrics.set_namespace("BatchSaving")
+        metrics.set_dimensions({"list_name": queue._inbox})
+        metrics.put_metric("published", count, "Count")
+        metrics.set_property("expiry", queue._expire_inflight_after_seconds)
+    except ClientError as e:
+        message = "Error sending CloudWatch Metric: {}".format(e)
+        current_app.logger.exception(message)
+    return
+
+
+@metric_scope
+def put_batch_saving_in_flight_metric(count: int, metrics: MetricsLogger):
+    """
+    Metric to calculate how many inflight lists have been created
+
+    Args:
+        count (int): default: 1, count of an inflight list created.
+        metrics (MetricsLogger): Submit metric to cloudwatch
+    """
+    try:
+        metrics.set_namespace("BatchSaving")
+        metrics.put_metric("inflight", count, "Count")
+        metrics.set_dimensions({"created": True})
+    except ClientError as e:
+        message = "Error sending CloudWatch Metric: {}".format(e)
+        current_app.logger.exception(message)
+    return
+
+
+@metric_scope
+def put_batch_saving_inflight_processed(count: int, metrics: MetricsLogger):
+    """
+    Metric to calculate how many inflight lists have been processed.
+
+    Args:
+        count (int): default: 1, count of an inflight list created.
+        metrics (MetricsLogger): Submit metric to cloudwatch
+    """
+    try:
+        metrics.set_namespace("BatchSaving")
+        metrics.put_metric("inflight", count, "Count")
+        metrics.set_dimensions({"acknowledged": True})
+    except ClientError as e:
+        message = "Error sending CloudWatch Metric: {}".format(e)
+        current_app.logger.exception(message)
+    return
+
+
+@metric_scope
+def put_batch_saving_expiry_metric(count: int, metrics: MetricsLogger):
+    """
+    Metric to calculate how many inflight list have not been processed and instead
+    sent back to the inbox.
+
+    Args:
+        count (int): Number of inlfight lists sent to inbox
+        metrics (MetricsLogger): Submit metric to cloudwatch
+    """
+    try:
+        metrics.set_namespace("BatchSaving")
+        metrics.put_metric("expired", count, "Count")
+    except ClientError as e:
+        message = "Error sending CloudWatch Metric: {}".format(e)
+        current_app.logger.exception(message)
+    return
