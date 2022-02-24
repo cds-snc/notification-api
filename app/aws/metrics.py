6a8946cd9067cc1ba564be61bf19132c0690bb7d
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2022-02-24 16:31:03 -0500

    chore: actually disable instead of relying on malformed payloads (#1490)
    * chore: actually disable instead of relying on malformed payloads
    
    * chore: disable tests for disabled feature

--- app/aws/metrics.py
+++ app/aws/metrics.py
@@ -6,20 +6,12 @@ from aws_embedded_metrics.config import get_config  # type: ignore
 from botocore.exceptions import ClientError
 from flask import current_app
 
-from app.config import Config
-
 if TYPE_CHECKING:  # A special Python 3 constant that is assumed to be True by 3rd party static type checkers
     from app.aws.metrics_logger import MetricsLogger
     from app.queue import RedisQueue
 
 metrics_config = get_config()
-metrics_config.agent_endpoint = Config.CLOUDWATCH_AGENT_ENDPOINT
-metrics_config.service_name = "BatchSaving"
-metrics_config.service_type = "Redis"
-metrics_config.log_group_name = "BatchSaving"
-
-if not Config.STATSD_ENABLED:
-    metrics_config.disable_metric_extraction = True
+metrics_config.disable_metric_extraction = True
 
 
 def put_batch_saving_metric(metrics_logger: MetricsLogger, queue: RedisQueue, count: int):
@@ -31,6 +23,8 @@ def put_batch_saving_metric(metrics_logger: MetricsLogger, queue: RedisQueue, co
         count (int): default: 1, count of an item added to the INBOX.
         metrics (MetricsLogger): Submit metric to cloudwatch
     """
+    if metrics_config.disable_metric_extraction:
+        return
     try:
         metrics_logger.set_namespace("NotificationCanadaCa")
         metrics_logger.put_metric("batch_saving_published", count, "Count")
@@ -50,6 +44,8 @@ def put_batch_saving_inflight_metric(metrics_logger: MetricsLogger, count: int):
         count (int): default: 1, count of an inflight list created.
         metrics (MetricsLogger): Submit metric to cloudwatch
     """
+    if metrics_config.disable_metric_extraction:
+        return
     try:
         metrics_logger.set_namespace("NotificationCanadaCa")
         metrics_logger.put_metric("batch_saving_inflight", count, "Count")
@@ -69,6 +65,8 @@ def put_batch_saving_inflight_processed(metrics_logger: MetricsLogger, count: in
         count (int): default: 1, count of an inflight list created.
         metrics (MetricsLogger): Submit metric to cloudwatch
     """
+    if metrics_config.disable_metric_extraction:
+        return
     try:
         metrics_logger.set_namespace("NotificationCanadaCa")
         metrics_logger.put_metric("batch_saving_inflight", count, "Count")
@@ -89,6 +87,8 @@ def put_batch_saving_expiry_metric(metrics_logger, count: int):
         count (int): Number of inlfight lists sent to inbox
         metrics (MetricsLogger): Submit metric to cloudwatch
     """
+    if metrics_config.disable_metric_extraction:
+        return
     try:
         metrics_logger.set_namespace("NotificationCanadaCa")
         metrics_logger.put_metric("batch_saving_inflight", count, "Count")
