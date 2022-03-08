0b2fc7f7655359d0fa7957bfd1b44b6245821d81
Pat Heard <patrick.heard@cds-snc.ca>
2022-03-08 15:16:20 +0000

    feat: add config logging during app initialize
    This logging can be used to troubleshoot app init problems and confirm
    that future changes to how environment variables are set are working
    as expected.
    
    As part of this change, a list of sensitive config keys has been
    defined to avoid leaking secrets in the app logs.

--- app/__init__.py
+++ app/__init__.py
@@ -94,6 +94,10 @@ def create_app(application, config=None):
     register_blueprint(application)
     register_v2_blueprints(application)
 
+    # Log the application configuration
+    config_values = config.get_config(config.get_sensitive_config())
+    application.logger.info(f"Notify config: {config_values}")
+
     # avoid circular imports by importing this file later
     from app.commands import setup_commands
 
