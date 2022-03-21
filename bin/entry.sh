70f45f4d57395e52c811c565a8548255633d7264
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-03-21 16:32:16 -0400

    feat: retrieve lambda .env from parameter store

--- bin/entry.sh
+++ bin/entry.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
+    aws ssm get-parameters --region ca-central-1 --with-decryption --names ENVIRONMENT_VARIABLES --query 'Parameters[*].Value' --output text > "${TASK_ROOT}/.env"
     exec /usr/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric $1
 else
     exec /usr/local/bin/python -m awslambdaric $1
