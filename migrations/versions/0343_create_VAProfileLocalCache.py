f453ffd468f3b7ff7fff5550b677d268d42b5be5
David J. Kalbfleisch <1.21e9W@protonmail.com>
2022-04-07 12:04:40 -0400

    Create the data model VAProfileLocalCache to support comp & pen integration.  Implemented a method to support creating migrations on a container.  Created migration 0343 to support these changes.  Closes #612.

---
+++ migrations/versions/0343_create_VAProfileLocalCache.py
@@ -0,0 +1,28 @@
+"""
+Revision ID: 0343_create_VAProfileLocalCache
+Revises: 0342_add_reply_to_field
+Create Date: 2022-04-06 21:54:43.488085
+"""
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '0343_create_VAProfileLocalCache'
+down_revision = '0342_add_reply_to_field'
+
+
+def upgrade():
+    op.create_table(
+        'va_profile_local_cache',
+        sa.Column('id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column('mpi_icn', sa.String(length=29), nullable=False),
+        sa.Column('va_profile_id', sa.Integer(), nullable=False),
+        sa.Column('va_profile_item_id', sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+    op.drop_table('va_profile_local_cache')
+
