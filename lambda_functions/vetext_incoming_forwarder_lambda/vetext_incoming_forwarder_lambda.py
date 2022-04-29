556e281321d9ce95649cff6d6d29c32a25d7f7e0
jacob cheriathundam <jacobcheriathundam@gmail.com>
2022-04-29 10:55:12 -0400

    added comments and formatting changes

--- lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
+++ lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
@@ -1,36 +1,51 @@
+"""This module is used to transfer incoming twilio requests to a Vetext endpoint"""
+
 import json
 import http.client
-import os 
-import boto3
-import pprint
+import os
 import urllib.parse
+import boto3
 
-def lambda_handler(event, context):
-    connection = http.client.HTTPSConnection(os.environ['vetext_api_endpoint_domain'])
-    headers = {'Content-type': 'application/json', 'Authorization': os.environ['vetext_api_endpoint_auth']}
-    
-    pp = pprint.PrettyPrinter(indent=4)
-    
-    eventBody = urllib.parse.parse_qs(event["body"]) 
-    
-    pp.pprint(eventBody)
- 
+def lambda_handler(event: any, context: any):
+    """this method takes in an event passed in by either an alb or sqs.
+        @param: event   -  contains data pertaining to an incoming sms from Twilio
+        @param: context -  contains information regarding information
+            regarding what triggered the lambda (context.invoked_function_arn).
+    """
+
+    connection = http.client.HTTPSConnection(
+        os.environ['vetext_api_endpoint_domain'])
+
+    # Authorization is basic token authentication that is stored in environment.
+    headers = {
+        'Content-type': 'application/json',
+        'Authorization': os.environ['vetext_api_endpoint_auth']
+    }
+
+    # event["body"] is a url-encoded string.
+    #   parse_qs converts url-encoded strings to dictionary objects
+    event_body = urllib.parse.parse_qs(event["body"])
+
     body = {
-        "accountSid": eventBody.get("AccountSid", ""),
-        "messageSid": eventBody.get("MessageSid", ""),
-    	"messagingServiceSid": "",
-    	"to": eventBody.get("To", ""),
-    	"from": eventBody.get("From", ""),
-    	"messageStatus": eventBody.get("SmsStatus", ""),
-        "body": eventBody.get("Body", "")
-        }
-    
+        "accountSid": event_body.get("AccountSid", ""),
+        "messageSid": event_body.get("MessageSid", ""),
+        "messagingServiceSid": "",
+        "to": event_body.get("To", ""),
+        "from": event_body.get("From", ""),
+        "messageStatus": event_body.get("SmsStatus", ""),
+        "body": event_body.get("Body", "")
+    }
+
     json_data = json.dumps(body)
-    
-    connection.request('POST', os.environ['vetext_api_endpoint_path'], json_data, headers)
+
+    connection.request(
+        'POST',
+        os.environ['vetext_api_endpoint_path'],
+        json_data,
+        headers)
 
     response = connection.getresponse()
-    
+
     if response.status == 200:
         return {
             'statusCode': 200,
@@ -39,15 +54,15 @@ def lambda_handler(event, context):
     else:
         sqs = boto3.client('sqs')
         queue_url = os.environ['vetext_request_drop_sqs_url']
-        
+
         queue_msg = json.dumps(event)
         queue_msg_attrs = {
-                            'source': {
-                                'DataType': 'String',
-                                'StringValue': 'twilio'
-                            }
-                          }
-        
+            'source': {
+                'DataType': 'String',
+                'StringValue': 'twilio'
+            }
+        }
+
         sqs.send_message(QueueUrl=queue_url,
                          MessageAttributes=queue_msg_attrs,
                          MessageBody=queue_msg)
