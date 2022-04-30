7d57da8c3bef807ee086b2aaf48a88335441caa9
jacob cheriathundam <jacobcheriathundam@gmail.com>
2022-04-30 15:42:09 -0400

    refactored code into methods and

--- lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
+++ lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
@@ -15,6 +15,99 @@ def lambda_handler(event: any, context: any):
     """
 
     try:
+        print(event)
+
+        # Determine if the invoker of the lambda is SQS or ALB
+        #   SQS will submit batches of records so there is potential for multiple events to be processed
+        #   ALB will submit a single request but to simplify code, it will also return an array of event bodies
+        if (event["requestContext"] and event["requestContext"]["elb"]):
+            print("alb invocation")
+            event_bodies = get_body_from_alb_invocation(event)            
+        elif event.Records and event.Records[0].eventSource == 'aws:sqs':
+            print("sqs invoication")
+            event_bodies = get_body_from_sqs_invocation(event)
+        else:
+            return{
+                'statusCode':400
+            }
+
+        print(event_bodies)
+        
+        responses = []
+
+        for event_body in event_bodies:            
+            response = make_vetext_request(event_body)
+
+            if response.status != 200:
+                push_to_sqs(event)
+
+            print(response.read().decode())
+
+            responses.append(response)
+
+        print(responses)
+    except KeyError as e:
+        # Handle failed env variable
+        print(f'Failed to find environmental variable: {e}')
+        # Place request on SQS for processing after environment variable issue is resolved
+        push_to_sqs(event)
+
+        return {
+            'statusCode': 424
+        }
+    except http.client.HTTPException as e:
+        # Handle failed http request to vetext endpoint
+        print(f'Failure with http connection or request: {e}')
+        # Place request on SQS for processing after environment variable issue is resolved
+        push_to_sqs(event)
+    except Exception as e:
+        # Place request on dead letter queue so that it can be analyzed 
+        #   for potential processing at a later time
+        print(f'Unknown Failure: {e}')
+        push_to_sqs(event)
+    
+    return {
+        'statusCode': 200
+    }
+
+def get_body_from_sqs_invocation(event):
+    event_bodies = []
+
+    try: 
+        for record in event.Records:
+            # record is a sqs event that contains a body
+            # body is an alb request that failed in an initial request
+            # event is a json document with a body attribute that contains
+            #   the payload of the twilio webhook
+            # event["body"] is a base 64 encoded string
+            # parse_qsl converts url-encoded strings to array of tuple objects
+            # event_body takes the array of tuples and creates a dictionary
+            event_body_decoded = parse_qsl(b64decode(record["body"]).decode('utf-8'))
+            event_body = dict(event_body_decoded)
+            event_bodies.append(event_body)
+
+        return event_bodies
+    except:
+        raise
+
+def get_body_from_alb_invocation(event):
+    event_bodies = []
+
+    try: 
+        # event is a json document with a body attribute that contains
+        #   the payload of the twilio webhook
+        # event["body"] is a base 64 encoded string
+        # parse_qsl converts url-encoded strings to array of tuple objects
+        # event_body takes the array of tuples and creates a dictionary
+        event_body_decoded = parse_qsl(b64decode(event["body"]).decode('utf-8'))
+        event_bodies.append(dict(event_body_decoded))
+
+        return event_bodies
+    except:
+        raise
+
+def make_vetext_request(request_body):
+    try:
         connection = http.client.HTTPSConnection(os.environ.get('vetext_api_endpoint_domain'))
 
         # Authorization is basic token authentication that is stored in environment.
@@ -23,23 +116,15 @@ def lambda_handler(event: any, context: any):
             'Authorization': os.environ.get('vetext_api_endpoint_auth')
         }
 
-        # event is a json document with a body attribute that contains
-        #   the payload of the twilio webhook
-        # event["body"] is a base 64 encoded string
-        # parse_qsl converts url-encoded strings to array of tuple objects
-        # event_body takes the array of tuples and creates a dictionary
-        event_body_decoded = parse_qsl(b64decode(event["body"]).decode('utf-8'))
-        event_body = dict(event_body_decoded)
-
         body = {
-            "accountSid": event_body.get("AccountSid", ""),
-            "messageSid": event_body.get("MessageSid", ""),
-            "messagingServiceSid": "",
-            "to": event_body.get("To", ""),
-            "from": event_body.get("From", ""),
-            "messageStatus": event_body.get("SmsStatus", ""),
-            "body": event_body.get("Body", "")
-        }
+                "accountSid": request_body.get("AccountSid", ""),
+                "messageSid": request_body.get("MessageSid", ""),
+                "messagingServiceSid": "",
+                "to": request_body.get("To", ""),
+                "from": request_body.get("From", ""),
+                "messageStatus": request_body.get("SmsStatus", ""),
+                "body": request_body.get("Body", "")
+            }
 
         json_data = json.dumps(body)
 
@@ -51,41 +136,26 @@ def lambda_handler(event: any, context: any):
 
         response = connection.getresponse()
 
-        if response.status != 200:
-            push_to_sqs(event)
-
-        return {
-            'statusCode': response.status,
-            'body': response.read().decode()
-        }
-    except KeyError as e:
-        print(f'Failed to find environmental variable: {e}')
-        # Handle failed env variable
-        return {
-            'statusCode': 424,
-            'body': "Missing env variable"
-        }
-    except http.client.HTTPException as e:
-        print(f'Failure with http connection or request: {e}')
-        push_to_sqs(event)
-        return {
-            'statusCode': 200,
-            'body': "Moved to queue"
-        }
+        return response
+    except:
+        raise
 
 def push_to_sqs(event):
-    """Places event on queue to be retried at a later time"""
-    sqs = boto3.client('sqs')
-    queue_url = os.environ.get('vetext_request_drop_sqs_url')
+    try:
+        """Places event on queue to be retried at a later time"""
+        sqs = boto3.client('sqs')
+        queue_url = os.environ.get('vetext_request_drop_sqs_url')
 
-    queue_msg = json.dumps(event)
-    queue_msg_attrs = {
-        'source': {
-            'DataType': 'String',
-            'StringValue': 'twilio'
+        queue_msg = json.dumps(event)
+        queue_msg_attrs = {
+            'source': {
+                'DataType': 'String',
+                'StringValue': 'twilio'
+            }
         }
-    }
 
-    sqs.send_message(QueueUrl=queue_url,
-                    MessageAttributes=queue_msg_attrs,
-                    MessageBody=queue_msg)
+        sqs.send_message(QueueUrl=queue_url,
+                        MessageAttributes=queue_msg_attrs,
+                        MessageBody=queue_msg)
+    except:
+        raise
