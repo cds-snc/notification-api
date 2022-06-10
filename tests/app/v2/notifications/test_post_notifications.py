4e987fa3ead0c381d0432de6d72b205ad819b2c2
Jimmy Royer <jimleroyer@gmail.com>
2022-06-09 20:39:15 -0400

    Silly edge condition mistake'

--- tests/app/v2/notifications/test_post_notifications.py
+++ tests/app/v2/notifications/test_post_notifications.py
@@ -1315,7 +1315,6 @@ def test_post_email_notification_with_personalisation_too_large(
     data = {
         "email_address": sample_email_template_with_placeholders.service.users[0].email_address,
         "template_id": sample_email_template_with_placeholders.id,
-        # "personalisation": {"name": random_sized_content(size=personalisation_size)},
         "personalisation": {"content": random_sized_content(size=personalisation_size)},
         "reference": "reference_from_client",
     }
