ee4e361dfeb614ab67402d65fbb201ad60bc1d2d
Andrew Leith <andrew.leith@cds-snc.ca>
2022-06-14 18:55:01 +0000

    chore(test): fix test that failed in CI but worked locally
    convert result to JSON before comparing instead of doing a string compare

--- tests/app/v2/test_errors.py
+++ tests/app/v2/test_errors.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 from flask import url_for
 from sqlalchemy.exc import DataError
@@ -169,5 +170,5 @@ def test_bad_method(app_for_test):
             assert response.status_code == 405
 
             assert (
-                response.data == b'{\n  "message": "The method is not allowed for the requested URL.", \n  "result": "error"\n}\n'
+                json.loads(response.data) == {"message": "The method is not allowed for the requested URL.", "result": "error"}
             )
