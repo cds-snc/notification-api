1ee52f28d10ddff7c61256a4c8748fd3aa1e8a25
jacob cheriathundam <jacobcheriathundam@gmail.com>
2022-07-19 10:32:06 -0400

    replaced json.loads with dict method for parsing decoded twilio message body

--- lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
+++ lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
@@ -22,7 +22,6 @@ def vetext_incoming_forwarder_lambda_handler(event: any, context: any):
 
     try:
         logger.debug(event)
-
         # Determine if the invoker of the lambda is SQS or ALB
         #   SQS will submit batches of records so there is potential for multiple events to be processed
         #   ALB will submit a single request but to simplify code, it will also return an array of event bodies
@@ -34,8 +33,7 @@ def vetext_incoming_forwarder_lambda_handler(event: any, context: any):
             event_bodies = process_body_from_sqs_invocation(event)
         else:
             logger.error("Invalid Event. Expecting the source of an invocation to be from alb or sqs")
-
-            push_to_sqs(event["body"])
+            logger.error(json.dumps(event))
 
             return{
                 'statusCode': 400
@@ -45,15 +43,26 @@ def vetext_incoming_forwarder_lambda_handler(event: any, context: any):
 
         responses = []
 
-        for event_body in event_bodies:            
-            response = make_vetext_request(event_body)
+        for event_body in event_bodies:       
+            try:      
+                logger.info("Making call to VeText")
+                response = make_vetext_request(event_body)
 
-            if response.status != 200:
-                push_to_sqs(event["body"])
-
-            logger.debug(response.read().decode())
-
-            responses.append(response)
+                if response.status != 200:
+                    logger.info("VeText call failed. Moving event body to failover queue")
+                    push_to_sqs(event_body)
+                
+                responses.append(response)
+            except http.client.HTTPException as e:
+                logger.info("HttpException With Call To VeText")
+                logger.info(json.dumps(event_body))
+                logger.exception(e)                                
+                push_to_sqs(event_body)
+            except Exception as e:
+                logger.info("General Exception With Call to VeText")
+                logger.info(json.dumps(event_body))
+                logger.exception(e)                        
+                push_to_sqs(event_body)
 
         logger.debug(responses)
         
@@ -61,27 +70,18 @@ def vetext_incoming_forwarder_lambda_handler(event: any, context: any):
             'statusCode': 200
         }
     except KeyError as e:
+        logger.info("Key Error")
+        logger.info(json.dumps(event))
         logger.exception(e)
-        # Place request on SQS for processing after environment variable issue is resolved
-        push_to_sqs(event["body"])
-
+        
         return {
             'statusCode': 424
-        }
-    except http.client.HTTPException as e:
-        logger.exception(e)
-        # Place request on SQS for processing after environment variable issue is resolved
-        push_to_sqs(event["body"])
-
-        return{
-            'statusCode':503
-        }
+        }   
     except Exception as e:
+        logger.info("General Exception")
+        logger.info(json.dumps(event))
         logger.exception(e)        
-        # Place request on dead letter queue so that it can be analyzed 
-        #   for potential processing at a later time
-        push_to_sqs(event["body"])
-
+        
         return{
             'statusCode':500
         }
@@ -96,13 +96,9 @@ def process_body_from_sqs_invocation(event):
         # event["body"] is a base 64 encoded string
         # parse_qsl converts url-encoded strings to array of tuple objects
         # event_body takes the array of tuples and creates a dictionary
-        try:
-            event_body_decoded = parse_qsl(b64decode(record["body"]).decode('utf-8'))
-            event_body = dict(event_body_decoded)
-            event_bodies.append(event_body)
-        except:
-            push_to_sqs(record["body"])
-
+        event_body = dict(record["body"])
+        event_bodies.append(event_body)
+    
     return event_bodies
 
 def process_body_from_alb_invocation(event):
@@ -114,10 +110,18 @@ def process_body_from_alb_invocation(event):
     # parse_qsl converts url-encoded strings to array of tuple objects
     # event_body takes the array of tuples and creates a dictionary
     event_body_decoded = parse_qsl(b64decode(event["body"]).decode('utf-8'))
-    event_bodies.append(dict(event_body_decoded))
+    event_body = dict(event_body_decoded)
+    # AddOns are a Twilio feature that are in the dictionary as a 
+    # string that contains escaped json.  To make it accessible for 
+    # processing, we are converting the string to json and re-attaching
+    # it to the dict
+    if 'AddOns' in event_body:
+        addons = json.loads(event_body.pop('AddOns'))
+        event_body['AddOns'] = addons
 
-    return event_bodies
-    
+    event_bodies.append(event_body)
+
+    return event_bodies    
 
 def read_from_ssm(key: str) -> str:
     ssm_client = boto3.client('ssm')
@@ -127,27 +131,29 @@ def read_from_ssm(key: str) -> str:
         WithDecryption=True
     )
 
-    logger.info(response)
+    logger.info("Successfully retrieved SSM Parameter")
 
     return response.get("Parameter", {}).get("Value", '')
 
 def make_vetext_request(request_body):    
+    # We have been directed by the VeText team to ignore SSL validation
+    #   that is why we use the ssl._create_unverified_context method
     connection = http.client.HTTPSConnection(os.environ.get('vetext_api_endpoint_domain'),  context = ssl._create_unverified_context())
 
     # Authorization is basic token authentication that is stored in environment.
-    authToken = read_from_ssm(os.environ.get('vetext_api_auth_ssm_path'))
+    auth_token = read_from_ssm(os.environ.get('vetext_api_auth_ssm_path'))
 
-    logger.info(f'ssm key: {authToken}')
-
+    logger.info("Successfully retrieved Auth Token")
+    
     headers = {
         'Content-type': 'application/json',
-        'Authorization': 'Basic ' + authToken
+        'Authorization': 'Basic ' + auth_token
     }
 
     body = {
             "accountSid": request_body.get("AccountSid", ""),
             "messageSid": request_body.get("MessageSid", ""),
-            "messagingServiceSid": "",
+            "messagingServiceSid": request_body.get("MessagingServiceSid", ""),
             "to": request_body.get("To", ""),
             "from": request_body.get("From", ""),
             "messageStatus": request_body.get("SmsStatus", ""),
@@ -156,22 +162,28 @@ def make_vetext_request(request_body):
 
     json_data = json.dumps(body)
 
+    logger.info("Making POST Request to VeText using: " + os.environ.get('vetext_api_endpoint_domain') + os.environ.get('vetext_api_endpoint_path'))
+    logger.debug("POST body: " + json_data)
+    
     connection.request(
         'POST',
         os.environ.get('vetext_api_endpoint_path'),
         json_data,
         headers)
-
+    
     response = connection.getresponse()
+    
+    logger.info("VeText call complete, with response: " + str(response.status))
+    logger.debug(response.read().decode())
 
     return response    
 
-def push_to_sqs(event):
-    """Places event on queue to be retried at a later time"""
+def push_to_sqs(event_body):
+    """Places event body dictionary on queue to be retried at a later time"""
     sqs = boto3.client('sqs')
     queue_url = os.environ.get('vetext_request_drop_sqs_url')
 
-    queue_msg = json.dumps(event)
+    queue_msg = json.dumps(event_body)
     queue_msg_attrs = {
         'source': {
             'DataType': 'String',
