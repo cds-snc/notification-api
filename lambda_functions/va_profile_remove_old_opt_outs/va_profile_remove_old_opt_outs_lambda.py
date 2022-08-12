1bd64864dfb6b5b381cc11ff23f569730550a84e
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2022-08-12 09:08:12 -0500

    #777 Resolve Lambda Timeout Related to Removing Opt-outs (#792)
    * Updated logger
    
    * Updated logging, closed connection
    
    * Added another log output.
    
    * Moved connection outside handler
    
    * Created execute function, fixed typo.
    
    * Added additional logging.
    
    * Added retry connection
    
    * Added comments, shifted logic for flow
    
    * DB connection should not be left open, closed. Added comments
    
    * Added name for the logger so it doesn't use the root logger.
    
    * Added log of closed connections

--- lambda_functions/va_profile_remove_old_opt_outs/va_profile_remove_old_opt_outs_lambda.py
+++ lambda_functions/va_profile_remove_old_opt_outs/va_profile_remove_old_opt_outs_lambda.py
@@ -3,12 +3,18 @@ import os
 import psycopg2
 import sys
 
+# Set globals
 REMOVE_OPTED_OUT_RECORDS_QUERY = """SELECT va_profile_remove_old_opt_outs();"""
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+logger = logging.getLogger('va_profile_remove_old_opt_outs')
+logger.setLevel(logging.INFO)
 
+# Verify environment is setup correctly
 if SQLALCHEMY_DATABASE_URI is None:
-    logging.error("The database URI is not set.")
+    logger.error("The database URI is not set.")
     sys.exit("Couldn't connect to the database.")
+else:
+    logger.info('Execution environment prepared...')
 
 
 def va_profile_remove_old_opt_outs_handler(event=None, context=None, worker_id=None):
@@ -16,17 +22,28 @@ def va_profile_remove_old_opt_outs_handler(event=None, context=None, worker_id=N
     This function deletes any va_profile cache records that
     are opted out and greater than 24 hours old.
     """
+    connection = None
 
     # https://www.psycopg.org/docs/module.html#exceptions
     try:
+        logger.info('Connecting to database...')
         connection = psycopg2.connect(SQLALCHEMY_DATABASE_URI + ('' if worker_id is None else f"_{worker_id}"))
+
         with connection.cursor() as c:
+            logger.info('Executing database function...')
             c.execute(REMOVE_OPTED_OUT_RECORDS_QUERY)
+            logger.info('Committing to database...')
             connection.commit()
+            logger.info('Completed commit...')
     except psycopg2.Warning as e:
-        logging.warning(e)
+        logger.warning(e)
     except psycopg2.Error as e:
-        logging.exception(e)
-        logging.error(e.pgcode)
+        logger.exception(e)
+        # https://www.postgresql.org/docs/11/errcodes-appendix.html
+        logger.error(e.pgcode)
     except Exception as e:
-        logging.exception(e)
+        logger.exception(e)
+    finally:
+        if connection:
+            connection.close()
+            logger.info('Connection closed...')
