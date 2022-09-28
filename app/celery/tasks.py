ce9fd8a8eefa47988409dee7b839820bd4c2a8df
Steve Astels <sastels@users.noreply.github.com>
2022-09-28 16:41:01 -0400

    fix/check daily_sms_limit wherever we check daily_message_limit (#1607)

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -71,7 +71,10 @@ from app.notifications.process_notifications import (
     persist_notifications,
     send_notification_to_queue,
 )
-from app.notifications.validators import check_service_over_daily_message_limit
+from app.notifications.validators import (
+    check_service_over_daily_message_limit,
+    check_service_over_daily_sms_limit,
+)
 from app.utils import get_csv_max_rows
 
 
@@ -286,6 +289,8 @@ def save_smss(self, service_id: Optional[str], signed_notifications: List[Any], 
         handle_batch_error_and_forward(self, signed_and_verified, SMS_TYPE, e, receipt, template)
 
     check_service_over_daily_message_limit(KEY_TYPE_NORMAL, service)
+    check_service_over_daily_sms_limit(KEY_TYPE_NORMAL, service)
+
     research_mode = service.research_mode  # type: ignore
 
     current_app.logger.info(f"Sending following sms notifications to AWS: {notification_id_queue.keys()}")
