08e4663697785930f30d2a1d124554dad43dd713
trevor2718 <47260522+trevor2718@users.noreply.github.com>
2022-10-07 12:10:32 -0500

    #768 Daily limit warning not sufficient and incorrect format (#851)
    Closed #768

--- app/notifications/validators.py
+++ app/notifications/validators.py
@@ -47,10 +47,9 @@ def check_service_over_daily_message_limit(key_type, service):
         if not service_stats:
             service_stats = services_dao.fetch_todays_total_message_count(service.id)
             redis_store.set(cache_key, service_stats, ex=3600)
-
-        if service.message_limit - int(service_stats) <= 100:
-            current_app.logger.info(f'service {service.id} nearing daily limit'
-                                    f'{service.message_limit} - {service_stats}')
+        if (round((int(service_stats) / service.message_limit), 1) * 100 > 75):
+            current_app.logger.info(f'service {service.id} nearing daily limit '
+                                    f'{(int(service_stats) / service.message_limit * 100):.1f}%')
 
         if int(service_stats) >= service.message_limit:
             current_app.logger.info(
