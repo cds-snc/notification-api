1034112ceec64a4699016528e1a6fb951bd60dc5
Stephen Astels <sastels@gmail.com>
2022-10-13 09:22:49 -0400

    dont check limits right before send

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -288,9 +288,6 @@ def save_smss(self, service_id: Optional[str], signed_notifications: List[Any], 
         signed_and_verified = list(zip(signed_notifications, verified_notifications))
         handle_batch_error_and_forward(self, signed_and_verified, SMS_TYPE, e, receipt, template)
 
-    check_service_over_daily_message_limit(KEY_TYPE_NORMAL, service)
-    check_service_over_daily_sms_limit(KEY_TYPE_NORMAL, service)
-
     research_mode = service.research_mode  # type: ignore
 
     current_app.logger.info(f"Sending following sms notifications to AWS: {notification_id_queue.keys()}")
@@ -398,7 +395,6 @@ def save_emails(self, service_id: Optional[str], signed_notifications: List[Any]
 
     if saved_notifications:
         current_app.logger.info(f"Sending following email notifications to AWS: {notification_id_queue.keys()}")
-        check_service_over_daily_message_limit(KEY_TYPE_NORMAL, service)
         research_mode = service.research_mode  # type: ignore
         for notification in saved_notifications:
             queue = notification_id_queue.get(notification.id) or template.queue_to_use()  # type: ignore
