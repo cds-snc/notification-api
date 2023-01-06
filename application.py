43dd3361ec8e2235ca6659d8fafeed56f9d8b081
Steve Astels <sastels@users.noreply.github.com>
2023-01-06 15:37:47 -0500

    Revert "Revert "Python 3.10 v3 (#1711)" (#1718)"
    This reverts commit 26ae374bbc373917ccdbd269affe14aaffcb0680.

--- application.py
+++ application.py
@@ -4,26 +4,15 @@ from __future__ import print_function
 import os
 
 import newrelic.agent  # See https://bit.ly/2xBVKBH
-import sentry_sdk
 from apig_wsgi import make_lambda_handler
 from dotenv import load_dotenv
 from flask import Flask
-from sentry_sdk.integrations.celery import CeleryIntegration
-from sentry_sdk.integrations.flask import FlaskIntegration
-from sentry_sdk.integrations.redis import RedisIntegration
-from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app import create_app
 
 load_dotenv()
 
-if "SENTRY_URL" in os.environ:
-    sentry_sdk.init(  # type: ignore
-        dsn=os.environ.get("SENTRY_URL", ""),
-        integrations=[CeleryIntegration(), FlaskIntegration(), RedisIntegration(), SqlalchemyIntegration()],
-        release="notify-api@" + os.environ.get("GIT_SHA", ""),
-    )
 
 application = Flask("api")
 application.wsgi_app = ProxyFix(application.wsgi_app)  # type: ignore
