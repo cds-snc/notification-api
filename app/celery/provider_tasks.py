a691379d40182c9473d5662b9d24e7f240025e43
Andrew Leith <andrew.leith@cds-snc.ca>
2023-01-09 16:38:39 +0000

    Fix merge issue.

--- app/celery/provider_tasks.py
+++ app/celery/provider_tasks.py
@@ -66,7 +66,7 @@ def deliver_email(self, notification_id):
         current_app.logger.error(f"Cannot send notification {notification_id}, got an invalid direct file url.")
         update_notification_status_by_id(notification_id, NOTIFICATION_TECHNICAL_FAILURE)
         _check_and_queue_callback_task(notification)
-    except Exception:
+    except Exception as e:
         try:
             current_app.logger.warning(f"The exception is {repr(e)}")
             current_app.logger.exception("RETRY: Email notification {} failed".format(notification_id))
