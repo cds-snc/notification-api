21c65fc88f214de06e332025f21721201186f66a
ldraney <draneylucas@gmail.com>
2023-02-22 15:06:42 -0700

    updated apm back to working

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from ddtrace import tracer
 from ddtrace import patch_all
 import os
 
@@ -25,6 +26,8 @@ application = Flask('app')
 application.wsgi_app = ProxyFix(application.wsgi_app)
 create_app(application)
 
+
+# this starts the ddtrace tracer and configures it to the right port and URL
 patch_all()
 os.environ["DD_TRACE_AGENT_HOSTNAME"] = "vanotify.ddog-gov.com"
 os.environ["DD_TRACE_AGENT_PORT"] = "8126"  # the default port for trace collection
