4778fac399e4fe90d1511f8b67c6da0d183de3c8
David Baldwin <124309521+justaskdavidb-docme360@users.noreply.github.com>
2023-02-24 11:50:34 -0600

    Update process_delivery_status_result_tasks.py
    added logic for CeleryEvent and corrected logger error

--- app/celery/process_delivery_status_result_tasks.py
+++ app/celery/process_delivery_status_result_tasks.py
@@ -1,7 +1,9 @@
 from flask import current_app
 from app import notify_celery
+from app.celery.process_pinpoint_inbound_sms import CeleryEvent
+
 
 @notify_celery.task(bind=True, name="process-delivery-status-result", max_retries=48, default_retry_delay=300)
 def process_delivery_status(self, event: CeleryEvent) -> bool:
-    logger.info('processing delivery status: %s', event)
+    current_app.logger.info('processing delivery status: %s', event)
     return True
