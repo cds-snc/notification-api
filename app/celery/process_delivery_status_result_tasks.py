f6b5186470923d9a53a1b0132ae74ec3997b3471
David Baldwin <124309521+justaskdavidb-docme360@users.noreply.github.com>
2023-03-03 14:39:23 -0600

    Revert "made event an any and logged out result of decoding object"
    This reverts commit 92fa9622af86b01bdff435ea9476bd2ab25d7e33.

--- app/celery/process_delivery_status_result_tasks.py
+++ app/celery/process_delivery_status_result_tasks.py
@@ -1,18 +1,10 @@
 from flask import current_app
 from app import notify_celery
-import base64
-import json
+from app.celery.process_pinpoint_inbound_sms import CeleryEvent
+
 
 # Create SQS Queue for Process Deliver Status.
 @notify_celery.task(bind=True, name="process-delivery-status-result", max_retries=48, default_retry_delay=300)
-def process_delivery_status(self, event) -> bool:
+def process_delivery_status(self, event: CeleryEvent) -> bool:
     current_app.logger.info('processing delivery status: %s', event)
-
-    try:
-        delivery_receipt_message = json.loads(base64.b64decode(event['Message']))
-        current_app.logger.info('processing delivery status: %s', delivery_receipt_message)
-    except (json.decoder.JSONDecodeError, ValueError, TypeError, KeyError) as e:
-        current_app.logger.exception(e)
-        return None
-    
     return True
