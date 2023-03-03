92fa9622af86b01bdff435ea9476bd2ab25d7e33
jacob cheriathundam <jacobcheriathundam@gmail.com>
2023-03-03 15:01:18 -0500

    made event an any and logged out result of decoding object

--- app/celery/process_delivery_status_result_tasks.py
+++ app/celery/process_delivery_status_result_tasks.py
@@ -1,10 +1,18 @@
 from flask import current_app
 from app import notify_celery
-from app.celery.process_pinpoint_inbound_sms import CeleryEvent
-
+import base64
+import json
 
 # Create SQS Queue for Process Deliver Status.
 @notify_celery.task(bind=True, name="process-delivery-status-result", max_retries=48, default_retry_delay=300)
-def process_delivery_status(self, event: CeleryEvent) -> bool:
+def process_delivery_status(self, event) -> bool:
     current_app.logger.info('processing delivery status: %s', event)
+
+    try:
+        delivery_receipt_message = json.loads(base64.b64decode(event['Message']))
+        current_app.logger.info('processing delivery status: %s', delivery_receipt_message)
+    except (json.decoder.JSONDecodeError, ValueError, TypeError, KeyError) as e:
+        current_app.logger.exception(e)
+        return None
+    
     return True
