cbb70d64b88661c52d54683f7833d58d2ba1de80
Stephen McMurtry <smcmurtry@gmail.com>
2023-03-06 22:04:04 +0000

    initial attempt to add an organisation_notes column to the services table

---
+++ migrations/versions/0428_add_organisation_notes.py
@@ -0,0 +1,27 @@
+"""
+
+Revision ID: 0428_add_organisation_notes
+Revises: 0427_add_bounce_type_indices
+Create Date: 2023-03-06 00:00:00
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+revision = "0428_add_organisation_notes"
+down_revision = "0427_add_bounce_type_indices"
+
+user = "postgres"
+timeout = 1200  # in seconds, i.e. 20 minutes
+default = 1
+
+
+def upgrade():
+    op.add_column(
+        "services",
+        sa.Column("organisation_notes", sa.String(), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_column("services", "organisation_notes")
