8a11cc2acf764c2e646dccd34a8d763280f46642
Stephen Astels <sastels@gmail.com>
2023-03-20 11:01:59 -0400

    use multiple signers

--- app/encryption.py
+++ app/encryption.py
@@ -28,74 +28,71 @@ class NotificationDictToSign(TypedDict):
 
 
 class CryptoSigner:
-    def init_app(self, app):
-        self.serializer = URLSafeSerializer(app.config.get("SECRET_KEY"))
-        self.salt = app.config.get("DANGEROUS_SALT")
+    def init_app(self, app, secret_key, salt):
+        self.serializer = URLSafeSerializer(secret_key)
+        self.salt = salt
+        self.dangerous_salt = app.config.get("DANGEROUS_SALT")
 
-    def _sign(self, to_sign: Any, salt: Optional[str] = None) -> str:
-        if salt is None:
-            salt = self.salt
-        return self.serializer.dumps(to_sign, salt=salt)
-
-    # NOTE: currently the verify checks agains the default salt as well as the salt passed in
-    # TODO: remove this once we've moved DANGEROUS_SALT to PASSWORD_SALT
-    def _verify(self, to_verify: str, salt: Optional[str] = None) -> Any:
-        if salt is None:
-            salt = self.salt
-        try:
-            return self.serializer.loads(to_verify, salt=salt)
-        except BadSignature:
-            return self.serializer.loads(to_verify, salt=self.salt)
+    def sign(self, to_sign: Any) -> str:
+        return self.serializer.dumps(to_sign, salt=self.salt)
 
     # TODO: get rid of this after everything is signed with the new salts
     # This is only needed where we look things up by the signed value:
     #     - get_api_key_by_secret()
     def sign_dangerous(self, to_sign: Any) -> str:
-        return self._sign(to_sign)
+        return self.serializer.dumps(to_sign, salt=self.dangerous_salt)
 
-    def sign_notification(self, notification: NotificationDictToSign) -> SignedNotification:
-        "A wrapper around the sign fn to define the argument type and return type"
-        return SignedNotification(self._sign(notification, "notification"))
+    # NOTE: currently the verify checks agains the default salt as well as the dangerous salt
+    # TODO: remove this double check once we've removed DANGEROUS_SALT
+    def verify(self, to_verify: str) -> Any:
+        try:
+            return self.serializer.loads(to_verify, salt=self.salt)
+        except BadSignature:
+            return self.serializer.loads(to_verify, salt=self.dangerous_salt)
 
-    def verify_notification(self, signed_notification: SignedNotification) -> NotificationDictToSign:
-        "A wrapper around the verify fn to define the argument type and return type"
-        return self._verify(signed_notification, "notification")
+    # def sign_notification(self, notification: NotificationDictToSign) -> SignedNotification:
+    #     "A wrapper around the sign fn to define the argument type and return type"
+    #     return SignedNotification(self._sign(notification, "notification"))
 
-    def sign_personalisation(self, personalisation: dict) -> str:
-        return self._sign(personalisation, "personalisation")
+    # def verify_notification(self, signed_notification: SignedNotification) -> NotificationDictToSign:
+    #     "A wrapper around the verify fn to define the argument type and return type"
+    #     return self._verify(signed_notification, "notification")
 
-    def verify_personalisation(self, signed_personalisation: str) -> dict:
-        return self._verify(signed_personalisation, "personalisation")
+    # def sign_personalisation(self, personalisation: dict) -> str:
+    #     return self._sign(personalisation, "personalisation")
 
-    def sign_complaint(self, complaint: dict) -> str:
-        return self._sign(complaint, "complaint")
+    # def verify_personalisation(self, signed_personalisation: str) -> dict:
+    #     return self._verify(signed_personalisation, "personalisation")
 
-    def verify_complaint(self, signed_complaint: str) -> dict:
-        return self._verify(signed_complaint, "complaint")
+    # def sign_complaint(self, complaint: dict) -> str:
+    #     return self._sign(complaint, "complaint")
 
-    def sign_delivery_status(self, delivery_status: dict) -> str:
-        return self._sign(delivery_status, "delivery-status")
+    # def verify_complaint(self, signed_complaint: str) -> dict:
+    #     return self._verify(signed_complaint, "complaint")
 
-    def verify_delivery_status(self, signed_delivery_status: str) -> dict:
-        return self._verify(signed_delivery_status, "delivery-status")
+    # def sign_delivery_status(self, delivery_status: dict) -> str:
+    #     return self._sign(delivery_status, "delivery-status")
 
-    def sign_bearer_token(self, bearer_token: str) -> str:
-        return self._sign(bearer_token, "bearer-token")
+    # def verify_delivery_status(self, signed_delivery_status: str) -> dict:
+    #     return self._verify(signed_delivery_status, "delivery-status")
 
-    def verify_bearer_token(self, signed_bearer_token: str) -> str:
-        return self._verify(signed_bearer_token, "bearer-token")
+    # def sign_bearer_token(self, bearer_token: str) -> str:
+    #     return self._sign(bearer_token, "bearer-token")
 
-    def sign_api_key(self, api_key_secret: str) -> str:
-        return self._sign(api_key_secret, "api-key")
+    # def verify_bearer_token(self, signed_bearer_token: str) -> str:
+    #     return self._verify(signed_bearer_token, "bearer-token")
 
-    def verify_api_key(self, signed_api_key_secret: str) -> str:
-        return self._verify(signed_api_key_secret, "api-key")
+    # def sign_api_key(self, api_key_secret: str) -> str:
+    #     return self._sign(api_key_secret, "api-key")
 
-    def sign_inbound_sms(self, content: str) -> str:
-        return self._sign(content, "inbound-sms")
+    # def verify_api_key(self, signed_api_key_secret: str) -> str:
+    #     return self._verify(signed_api_key_secret, "api-key")
 
-    def verify_inbound_sms(self, signed_content: str) -> str:
-        return self._verify(signed_content, "inbound-sms")
+    # def sign_inbound_sms(self, content: str) -> str:
+    #     return self._sign(content, "inbound-sms")
+
+    # def verify_inbound_sms(self, signed_content: str) -> str:
+    #     return self._verify(signed_content, "inbound-sms")
 
 
 def hashpw(password):
