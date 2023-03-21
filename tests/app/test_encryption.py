6145143f161cc38e3073341d40baf4a91eda4232
Stephen Astels <sastels@gmail.com>
2023-03-21 14:20:09 -0400

    move tests to a class

--- tests/app/test_encryption.py
+++ tests/app/test_encryption.py
@@ -4,40 +4,36 @@ from itsdangerous import BadSignature
 from app.encryption import CryptoSigner
 
 
-def test_sign_and_verify(notify_api):
-    signer = CryptoSigner()
-    signer.init_app(notify_api, "secret", "salt")
-    signed = signer.sign("this")
-    assert signed != "this"
-    assert signer.verify(signed) == "this"
+class TestEncryption:
+    def test_sign_and_verify(self, notify_api):
+        signer = CryptoSigner()
+        signer.init_app(notify_api, "secret", "salt")
+        signed = signer.sign("this")
+        assert signed != "this"
+        assert signer.verify(signed) == "this"
 
+    def test_should_verify_content_signed_with_DANGEROUS_SALT(self, notify_api):
+        signer = CryptoSigner()
+        signer.init_app(notify_api, "secret", "salt")
+        assert signer.verify(signer.sign_dangerous("this")) == "this"
 
-def test_should_verify_content_signed_with_DANGEROUS_SALT(notify_api):
-    signer = CryptoSigner()
-    signer.init_app(notify_api, "secret", "salt")
-    assert signer.verify(signer.sign_dangerous("this")) == "this"
+    def should_not_verify_content_signed_with_different_secrets(self, notify_api):
+        signer1 = CryptoSigner()
+        signer2 = CryptoSigner()
+        signer1.init_app(notify_api, "secret1", "salt")
+        signer2.init_app(notify_api, "secret2", "salt")
+        with pytest.raises(BadSignature):
+            signer2.verify(signer1.sign("this"))
 
+    def should_not_verify_content_signed_with_different_salts(self, notify_api):
+        signer1 = CryptoSigner()
+        signer2 = CryptoSigner()
+        signer1.init_app(notify_api, "secret", "salt1")
+        signer2.init_app(notify_api, "secret", "salt2")
+        with pytest.raises(BadSignature):
+            signer2.verify(signer1.sign("this"))
 
-def should_not_verify_content_signed_with_different_secrets(notify_api):
-    signer1 = CryptoSigner()
-    signer2 = CryptoSigner()
-    signer1.init_app(notify_api, "secret1", "salt")
-    signer2.init_app(notify_api, "secret2", "salt")
-    with pytest.raises(BadSignature):
-        signer2.verify(signer1.sign("this"))
-
-
-def should_not_verify_content_signed_with_different_salts(notify_api):
-    signer1 = CryptoSigner()
-    signer2 = CryptoSigner()
-    signer1.init_app(notify_api, "secret", "salt1")
-    signer2.init_app(notify_api, "secret", "salt2")
-
-    with pytest.raises(BadSignature):
-        signer2.verify(signer1.sign("this"))
-
-
-def test_should_sign_dicts(notify_api):
-    signer = CryptoSigner()
-    signer.init_app(notify_api, "secret", "salt")
-    assert signer.verify(signer.sign({"this": "that"})) == {"this": "that"}
+    def test_should_sign_dicts(self, notify_api):
+        signer = CryptoSigner()
+        signer.init_app(notify_api, "secret", "salt")
+        assert signer.verify(signer.sign({"this": "that"})) == {"this": "that"}
