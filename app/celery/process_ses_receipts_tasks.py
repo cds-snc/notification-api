b2398c2ccc9b560c520ebeaa577c74c9714712ec
wbanks <william.banks@cds-snc.ca>
2023-04-18 12:05:04 -0400

    Add logging

--- app/celery/process_ses_receipts_tasks.py
+++ app/celery/process_ses_receipts_tasks.py
@@ -82,7 +82,9 @@ def process_ses_results(self, response):
         if current_app.config["FF_BOUNCE_RATE_V1"]:
             if notification_status == NOTIFICATION_PERMANENT_FAILURE:
                 bounce_rate_client.set_sliding_hard_bounce(notification.service_id)
+                current_app.logger.info(f"Setting total hard bounce notifications for service {notification.service.id} with notification {notification.id} in REDIS")
             bounce_rate_client.set_sliding_notifications(notification.service_id)
+            current_app.logger.info(f"Setting total notifications for service {notification.service.id} with notification {notification.id} in REDIS")
 
         if notification.sent_at:
             statsd_client.timing_with_dates("callback.ses.elapsed-time", datetime.utcnow(), notification.sent_at)
