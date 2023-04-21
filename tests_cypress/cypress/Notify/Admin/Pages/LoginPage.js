85da662e9ac0cf57ff616620e80985f53428d91f
Andrew Leith <andrew.leith@cds-snc.ca>
2023-04-21 16:57:49 -0300

    chore: refactor login code to all be in the LoginPage

--- tests_cypress/cypress/Notify/Admin/Pages/LoginPage.js
+++ tests_cypress/cypress/Notify/Admin/Pages/LoginPage.js
@@ -1,17 +1,49 @@
+const { recurse } = require('cypress-recurse')
+
+
 // Parts of the page a user can interact with
 let Components = {
     EmailAddress: () => cy.get('#email_address'),
     Password: () => cy.get('#password'),
     SubmitButton: () => cy.get('button[type="submit"]'),
+    TwoFactorCode: () => cy.get('#two_factor_code'),
 };
 
 // Actions users can take on the page
 let Actions = {
+    EnterCode: (code) => {
+        Components.TwoFactorCode().type(code);
+        Components.SubmitButton().click();
+    },
     Login: (email, password) => {
+        // login with username and password
         cy.visit(LoginPage.URL);
         Components.EmailAddress().type(email);
         Components.Password().type(password);
         Components.SubmitButton().click();
+
+        // get email 2fa code
+        recurse(
+            () => cy.task('getLastEmail', {} ), // Cypress commands to retry
+            Cypress._.isObject, // keep retrying until the task returns an object
+            {
+                timeout: 60000, // retry up to 1 minute
+                delay: 5000, // wait 5 seconds between attempts
+            },
+        )
+            .its('html')
+            .then((html) => {
+                cy.document({ log: false }).invoke({ log: false }, 'write', html)
+            })
+
+        // ensure code is received and enter it
+        cy.contains('p', "security code to log in").should('be.visible');
+        cy.contains('p', 'security code to log in').invoke('text').as('MFACode');
+        cy.get('@MFACode').then((text) => {
+            let code = text.slice(0, 5);
+            cy.visit('/two-factor-email-sent');
+            Actions.EnterCode(code);
+        });
     }
 };
 
--- tests_cypress/cypress/e2e/admin/login.cy.js
+++ tests_cypress/cypress/e2e/admin/login.cy.js
@@ -1,9 +1,8 @@
 /// <reference types="cypress" />
 
 import config from "../../../config";
-import { LoginPage, TwoFactorPage } from "../../Notify/Admin/Pages/all";
+import { LoginPage } from "../../Notify/Admin/Pages/all";
 
-const { recurse } = require('cypress-recurse')
 
 const ADMIN_COOKIE = 'notify_admin_session';
 describe('Basic login', () => {
@@ -14,33 +13,7 @@ describe('Basic login', () => {
         cy.clearCookie(ADMIN_COOKIE); // clear auth cookie
         cy.task('deleteAllEmails'); // purge email inbox to make getting the 2fa code easier
 
-        cy.visit(LoginPage.URL);
-
         LoginPage.Login(Cypress.env('UI_TEST_USER'), Cypress.env('ADMIN_USER_PASSWORD'));
-
-        // retry fetching the email 
-        recurse(
-            () => cy.task('getLastEmail', {} ), // Cypress commands to retry
-            Cypress._.isObject, // keep retrying until the task returns an object
-            {
-                timeout: 60000, // retry up to 1 minute
-                delay: 5000, // wait 5 seconds between attempts
-            },
-        )
-            .its('html')
-            .then((html) => {
-                cy.document({ log: false }).invoke({ log: false }, 'write', html)
-            })
-
-        // ensure the email with security code is received 
-        cy.contains('p', "security code to log in").should('be.visible');
-
-        cy.contains('p', 'security code to log in').invoke('text').as('MFACode');
-        cy.get('@MFACode').then((text) => {
-            let code = text.slice(0, 5);
-            cy.visit(TwoFactorPage.URL);
-            TwoFactorPage.EnterCode(code);
-        });
 
         // ensure we logged in correctly
         cy.contains('h1', 'Sign-in history').should('be.visible');
@@ -48,12 +21,9 @@ describe('Basic login', () => {
 
     // Before each test, persist the auth cookie so we don't have to login again
     beforeEach(() => {
-        Cypress.Cookies.preserveOnce(ADMIN_COOKIE);
-
         // stop the recurring dashboard fetch requests
         cy.intercept('GET', '**/dashboard.json', {});
     });
-
 
     it('succeeds and ADMIN displays accounts page', () => {
         cy.visit("/accounts");
@@ -63,14 +33,14 @@ describe('Basic login', () => {
         cy.contains('h1', 'Your services').should('be.visible');
     });
 
-    // it('displays notify service page', () => {
-    //     cy.visit(`/services/${config.Services.Notify}`);
-    //     cy.contains('h1', 'Dashboard').should('be.visible');
-    // });
+    it('displays notify service page', () => {
+        cy.visit(`/services/${config.Services.Notify}`);
+        cy.contains('h1', 'Dashboard').should('be.visible');
+    });
 
-    // it('has a qualtrics survey', () => {
-    //     cy.get('#QSIFeedbackButton-btn').should('be.visible'); // qualtrics survey button
-    //     cy.get('#QSIFeedbackButton-btn').click(); // click the button
-    //     cy.get('#QSIFeedbackButton-survey-iframe').should('be.visible'); // 
-    // });
+    it('has a qualtrics survey', () => {
+        cy.get('#QSIFeedbackButton-btn').should('be.visible'); // qualtrics survey button
+        cy.get('#QSIFeedbackButton-btn').click(); // click the button
+        cy.get('#QSIFeedbackButton-survey-iframe').should('be.visible'); // 
+    });
 });
