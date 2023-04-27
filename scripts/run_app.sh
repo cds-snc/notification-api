edfb18e2e3cae19a58f5f081e6ac2bbe84cd3aec
ldraney <draneylucas@gmail.com>
2023-04-27 14:29:35 -0600

    added ddtrace to the celery startup command as well

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -3,4 +3,4 @@
 set -e
 
 # https://flask.palletsprojects.com/en/2.2.x/config/?highlight=flask_debug#debug-mode
-flask --debug run -p 6011 --host=0.0.0.0
+ddtrace-run flask --debug run -p 6011 --host=0.0.0.0
