bdf8690671d299f630a79a845a9c13183eb0c9f3
Stephen Astels <sastels@gmail.com>
2023-05-02 15:54:22 -0400

    add confirmation

--- scripts/run_celery_purge.sh
+++ scripts/run_celery_purge.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-celery -A run_celery.notify_celery purge -f
+echo "\n--------------------------------------------------\n"
+echo "                   WARNING!!!!\n"
+echo "    This script is for local development only!\n"
+echo "  It will delete everything in the celery queues.\n"
+echo "--------------------------------------------------\n"
+echo "Are you sure you want to continue?"
+read -p "If so, type 'purge'> " check
+    case $check in
+        purge ) echo "purging!"; celery -A run_celery.notify_celery purge -f; break;;
+        [Nn]* ) exit;;
+        * ) echo "\nNot purging\n";;
+    esac
+
