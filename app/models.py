4616391986f7b8aeddd5dde92fdfe96ba34e41d1
William B <7444334+whabanks@users.noreply.github.com>
2023-05-29 15:50:23 -0400

    Improve api and callback status messages (#1870)
    * Improve notification status wording for api calls and api callbacks
    
    * formatting
    
    * Fix bugs and tests
    
    * Remove formatted_status from serialized data

--- app/models.py
+++ app/models.py
@@ -1781,6 +1781,9 @@ class Notification(BaseModel):
                     "sending": "In transit",
                     "created": "In transit",
                     "sent": "Delivered",
+                    "pending": "In transit",
+                    "pending-virus-check": "In transit",
+                    "pii-check-failed": "Exceeds Protected A",
                 },
                 "sms": {
                     "failed": "Failed",
@@ -1790,6 +1793,7 @@ class Notification(BaseModel):
                     "delivered": "Delivered",
                     "sending": "In transit",
                     "created": "In transit",
+                    "pending": "In transit",
                     "sent": "Sent",
                 },
                 "letter": {
@@ -1905,7 +1909,7 @@ class Notification(BaseModel):
             "line_6": None,
             "postcode": None,
             "type": self.notification_type,
-            "status": self.get_letter_status() if self.notification_type == LETTER_TYPE else self.status,
+            "status": self.get_letter_status() if self.notification_type == LETTER_TYPE else self.formatted_status,
             "provider_response": self.provider_response,
             "template": template_dict,
             "body": self.content,
