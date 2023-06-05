603b336108dcfc003a28b4ad4c3d7a5847c66c5d
Stephen Astels <sastels@gmail.com>
2023-06-05 15:05:50 -0400

    add a flag for resigning notifications

--- scripts/resign_database.py
+++ scripts/resign_database.py
@@ -28,20 +28,23 @@ from app.dao.notifications_dao import resign_notifications  # noqa: E402
 from app.dao.service_callback_api_dao import resign_service_callbacks  # noqa: E402
 
 
-def resign_all(chunk: int, resign: bool, unsafe: bool):
+def resign_all(chunk: int, resign: bool, unsafe: bool, notifications: bool):
     resign_api_keys(resign, unsafe)
     resign_inbound_sms(resign, unsafe)
     resign_service_callbacks(resign, unsafe)
-    resign_notifications(chunk, resign, unsafe)
+    if notifications:
+        resign_notifications(chunk, resign, unsafe)
     if not resign:
         current_app.logger.info("NOTE: this is a preview, fields have not been changed. To resign fields, run with --resign flag")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--chunk", default=25000, type=int, help="size of chunks of notifications to resign at a time (default 25000)")
-    parser.add_argument("--resign", default=False, action='store_true', help="resign columns (default false)")
-    parser.add_argument("--unsafe", default=False, action='store_true', help="ignore bad signatures (default false)")
+    parser.add_argument("-n", "--notifications", default=False, action='store_true', help="resign notifications (default false)")
+    parser.add_argument("-c", "--chunk", default=25000, type=int, help="size of chunks of notifications to resign at a time (default 25000)")
+    parser.add_argument("-r", "--resign", default=False, action='store_true', help="resign columns (default false)")
+    parser.add_argument("-u", "--unsafe", default=False, action='store_true', help="ignore bad signatures (default false)")
+    
     args = parser.parse_args()
 
     load_dotenv()
@@ -49,4 +52,4 @@ if __name__ == "__main__":
     create_app(application)
     application.app_context().push()
 
-    resign_all(args.chunk, args.resign, args.unsafe)
+    resign_all(args.chunk, args.resign, args.unsafe, args.notifications)
