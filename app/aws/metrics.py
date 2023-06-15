966bc9023888b865c30a8371ca2364d089d52d3a
Steve Astels <sastels@users.noreply.github.com>
2023-06-15 14:50:17 -0400

    cant reuse the old metric (#1895)

--- app/aws/metrics.py
+++ app/aws/metrics.py
@@ -92,6 +92,7 @@ def put_batch_saving_expiry_metric(metrics_logger: MetricsLogger, queue: RedisQu
         metrics_logger.put_metric("batch_saving_inflight", count, "Count")
         metrics_logger.set_dimensions({"expired": "True", "notification_type": queue._suffix, "priority": queue._process_type})
         metrics_logger.flush()
+        metrics_logger.put_metric("batch_saving_inflight", count, "Count")
         metrics_logger.set_dimensions({"expired": "True", "notification_type": "any", "priority": "any"})
         metrics_logger.flush()
     except ClientError as e:
