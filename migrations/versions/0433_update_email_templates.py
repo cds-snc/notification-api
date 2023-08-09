1729088618eae07dbe4849335a9cfc633b548b5e
Stephen McMurtry <smcmurtry@gmail.com>
2023-08-09 14:01:31 +0000

    changed the filename

--- migrations/versions/0433_update_email_templates.py
+++ migrations/versions/0433_update_email_templates.py
@@ -1,6 +1,6 @@
 """
 
-Revision ID: 0433_update_daily_email_limit_templates
+Revision ID: 0433_update_email_templates
 Revises: 0432_daily_email_limit_templates
 Create Date: 2023-08-08 00:00:00
 
@@ -10,73 +10,83 @@ from datetime import datetime
 from alembic import op
 from flask import current_app
 
-revision = "0433_update_daily_email_limit_templates"
+revision = "0433_update_email_templates"
 down_revision = "0432_daily_email_limit_templates"
 
-near_content = """
-(la version française suit)
+near_content = "\n".join(
+    [
+        "(la version française suit)",
+        "",
+        "Hello ((name)),",
+        "",
+        "((service_name)) can send ((message_limit_en)) emails per day. You’ll be blocked from sending if you exceed that limit before 7pm Eastern Time. Check [your current local time](https://nrc.canada.ca/en/web-clock/).",
+        "",
+        "To request a limit increase, [contact us](https://notification.canada.ca/contact). We’ll respond within 1 business day.",
+        "",
+        "The GC Notify team",
+        "",
+        "---",
+        "",
+        "Bonjour ((name)),",
+        "",
+        "La limite quotidienne d’envoi est de ((message_limit_fr)) courriels par jour pour ((service_name)). Si vous dépassez cette limite avant 19 heures, heure de l’Est, vos envois seront bloqués.",
+        "",
+        "Comparez [les heures officielles au Canada](https://nrc.canada.ca/fr/horloge-web/).",
+        "",
+        "Veuillez [nous contacter](https://notification.canada.ca/contact) si vous souhaitez augmenter votre limite d’envoi. Nous vous répondrons en un jour ouvrable.",
+        "",
+        "L’équipe Notification GC",
+    ]
+)
 
-Hello ((name)),
 
-((service_name)) can send ((message_limit_en)) emails per day. You’ll be blocked from sending if you exceed that limit before 7pm Eastern Time. Check [your current local time](https://nrc.canada.ca/en/web-clock/).
+reached_content = "\n".join(
+    [
+        "(la version française suit)",
+        "",
+        "Hello ((name)),",
+        "",
+        "((service_name)) has sent ((message_limit_en)) emails today.",
+        "",
+        "You can send more messages after 7pm Eastern Time. Compare [official times across Canada](https://nrc.canada.ca/en/web-clock/).",
+        "",
+        "To request a limit increase, [contact us](https://notification.canada.ca/contact). We’ll respond within 1 business day.",
+        "",
+        "The GC Notify team",
+        "",
+        "---",
+        "",
+        "Bonjour ((name)),",
+        "",
+        "Aujourd’hui, ((message_limit_fr)) courriels ont été envoyés pour ((service_name)).",
+        "",
+        "Vous pourrez envoyer davantage de courriels après 19 heures, heure de l’Est. Comparez [les heures officielles au Canada](https://nrc.canada.ca/fr/horloge-web/).",
+        "",
+        "Veuillez [nous contacter](https://notification.canada.ca/contact) si vous désirez augmenter votre limite d’envoi. Nous vous répondrons en un jour ouvrable.",
+        "",
+        "L’équipe Notification GC",
+    ]
+)
 
-To request a limit increase, [contact us](https://notification.canada.ca/contact). We’ll respond within 1 business day.
-
-The GC Notify team
-
----
-Bonjour ((name)),
-
-La limite quotidienne d’envoi est de ((message_limit_fr)) courriels par jour pour ((service_name)). Si vous dépassez cette limite avant 19 heures, heure de l’Est, vos envois seront bloqués.
-
-Comparez [les heures officielles au Canada](https://nrc.canada.ca/fr/horloge-web/).
-
-Veuillez [nous contacter](https://notification.canada.ca/contact) si vous souhaitez augmenter votre limite d’envoi. Nous vous répondrons en un jour ouvrable.
-
-L’équipe Notification GC
-"""
-
-reached_content = """
-(la version française suit)
-
-Hello ((name)),
-
-((service_name)) has sent ((message_limit_en)) emails today.
-
-You can send more messages after 7pm Eastern Time. Compare [official times across Canada](https://nrc.canada.ca/en/web-clock/).
-
-To request a limit increase, [contact us](https://notification.canada.ca/contact). We’ll respond within 1 business day.
-
-The GC Notify team
-
----
-Bonjour ((name)),
-
-Aujourd’hui, ((message_limit_fr)) courriels ont été envoyés pour ((service_name)).
-
-Vous pourrez envoyer davantage de courriels après 19 heures, heure de l’Est. Comparez [les heures officielles au Canada](https://nrc.canada.ca/fr/horloge-web/). 
-
-Veuillez [nous contacter](https://notification.canada.ca/contact) si vous désirez augmenter votre limite d’envoi. Nous vous répondrons en un jour ouvrable.
-
-L’équipe Notification GC
-"""
-
-updated_content = """
-(la version française suit)
-
-Hello ((name)),
-
-You can now send ((message_limit_en)) email messages per day.
-
-The GC Notify Team
-
----
-Bonjour ((name)),
-
-Vous pouvez désormais envoyer ((message_limit_fr)) courriels par jour.
-
-L’équipe Notification GC
-"""
+updated_content =  "\n".join(
+    [
+        "(la version française suit)",
+        "",
+        "Hello ((name)),",
+        "",
+        "You can now send ((message_limit_en)) email messages per day.",
+        "",
+        "The GC Notify Team",
+        "",
+        "---",
+        "",
+        "Bonjour ((name)),",
+        "",
+        "Vous pouvez désormais envoyer ((message_limit_fr)) courriels par jour.",
+        "",
+        "L’équipe Notification GC",
+    ]
+)
 
 templates = [
     {
