fca91ffbb3c306b317f1e238e1f3cbab95552299
Andrew Leith <andrew.leith@cds-snc.ca>
2023-08-10 15:18:30 +0000

    fix(ci): insert user

--- tests_cypress/seed_db.py
+++ tests_cypress/seed_db.py
@@ -10,11 +10,11 @@ engine = sa.create_engine(SQLALCHEMY_DATABASE_URI)
 
 with engine.connect() as conn:
     with conn.begin():        
-        # insert_user = """
-        # INSERT INTO "public"."users"("id","name","email_address","created_at","updated_at","_password","mobile_number","password_changed_at","logged_in_at","failed_login_count","state","platform_admin","current_session_id","auth_type","blocked","additional_information","password_expired")
-        #     VALUES
-        #     (E'3f478896-6d3f-4ef3-aa5a-530fea1206bb',E'Notify UI Tests',E'notify-uid-tests@cds-snc.ca',E'2023-05-24 13:41:48.022737',E'2023-08-01 13:20:08.017647',E'$2b$10$jwj45gXRLUIteNBwhRgEV.4P7uHGH11O2PhNiDK6SY6oRPSdKhg2u',E'6139863526',E'2023-05-24 13:41:48.017676',E'2023-08-01 13:20:08.014524',0,E'active',FALSE,E'8f6e79df-782d-4620-b86c-adf1ea933934',E'email_auth',FALSE,E'{}',FALSE);
-        # """
+        insert_user = """
+        INSERT INTO "public"."users"("id","name","email_address","created_at","updated_at","_password","mobile_number","password_changed_at","logged_in_at","failed_login_count","state","platform_admin","current_session_id","auth_type","blocked","additional_information","password_expired")
+            VALUES
+            (E'3f478896-6d3f-4ef3-aa5a-530fea1206bb',E'Notify UI Tests',E'notify-ui-tests@cds-snc.ca',E'2023-05-24 13:41:48.022737',E'2023-08-01 13:20:08.017647',E'$2b$10$jwj45gXRLUIteNBwhRgEV.4P7uHGH11O2PhNiDK6SY6oRPSdKhg2u',E'6139863526',E'2023-05-24 13:41:48.017676',E'2023-08-01 13:20:08.014524',0,E'active',FALSE,E'8f6e79df-782d-4620-b86c-adf1ea933934',E'email_auth',FALSE,E'{}',FALSE);
+        """
         insert_org = """
             INSERT INTO "public"."organisation"("id","name","active","created_at","updated_at","email_branding_id","letter_branding_id","agreement_signed","agreement_signed_at","agreement_signed_by_id","agreement_signed_version","crown","organisation_type","request_to_go_live_notes","agreement_signed_on_behalf_of_email_address","agreement_signed_on_behalf_of_name","default_branding_is_french")
             VALUES
@@ -23,7 +23,7 @@ with engine.connect() as conn:
         insert_service = """
             INSERT INTO "public"."services"("id","name","created_at","updated_at","active","message_limit","restricted","email_from","created_by_id","version","research_mode","organisation_type","prefix_sms","crown","rate_limit","contact_link","consent_to_research","volume_email","volume_letter","volume_sms","count_as_live","go_live_at","go_live_user_id","organisation_id","sending_domain","default_branding_is_french","sms_daily_limit","organisation_notes")
             VALUES
-            (E'4049c2d0-0cab-455c-8f4c-f356dff51811',E'Cypress2',E'2023-05-16 15:02:43.663183',E'2023-05-24 14:01:51.335072',TRUE,10000,FALSE,E'bouncey',E'6af522d0-2915-4e52-83a3-3690455a5fe6',5,FALSE,E'central',TRUE,TRUE,1000,NULL,NULL,NULL,NULL,NULL,FALSE,E'2023-05-16 15:08:46.692247',NULL,E'93413f91-227f-4704-b229-b8210d1ecc0a',NULL,FALSE,1000,NULL);
+            (E'4049c2d0-0cab-455c-8f4c-f356dff51811',E'Cypress2',E'2023-05-16 15:02:43.663183',E'2023-05-24 14:01:51.335072',TRUE,10000,FALSE,E'bouncey',E'3f478896-6d3f-4ef3-aa5a-530fea1206bb',5,FALSE,E'central',TRUE,TRUE,1000,NULL,NULL,NULL,NULL,NULL,FALSE,E'2023-05-16 15:08:46.692247',NULL,E'93413f91-227f-4704-b229-b8210d1ecc0a',NULL,FALSE,1000,NULL);
         """
         
         insert_templates = """
@@ -41,10 +41,11 @@ with engine.connect() as conn:
         insert_api_keys = """
             INSERT INTO "public"."api_keys"("id","name","secret","service_id","expiry_date","created_at","created_by_id","updated_at","version","key_type")
             VALUES
-            (E'076fc5ba-8578-44d6-8b69-122ac3a7b206',E'CYPRESS_TEST_KEY',E'IjY3YTViZmU4LWMxNjktNDQ0NS04OGVjLWRkNWExZDA3NjFmYSI.wLt-AQOYG3vgab_qP1p0Djw_r-8',E'4049c2d0-0cab-455c-8f4c-f356dff51811',NULL,E'2023-07-10 16:44:13.025984',E'6af522d0-2915-4e52-83a3-3690455a5fe6',NULL,1,E'test'),
-            (E'61a3f8e1-516d-446c-bc3a-c2dac33c9474',E'CYPRESS',E'IjhmYmE3MzUwLTIwN2ItNDRhYi1iNmYwLTVkZjk3ZjAzNjZlMyI.xiQ94d2oAedkohXNhx83B6cWr_0',E'4049c2d0-0cab-455c-8f4c-f356dff51811',NULL,E'2023-05-24 13:48:23.93561',E'6af522d0-2915-4e52-83a3-3690455a5fe6',NULL,1,E'normal'),
-            (E'74a06881-7742-4d4a-85ff-b91ae71e1bcd',E'CYPRESS_TEAM_KEY',E'IjYxZjhhYzU3LTNlZGEtNDE3MS1iMDcyLTJjNTY0OWEzODI4ZSI.p6K5JNnYp6Pcn3OEQg1kDM1UHAY',E'4049c2d0-0cab-455c-8f4c-f356dff51811',NULL,E'2023-07-10 16:43:19.425995',E'6af522d0-2915-4e52-83a3-3690455a5fe6',NULL,1,E'team');
+            (E'076fc5ba-8578-44d6-8b69-122ac3a7b206',E'CYPRESS_TEST_KEY',E'IjY3YTViZmU4LWMxNjktNDQ0NS04OGVjLWRkNWExZDA3NjFmYSI.wLt-AQOYG3vgab_qP1p0Djw_r-8',E'4049c2d0-0cab-455c-8f4c-f356dff51811',NULL,E'2023-07-10 16:44:13.025984',E'3f478896-6d3f-4ef3-aa5a-530fea1206bb',NULL,1,E'test'),
+            (E'61a3f8e1-516d-446c-bc3a-c2dac33c9474',E'CYPRESS',E'IjhmYmE3MzUwLTIwN2ItNDRhYi1iNmYwLTVkZjk3ZjAzNjZlMyI.xiQ94d2oAedkohXNhx83B6cWr_0',E'4049c2d0-0cab-455c-8f4c-f356dff51811',NULL,E'2023-05-24 13:48:23.93561',E'3f478896-6d3f-4ef3-aa5a-530fea1206bb',NULL,1,E'normal'),
+            (E'74a06881-7742-4d4a-85ff-b91ae71e1bcd',E'CYPRESS_TEAM_KEY',E'IjYxZjhhYzU3LTNlZGEtNDE3MS1iMDcyLTJjNTY0OWEzODI4ZSI.p6K5JNnYp6Pcn3OEQg1kDM1UHAY',E'4049c2d0-0cab-455c-8f4c-f356dff51811',NULL,E'2023-07-10 16:43:19.425995',E'3f478896-6d3f-4ef3-aa5a-530fea1206bb',NULL,1,E'team');
         """
+        conn.execute(sa.text(insert_user))
         conn.execute(sa.text(insert_org))
         conn.execute(sa.text(insert_service))
         conn.execute(sa.text(insert_templates))
