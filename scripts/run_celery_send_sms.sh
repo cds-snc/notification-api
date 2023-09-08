62253a7d6387cf2b634c5b994f1448e9944457e0
Steve Astels <sastels@users.noreply.github.com>
2023-09-08 08:47:03 -0400

    tweak celery scripts (#1972)

---
+++ scripts/run_celery_send_sms.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# runs celery with only the send-sms-* queues
+
+set -e
+
+echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
+
+# TODO: we shouldn't be using the send-sms-tasks queue anymore - once we verify this we can remove it
+celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=${CELERY_CONCURRENCY-4} -Q send-sms-tasks,send-sms-high,send-sms-medium,send-sms-low
