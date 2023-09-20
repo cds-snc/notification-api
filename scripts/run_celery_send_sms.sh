f799a98f384ae8fe9a76395c0977e597ef42d300
Steve Astels <sastels@users.noreply.github.com>
2023-09-20 14:08:57 -0400

    sms celery concurrency to 10 (#1985)

--- scripts/run_celery_send_sms.sh
+++ scripts/run_celery_send_sms.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-echo "Start celery, concurrency: 6"
+echo "Start celery, concurrency: 10"
 
 # TODO: we shouldn't be using the send-sms-tasks queue anymore - once we verify this we can remove it
-celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=6 -Q send-sms-tasks,send-sms-high,send-sms-medium,send-sms-low
+celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=10 -Q send-sms-tasks,send-sms-high,send-sms-medium,send-sms-low
