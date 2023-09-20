5fcb7b3011fa40f3915b2d2f438286c1d3e3d0e6
Steve Astels <sastels@users.noreply.github.com>
2023-09-20 13:23:42 -0400

    increase concurrency on staging sms celery pods (#1984)

--- scripts/run_celery_send_sms.sh
+++ scripts/run_celery_send_sms.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
+echo "Start celery, concurrency: 6"
 
 # TODO: we shouldn't be using the send-sms-tasks queue anymore - once we verify this we can remove it
-celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=${CELERY_CONCURRENCY-4} -Q send-sms-tasks,send-sms-high,send-sms-medium,send-sms-low
+celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=6 -Q send-sms-tasks,send-sms-high,send-sms-medium,send-sms-low
