1310f6eda9121481d2db7036d210d1e9e323a074
Ben Larabie <ben.larabie@cds-snc.ca>
2023-10-06 08:53:35 -0400

    Adding a check to wait for cwagent before starting

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -2,7 +2,28 @@
 
 # runs celery with all celery queues except the throtted sms queue
 
+init()
+{
+    # Wait for cwagent to become available.
+
+    while :
+    do
+        if  nc -vz $STATSD_HOST 25888; then
+            echo "CWAgent is Ready."
+            break;
+        else
+            echo "Waiting for CWAgent to become ready."
+            sleep 1
+        fi
+    done
+}
+
 set -e
+
+# Check and see if this is running in K8s and if so, wait for cloudwatch agent
+if [[ -z "${STATSD_HOST}" ]]; then
+    init
+fi
 
 echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
 
