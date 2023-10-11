d45511d347c7d16438995c60f15ede251a17e9fa
Ben Larabie <benlarabie@gmail.com>
2023-10-11 10:26:33 -0400

    Adding a check to wait for cwagent before starting (#1996)
    * Adding a check to wait for cwagent before starting
    
    * Adding cwagent check to nosms script

--- scripts/run_celery_no_sms_sending.sh
+++ scripts/run_celery_no_sms_sending.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
+# runs celery with only the throttled sms sending queue
+
+init()
+{
+     # Wait for cwagent to become available.   
+    while :
+    do
+        if  nc -vz $STATSD_HOST 25888; then
+            echo "CWAgent is Ready."
+            break;
+        else
+            echo "Waiting for CWAgent to become ready."
+            sleep 1
+        fi
+    done
+}
+
 # runs celery with all celery queues except send-throttled-sms-tasks, send-sms-tasks, send-sms-high, send-sms-medium, or send-sms-low
 
 set -e
+
+# Check and see if this is running in K8s and if so, wait for cloudwatch agent
+if [[ -z "${STATSD_HOST}" ]]; then
+    init
+fi
 
 echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
 
