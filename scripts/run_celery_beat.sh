11da5601828ff74f748d0dcf3f1106473b42620e
Ben Larabie <benlarabie@gmail.com>
2023-11-02 15:04:43 -0400

    Adding cwagent wait script to all celery launchers (#2013)

--- scripts/run_celery_beat.sh
+++ scripts/run_celery_beat.sh
@@ -4,4 +4,20 @@
 
 set -e
 
+# Check and see if this is running in K8s and if so, wait for cloudwatch agent
+if [[ ! -z "${STATSD_HOST}" ]]; then
+
+    echo "Initializing... Waiting for CWAgent to become ready."
+    while :
+    do
+        if  nc -vz $STATSD_HOST 25888; then
+            echo "CWAgent is Ready."
+            break;
+        else
+            echo "Waiting for CWAgent to become ready."
+            sleep 1
+        fi
+    done
+fi
+
 celery -A run_celery.notify_celery beat --loglevel=INFO
