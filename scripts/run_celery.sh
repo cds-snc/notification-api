5d87904d11bec952c3bd3823919fa6f3877549c9
Jimmy Royer <jimleroyer@gmail.com>
2023-11-06 17:43:56 -0500

    Added timeout to cwagents waiting + shellcheck addition

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-# runs celery with all celery queues except the throtted sms queue
-
 set -e
 
-# Check and see if this is running in K8s and if so, wait for cloudwatch agent
-if [[ ! -z "${STATSD_HOST}" ]]; then
+# Runs celery with all celery queues except the throtted sms queue.
 
-    echo "Initializing... Waiting for CWAgent to become ready."
-    while :
-    do
-        if  nc -vz $STATSD_HOST 25888; then
+# Check and see if this is running in K8s and if so, wait for cloudwatch agent
+if [ -n "${STATSD_HOST}" ]; then
+    echo "Initializing... Waiting for CWAgent to become ready within the next 30 seconds."
+    timeout=30
+    while [ $timeout -gt 0 ]; do
+        if nc -vz "$STATSD_HOST" 25888; then
             echo "CWAgent is Ready."
-            break;
+            break
         else
             echo "Waiting for CWAgent to become ready."
             sleep 1
+            timeout=$((timeout - 1))
         fi
     done
+    
+    if [ $timeout -eq 0 ]; then
+        echo "Timeout reached. CWAgent did not become ready in 30 seconds."
+        exit 1
+    fi
 fi
 
 echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
--- scripts/run_celery_send_email.sh
+++ scripts/run_celery_send_email.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-# runs celery with only the send-email-* queues
-
 set -e
 
-# Check and see if this is running in K8s and if so, wait for cloudwatch agent
-if [[ ! -z "${STATSD_HOST}" ]]; then
+# Runs celery with only the send-email-* queues.
 
-    echo "Initializing... Waiting for CWAgent to become ready."
-    while :
-    do
-        if  nc -vz $STATSD_HOST 25888; then
+# Check and see if this is running in K8s and if so, wait for cloudwatch agent
+if [ -n "${STATSD_HOST}" ]; then
+    echo "Initializing... Waiting for CWAgent to become ready within the next 30 seconds."
+    timeout=30
+    while [ $timeout -gt 0 ]; do
+        if nc -vz "$STATSD_HOST" 25888; then
             echo "CWAgent is Ready."
-            break;
+            break
         else
             echo "Waiting for CWAgent to become ready."
             sleep 1
+            timeout=$((timeout - 1))
         fi
     done
+    
+    if [ $timeout -eq 0 ]; then
+        echo "Timeout reached. CWAgent did not become ready in 30 seconds."
+        exit 1
+    fi
 fi
 
 echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
 
-# TODO: we shouldn't be using the send-email-tasks queue anymore - once we verify this we can remove it
+# TODO: we shouldn't be using the send-email-tasks queue anymore, once we verify this we can remove it
 celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=${CELERY_CONCURRENCY-4} -Q send-email-tasks,send-email-high,send-email-medium,send-email-low
