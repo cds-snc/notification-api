4650cb1dad30b899b9c185e7938237a0e6d80bf6
Calvin Rodo <calvinv@gmail.com>
2023-11-15 03:00:01 +0000

    fix: remove unneccessary break and read

--- scripts/run_celery_purge.sh
+++ scripts/run_celery_purge.sh
@@ -6,11 +6,12 @@ printf "\n--------------------------------------------------\n"
 printf "                   WARNING!!!!\n"
 printf "    This script is for local development only!\n"
 printf "  It will delete everything in the celery queues.\n"
-printf "--------------------------------------------------\n"
+printf "\n--------------------------------------------------\n"
 printf "Are you sure you want to continue?"
-read -p "If so, type 'purge'> " check
+echo "If so, type 'purge'> \c"
+read -r check
     case $check in
-        purge ) echo "purging!"; celery -A run_celery.notify_celery purge -f; break;;
+        purge ) echo "purging!"; celery -A run_celery.notify_celery purge -f;;
         * ) printf "\nNot purging\n";;
     esac
 
