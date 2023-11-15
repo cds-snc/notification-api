8bc84d702e33e863294051d225e43fc7b4fc67b1
Calvin Rodo <calvin.rodo@cds-snc.ca>
2023-11-15 10:49:00 -0500

    Fix Shellcheck errors by doing as little work as possible (#2022)
    # Summary | Résumé
    
    These fixes a bunch of shellcheck errors and ignores a few I didn't test, actually I didn't test any of this, most of the fixes were pretty straightforward ones, there was one I fixed with co-pilot that should be tested and the others I didn't fix I just assumed they worked as is since they were in the existing codebase.
    
    - **Fixes**: https://www.shellcheck.net/wiki/SC1091 Couldn't find the file that was being referenced (`environment_test.sh`) in `scripts/run_single_test.sh` so I just the source to /dev/null
    - **Fixes**: https://www.shellcheck.net/wiki/SC2028 Replaces some echo's where a control character was passed "\n" with a printf so it should work the same and not complain
    - **Fixes the read -p error**: https://www.shellcheck.net/wiki/SC3045 by asking co-pilot to fix it, I can't guarantee this one will work, **YOU SHOULD TEST THIS FILE: scripts/run_celery_purge.sh**
    - **Ignores**: https://www.shellcheck.net/wiki/SC2009 I'm assuming it worked and I didn't want to find the pgrep way of doing it, also it's not posix
    - **Ignores**: https://www.shellcheck.net/wiki/SC2219 I'm assuming it worked and I didn't want to test if the following works
    ```bash 
    ((wait_time++)) || true
    ```
    - **Fixes**: https://www.shellcheck.net/wiki/SC2105 By removing the break from the case, they don't need it.
    - **Fixes**: https://www.shellcheck.net/wiki/SC3046 By replacing `source` with `.`
    - **Fixes**: https://www.shellcheck.net/wiki/SC2068 by wrapping array Expansion with double quotes
    - **Fixes**: https://www.shellcheck.net/wiki/SC2086 By wrapping the variable in double quotes

--- scripts/run_celery_send_sms.sh
+++ scripts/run_celery_send_sms.sh
@@ -28,4 +28,4 @@ fi
 echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
 
 # TODO: we shouldn't be using the send-sms-tasks queue anymore - once we verify this we can remove it
-celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=${CELERY_CONCURRENCY-4} -Q send-sms-tasks,send-sms-high,send-sms-medium,send-sms-low
+celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency="${CELERY_CONCURRENCY-4}" -Q send-sms-tasks,send-sms-high,send-sms-medium,send-sms-low
--- scripts/run_single_test.sh
+++ scripts/run_single_test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # run a single unit test, pass in the unit test name for example: tests/app/service/test_rest.py::test_get_template_list
-source environment_test.sh
-py.test $@
+# shellcheck source=/dev/null # Not finding this file in code base
+. environment_test.sh
+py.test "$@"
