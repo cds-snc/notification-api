58fe111ef684302ae2f7d1d8ec2caa3f371474d5
Ben Larabie <benlarabie@gmail.com>
2023-11-20 09:58:24 -0500

    Revert "Moving cwagent check outside of run celery (#2027)"
    This reverts commit a7b67264be0d866bb5f42d98e3768db3cc853a64.

--- scripts/run_celery_send_sms.sh
+++ scripts/run_celery_send_sms.sh
@@ -4,6 +4,27 @@ set -e
 
 # Runs celery with only the send-sms-* queues.
 
+# Check and see if this is running in K8s and if so, wait for cloudwatch agent
+if [ -n "${STATSD_HOST}" ]; then
+    echo "Initializing... Waiting for CWAgent to become ready within the next 30 seconds."
+    timeout=30
+    while [ $timeout -gt 0 ]; do
+        if nc -vz "$STATSD_HOST" 25888; then
+            echo "CWAgent is Ready."
+            break
+        else
+            echo "Waiting for CWAgent to become ready."
+            sleep 1
+            timeout=$((timeout - 1))
+        fi
+    done
+    
+    if [ $timeout -eq 0 ]; then
+        echo "Timeout reached. CWAgent did not become ready in 30 seconds."
+        exit 1
+    fi
+fi
+
 echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
 
 # TODO: we shouldn't be using the send-sms-tasks queue anymore - once we verify this we can remove it
