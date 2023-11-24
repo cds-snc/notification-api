d6f6d05d7201cf5c83bfcd5faba1c50bd0a56dbd
Ben Larabie <ben.larabie@cds-snc.ca>
2023-11-24 14:53:27 -0500

    formatting

--- tests_smoke/smoke_loop.py
+++ tests_smoke/smoke_loop.py
@@ -1,7 +1,8 @@
-import subprocess
 import os
+import subprocess
 import time
 from argparse import ArgumentParser
+
 from smoke.common import Attachment_type, Config, Notification_type  # type: ignore
 from smoke.test_admin_csv import test_admin_csv  # type: ignore
 from smoke.test_admin_one_off import test_admin_one_off  # type: ignore
@@ -11,7 +12,9 @@ from smoke.test_api_one_off import test_api_one_off  # type: ignore
 runCount = 0
 maxRuns = 0
 
+
 class bcolors:
+
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
@@ -22,79 +25,73 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def init():
 
-    global maxRuns 
+    global maxRuns
 
     parser = ArgumentParser()
     parser.add_argument("-m", "--max-runs", dest="maxruns",
                         help="Set the maximum number of runs you want", metavar="MAXRUNS")
     parser.add_argument("-s", "--source-script", dest="source",
-                    help="Pass a shell script to source environment variables from", metavar="SOURCE")
+                        help="Pass a shell script to source environment variables from", metavar="SOURCE")
     args = parser.parse_args()
 
     print(bcolors.OKBLUE)
-    
+
     print("API Smoke test\n")
     for key in ["API_HOST_NAME", "SERVICE_ID", "EMAIL_TEMPLATE_ID", "SMS_TEMPLATE_ID", "EMAIL_TO", "SMS_TO"]:
         print(f"{key:>17}: {Config.__dict__[key]}")
-    print("")    
+    print("")
 
-    if args.maxruns != None:
+    if args.maxruns is not None:
         print(f"Running smoke tests in a loop up to {args.maxruns} times")
         maxRuns = int(args.maxruns)
     else:
-        print(f"Running smoke tests in a loop indefinitely")
+        print("Running smoke tests in a loop indefinitely")
 
     os.chdir('../')
 
-    if args.source != None:
+    if args.source is not None:
         print(f"Sourcing environment variables from external shell script {args.source}")
-        subprocess.run(["source", args.source],executable='/bin/bash')
-    
-    print(bcolors.ENDC)        
+        subprocess.run(["source", args.source], executable='/bin/bash')
+
+    print(bcolors.ENDC)
+
 
 init()
 
 
 while True:
-    
+
     result = None
     seconds = None
     startTime = None
     endTime = None
 
-    try:
-        runCount +=1
-        print(bcolors.OKBLUE)
-        print(f"Running smoke test #{str(runCount)}")
-        print(bcolors.ENDC)        
-        startTime = time.time()
-        
-        for notification_type in [Notification_type.EMAIL, Notification_type.SMS]:
-            test_admin_one_off(notification_type)
-            test_admin_csv(notification_type)
-            test_api_one_off(notification_type)
-            test_api_bulk(notification_type)
-        test_api_one_off(Notification_type.EMAIL, Attachment_type.ATTACHED)
-        test_api_one_off(Notification_type.EMAIL, Attachment_type.LINK)
+    runCount += 1
+    print(bcolors.OKBLUE)
+    print(f"Running smoke test #{str(runCount)}")
+    print(bcolors.ENDC)
+    startTime = time.time()
 
-        print(subprocess.STDOUT)        
-        endTime = time.time() 
-        totalTime = endTime - startTime
-        print(bcolors.OKBLUE)
-        print(f"Smoke Test {str(runCount)} complete in {totalTime} seconds")
-        print(bcolors.ENDC)                
-        if maxRuns != 0:
-            if maxRuns >= runCount:
-                print(bcolors.WARNING)        
-                print(f"Run limit reached. Stopping")
-                print(bcolors.ENDC)          
-                break
+    for notification_type in [Notification_type.EMAIL, Notification_type.SMS]:
+        test_admin_one_off(notification_type)
+        test_admin_csv(notification_type)
+        test_api_one_off(notification_type)
+        test_api_bulk(notification_type)
+    test_api_one_off(Notification_type.EMAIL, Attachment_type.ATTACHED)
+    test_api_one_off(Notification_type.EMAIL, Attachment_type.LINK)
 
-    except:
-
-        print(bcolors.FAIL)
-        print(f"Smoke Test run {str(runCount)} failed")
-        print(bcolors.ENDC)                  
-        break
+    print(subprocess.STDOUT)
+    endTime = time.time()
+    totalTime = endTime - startTime
+    print(bcolors.OKBLUE)
+    print(f"Smoke Test {str(runCount)} complete in {totalTime} seconds")
+    print(bcolors.ENDC)
+    if maxRuns != 0:
+        if maxRuns >= runCount:
+            print(bcolors.WARNING)
+            print("Run limit reached. Stopping")
+            print(bcolors.ENDC)
+            break
