7d917aa6e71c85482d0604f596c2edc416e6598a
Jumana B <jzbahrai@users.noreply.github.com>
2024-01-18 11:45:23 -0500

    Update tests/app/authentication/test_authentication.py
    Co-authored-by: Andrew <andrew.leith@cds-snc.ca>

--- tests/app/authentication/test_authentication.py
+++ tests/app/authentication/test_authentication.py
@@ -141,7 +141,7 @@ def test_should_allow_auth_with_api_key_scheme(client, sample_api_key, scheme):
     assert response.status_code == 200
 
 
-def test_should_allow_NOT_allow_auth_with_api_key_scheme_with_incorrect_format(client, sample_api_key):
+def test_should_NOT_allow_auth_with_api_key_scheme_with_incorrect_format(client, sample_api_key):
     api_key_secret = "fhsdkjhfdsfhsd" + get_unsigned_secret(sample_api_key.id)
 
     response = client.get("/notifications", headers={"Authorization": f"ApiKey-v1 {api_key_secret}"})
