5a7de26c4dbd0ff290e1629d2ac92ce876ebe89b
Stephen Astels <sastels@gmail.com>
2024-03-19 13:49:21 -0400

    format

--- tests/app/dao/test_jobs_dao.py
+++ tests/app/dao/test_jobs_dao.py
@@ -351,7 +351,6 @@ def test_should_get_jobs_seven_days_old_by_scheduled_for_date(sample_service):
 
 @freeze_time("2016-10-31 10:00:00")
 def test_should_get_limited_number_of_jobs(sample_template):
-
     flexable_retention_service1 = create_service(service_name="Another service 1")
     insert_service_data_retention(flexable_retention_service1.id, sample_template.template_type, 3)
     flexable_template1 = create_template(flexable_retention_service1, template_type=sample_template.template_type)
@@ -367,7 +366,7 @@ def test_should_get_limited_number_of_jobs(sample_template):
         create_job(flexable_template1, created_at=four_days_ago)
         create_job(flexable_template2, created_at=four_days_ago)
         create_job(sample_template, created_at=eight_days_ago)
-        
+
     jobs = dao_get_jobs_older_than_data_retention(notification_types=[sample_template.template_type], limit=3)
 
     assert len(jobs) == 3
