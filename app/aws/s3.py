427429a6f4cb6792a222a665be0f16b8be604dd3
Steve Astels <sastels@users.noreply.github.com>
2024-03-19 14:58:02 -0400

    Update app/aws/s3.py
    Co-authored-by: Jimmy Royer <jimleroyer@gmail.com>

--- app/aws/s3.py
+++ app/aws/s3.py
@@ -60,7 +60,7 @@ def get_job_metadata_from_s3(service_id, job_id):
     return obj.get()["Metadata"]
 
 
-def remove_job_batch_from_s3(jobs):
+def remove_jobs_from_s3(jobs):
     bucket = resource("s3").Bucket(current_app.config["CSV_UPLOAD_BUCKET_NAME"])
     object_keys = [FILE_LOCATION_STRUCTURE.format(job.service_id, job.id) for job in jobs]
     bucket.delete_objects(Delete={"Objects": [{"Key": key} for key in object_keys]})
