dc8df4c3f77caffc7e5272a5f37fd1f04667ccf3
Ben Larabie <ben.larabie@cds-snc.ca>
2024-03-21 09:32:41 -0400

    fixing for shellcheck

--- scripts/callManifestsRollout.sh
+++ scripts/callManifestsRollout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GITHUB_SHA=$1
-PAYLOAD={\"ref\":\"main\",\"inputs\":{\"docker_sha\":\"$GITHUB_SHA\"}}
+PAYLOAD="{\"ref\":\"main\",\"inputs\":{\"docker_sha\":\"$GITHUB_SHA\"}}"
 
 
 RESPONSE=$(curl -w '%{http_code}\n' \
@@ -9,9 +9,9 @@ RESPONSE=$(curl -w '%{http_code}\n' \
   -H "Authorization: Bearer $WORKFLOW_PAT" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/cds-snc/notification-manifests/actions/workflows/api-rollout-k8s-staging.yaml/dispatches \
-  -d $PAYLOAD)
+  -d "$PAYLOAD")
 
-if [ $RESPONSE != 204 ]; then
+if [ "$RESPONSE" != 204 ]; then
   echo "ERROR CALLING MANIFESTS ROLLOUT: HTTP RESPONSE: $RESPONSE"
   exit 1
 fi
