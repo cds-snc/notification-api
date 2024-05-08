6e3e94f931b0337d7465610f8ad2cc8ea2cb677a
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2024-05-08 20:27:28 +0000

    fix

--- tests/app/clients/test_freshdesk.py
+++ tests/app/clients/test_freshdesk.py
@@ -172,6 +172,7 @@ class TestSendTicket:
                 "department_org_name": "best org name ever",
                 "service_id": "8624bd36-b70b-4d4b-a459-13e1f4770b92",
                 "branding_url": "branding_url",
+                "branding_logo_name": "branding_log_name",
                 "alt_text_en": "hello world",
                 "alt_text_fr": "bonjour le monde",
             }
