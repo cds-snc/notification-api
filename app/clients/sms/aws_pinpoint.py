727070fa24d09b506e68da1b572275e156d517cd
Stephen Astels <sastels@gmail.com>
2024-05-13 11:56:33 -0400

    add pinpoint tests

--- app/clients/sms/aws_pinpoint.py
+++ app/clients/sms/aws_pinpoint.py
@@ -4,7 +4,6 @@ import boto3
 import phonenumbers
 
 from app.clients.sms import SmsClient
-from app.config import Config
 
 
 class AwsPinpointClient(SmsClient):
@@ -26,10 +25,10 @@ class AwsPinpointClient(SmsClient):
         messageType = "TRANSACTIONAL"
         matched = False
 
-        if template_id is not None and str(template_id) in Config.AWS_PINPOINT_SC_TEMPLATE_IDS:
-            pool_id = Config.AWS_PINPOINT_SC_POOL_ID
+        if template_id is not None and str(template_id) in self.current_app.config["AWS_PINPOINT_SC_TEMPLATE_IDS"]:
+            pool_id = self.current_app.config["AWS_PINPOINT_SC_POOL_ID"]
         else:
-            pool_id = Config.AWS_PINPOINT_DEFAULT_POOL_ID
+            pool_id = self.current_app.config["AWS_PINPOINT_DEFAULT_POOL_ID"]
 
         for match in phonenumbers.PhoneNumberMatcher(to, "US"):
             matched = True
