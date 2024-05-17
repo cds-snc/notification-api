c693cbd01daec15cb8234eb2ffbe2d535bf143e0
Pat Heard <patrick.heard@cds-snc.ca>
2024-05-17 15:53:40 +0000

    fix: prevent migration from running in test databases

---
+++ migrations/versions/0450_set_pgaudit_config.py
@@ -0,0 +1,55 @@
+"""
+
+Revision ID: 0450_set_pgaudit_config
+Revises: 0449_update_magic_link_auth
+Create Date: 2024-05-17 12:00:00
+
+"""
+from alembic import op
+
+revision = "0450_set_pgaudit_config"
+down_revision = "0449_update_magic_link_auth"
+
+users = ["app_db_user", "rdsproxyadmin"]
+database_name = op.get_bind().engine.url.database  # database name that the migration is being run on
+
+
+def upgrade():
+    # Skip this migration in the test database as there are multiple test databases that are created.
+    # This leads to a race condition attempting to alter the same users\ multiple times and causes
+    # sporadic unit test failures.
+    if "test_notification_api" in database_name:
+        return
+
+    for user in users:
+        create_user_if_not_exists(user)
+        op.execute(f"ALTER USER {user} SET pgaudit.log TO 'NONE'")
+
+
+def downgrade():
+    if "test_notification_api" in database_name:
+        return
+
+    # Reset the pgaudit.log setting, but do not remove the roles as they are managed
+    # outside of the API migrations.
+    for user in users:
+        create_user_if_not_exists(user)
+        op.execute(f"ALTER USER {user} RESET pgaudit.log")
+
+
+def create_user_if_not_exists(user):
+    """
+    Makes sure the expected user exists in the database before performing the ALTER USER operation.
+    If the user already exists, nothing happens.  This is needed so that the migrations can be
+    run on localhost where the users do not exist.
+    """
+    op.execute(
+        f"""
+        DO $$
+        BEGIN
+        CREATE USER {user};
+        EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+        END
+        $$;
+    """
+    )
