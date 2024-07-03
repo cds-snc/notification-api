ac90f0dc7c379a1c111c44b1e0799a57f1ac180e
wbanks <william.banks@cds-snc.ca>
2024-07-03 16:04:56 -0400

    Fix silly typo..

--- app/dao/template_categories_dao.py
+++ app/dao/template_categories_dao.py
@@ -70,7 +70,7 @@ def dao_delete_template_category_by_id(template_category_id, cascade=False):
                     else template_category.email_process_type
                 )
                 template.category = dao_get_template_category_by_id(default_category_id)
-                template.updated_at = datetime.utcnow
+                template.updated_at = datetime.utcnow()
                 db.session.add(template)
 
         db.session.delete(template_category)
