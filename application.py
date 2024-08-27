03760e515dbd83b5e0f064babd0f2ae42bce6e57
Jimmy Royer <jimleroyer@gmail.com>
2024-08-27 09:08:53 -0400

    Adding custom context to add a warning level log for X-Ray messages (#2272)
    * Adding custom context to add a warning level log for xray messages
    
    * Set the default logging level at LOG_WARNING by default

--- application.py
+++ application.py
@@ -12,6 +12,7 @@ from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app import create_app
+from app.aws.xray.context import NotifyContext
 
 load_dotenv()
 
@@ -20,7 +21,7 @@ application.wsgi_app = ProxyFix(application.wsgi_app)  # type: ignore
 
 app = create_app(application)
 
-xray_recorder.configure(service='api')
+xray_recorder.configure(service='api', context=NotifyContext())
 XRayMiddleware(app, xray_recorder)
 
 apig_wsgi_handler = make_lambda_handler(app, binary_support=True)
