bbf513e9b71fe68ef91a57ad673d6b7ce4334c3d
Stephen Astels <sastels@gmail.com>
2024-09-09 16:35:51 -0400

    format

--- tests/app/support/test_rest.py
+++ tests/app/support/test_rest.py
@@ -1,51 +1,4 @@
-import json
 import uuid
-from datetime import date, datetime, timedelta
-from functools import partial
-from unittest.mock import ANY, call
-
-import pytest
-import pytest_mock
-from flask import Flask, current_app, url_for
-from freezegun import freeze_time
-from notifications_utils.clients.redis import (
-    daily_limit_cache_key,
-    near_daily_limit_cache_key,
-    over_daily_limit_cache_key,
-)
-
-from app.dao.organisation_dao import dao_add_service_to_organisation
-from app.dao.service_sms_sender_dao import dao_get_sms_senders_by_service_id
-from app.dao.service_user_dao import dao_get_service_user
-from app.dao.services_dao import dao_remove_user_from_service
-from app.dao.templates_dao import dao_redact_template
-from app.dao.users_dao import save_model_user
-from app.dbsetup import RoutingSQLAlchemy
-from app.models import (
-    EMAIL_TYPE,
-    INBOUND_SMS_TYPE,
-    INTERNATIONAL_SMS_TYPE,
-    KEY_TYPE_NORMAL,
-    KEY_TYPE_TEAM,
-    KEY_TYPE_TEST,
-    LETTER_TYPE,
-    SMS_TYPE,
-    EmailBranding,
-    InboundNumber,
-    Notification,
-    Service,
-    ServiceEmailReplyTo,
-    ServiceLetterContact,
-    ServicePermission,
-    ServiceSmsSender,
-    User,
-)
-from tests import create_authorization_header
-from tests.app.conftest import sample_notification as create_sample_notification
-from tests.app.conftest import sample_notification_with_job
-from tests.app.conftest import (
-    sample_user_service_permission as create_user_service_permission,
-)
 
 
 def test_query_id_unknown_uuid(admin_request, sample_user):
@@ -105,4 +58,4 @@ def test_query_id_notification(admin_request, sample_notification_with_job):
     assert json_resp["service_name"] == sample_notification_with_job.service.name
     assert json_resp["job_id"] == str(sample_notification_with_job.job_id)
     assert json_resp["job_row_number"] == sample_notification_with_job.job_row_number
-    assert json_resp["api_key_id"] == None
+    assert json_resp["api_key_id"] is None
