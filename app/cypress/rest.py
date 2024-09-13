00dc686c435452f1a26bba685ea9d4ce6a39624e
Andrew Leith <andrew.leith@cds-snc.ca>
2024-09-13 18:16:01 +0000

    feat(cypress api): update cleanup_stale_users route: - add safeguards to ensure this cant run in prod - remove/update related tables

--- app/cypress/rest.py
+++ app/cypress/rest.py
@@ -1,12 +1,22 @@
+import hashlib
+import logging
 import uuid
-from flask import Blueprint, jsonify, request
+
+from datetime import datetime, timedelta
+from flask import Blueprint, current_app, jsonify, request
 
 from app import db
 from app.dao.services_dao import dao_add_user_to_service
 from app.dao.templates_dao import dao_update_template
 from app.dao.users_dao import save_model_user
 from app.errors import register_errors
-from app.models import LoginEvent, Permission, Service, ServiceUser, Template, TemplateHistory, User, VerifyCode
+from app.models import LoginEvent, Permission, Service, ServiceUser, Template, TemplateHistory, TemplateRedacted, User, VerifyCode
+
+""" 
+This module will be used by the cypress tests to create users on the fly whenever a test suite is run. 
+
+Additionally, this module will also be used to clean up test users periodically to keep the data footprint small.
+"""
 
 cypress_blueprint = Blueprint("cypress", __name__)
 register_errors(cypress_blueprint)
@@ -41,57 +51,95 @@ def create_test_user(email_name):
     
     dao_add_user_to_service(service, user, permissions=permissions)
 
-    # things to delete
-    # login_events
-    # verify_codes
-
     return jsonify(user.serialize()), 201
 
 
-@cypress_blueprint.route("/destroy_user/<email_name>", methods=["GET"])
-def destroy_user(email_name):
+def _destroy_test_user(email_name):
+    CYPRESS_TEST_USER_ID = current_app.config["CYPRESS_TEST_USER_ID"]
+
     user = User.query.filter_by(email_address=f"notify-ui-tests+{email_name}@cds-snc.ca").first()
-    
+
     if not user:
         return
     
-    # update the created_by field for each template to use id NOTIFY_TEST_USER_ID
-    templates = Template.query.filter_by(created_by=user).all()
-    for template in templates:
-        template.created_by_id = NOTIFY_TEST_USER_ID
-        dao_update_template(template)
+    try:
+        # update the created_by field for each template to use id CYPRESS_TEST_USER_ID
+        templates = Template.query.filter_by(created_by=user).all()
+        for template in templates:
+            template.created_by_id = CYPRESS_TEST_USER_ID
+            dao_update_template(template)
 
-    # update the created_by field for each template to use id NOTIFY_TEST_USER_ID
-    history_templates = TemplateHistory.query.filter_by(created_by=user).all()
-    for templateh in history_templates:
-        templateh.created_by_id = NOTIFY_TEST_USER_ID
-        db.session.add(templateh)
+        # update the created_by field for each template to use id CYPRESS_TEST_USER_ID
+        history_templates = TemplateHistory.query.filter_by(created_by=user).all()
+        for templateh in history_templates:
+            templateh.created_by_id = CYPRESS_TEST_USER_ID
+            db.session.add(templateh)
 
-    # remove all the login events for this user
-    LoginEvent.query.filter_by(user=user).delete()
+        # update the created_by field for each template_redacted to use id CYPRESS_TEST_USER_ID
+        redacted_templates = TemplateRedacted.query.filter_by(updated_by=user).all()
+        for templater in redacted_templates:
+            templater.updated_by_id = CYPRESS_TEST_USER_ID
+            db.session.add(templater)
 
-    # remove all permissions for this user
-    Permission.query.filter_by(user=user).delete()
-    
-    # remove user_to_service entries
-    ServiceUser.query.filter_by(user_id=user.id).delete()
+        # Update services create by this user to use CYPRESS_TEST_USER_ID
+        services = Service.query.filter_by(created_by=user).all()
+        for service in services:
+            service.created_by_id = CYPRESS_TEST_USER_ID
+            db.session.add(service)
+     
+        # remove all the login events for this user
+        LoginEvent.query.filter_by(user=user).delete()
 
-    # remove verify codes
-    VerifyCode.query.filter_by(user=user).delete()
-    
-    # remove the user
-    User.query.filter_by(email_address=f"notify-ui-tests+{email_name}@cds-snc.ca").delete()
+        # remove all permissions for this user
+        Permission.query.filter_by(user=user).delete()
+        
+        # remove user_to_service entries
+        ServiceUser.query.filter_by(user_id=user.id).delete()
 
-    db.session.commit()
+        # remove verify codes
+        VerifyCode.query.filter_by(user=user).delete()
+        
+        # remove the user
+        User.query.filter_by(email_address=f"notify-ui-tests+{email_name}@cds-snc.ca").delete()
 
-    return jsonify(message="Zeds dead, baby"), 201
+        print("removal success: " + email_name)
+    except Exception as e:
+        print(f"Error cleaning up test user: {e}")
+        db.session.rollback()
+        
 
+"""
+Endpoint for cleaning up stale users.  This endpoint will only be used internally by the Cypress tests.
+
+This endpoint is responsible for removing stale testing users from the database. 
+Stale users are identified as users whose email addresses match the pattern "%notify-ui-tests+%@cds-snc.ca%" and whose creation time is older than three hours ago.
+
+Returns:
+    A JSON response with a success message if the cleanup is successful, or an error message if an exception occurs during the cleanup process.
+"""
 @cypress_blueprint.route("/cleanup", methods=["GET"])
-def cleanup_users():
-    users = User.query.filter(User.email_address.like(f"%notify-ui-tests+%@cds-snc.ca%"))
+def cleanup_stale_users():
+    if current_app.config["NOTIFY_ENVIRONMENT"] == "production":
+        return jsonify(message="Forbidden"), 403
+
+    three_hours_ago = datetime.utcnow() - timedelta(hours=3)
+    users = User.query.filter(User.email_address.like(f"%notify-ui-tests+%@cds-snc.ca%"),
+                              User.created_at < three_hours_ago).all()
+
+    # get the list of email_address property from users
+    users_emails = [user for user in users]
+
+    print('users to clean: ' + str(users_emails))
 
     # loop through users and call destroy_user on each one
     for user in users:
         user_email = user.email_address.split("+")[1].split("@")[0]
-        print(user_email)
-        destroy_user(user_email)
+        print("Trying to remove:" + user_email)
+
+        try:
+            _destroy_test_user(user_email)
+        except Exception as e:
+            return jsonify(message="Error cleaning up"), 500
+
+    db.session.commit()
+    return jsonify(message="Zeds dead, baby"), 201
