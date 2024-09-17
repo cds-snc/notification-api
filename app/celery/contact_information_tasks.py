426144cc0de9e5e3d531414960be8a56bf7507a5
Adam King <adam.king0126@gmail.com>
2024-09-17 18:08:50 -0500

    refactor: update docstrings, typing in contact_information_tasks.py

--- app/celery/contact_information_tasks.py
+++ app/celery/contact_information_tasks.py
@@ -1,4 +1,5 @@
 from app import notify_celery, va_profile_client
+from celery import Task
 from app.celery.common import can_retry, handle_max_retries_exceeded
 from app.celery.exceptions import AutoRetryException
 from app.celery.service_callback_tasks import check_and_queue_callback_task
@@ -10,6 +11,7 @@ from app.models import (
     NOTIFICATION_PREFERENCES_DECLINED,
     EMAIL_TYPE,
     SMS_TYPE,
+    Notification,
     RecipientIdentifier,
 )
 from app.va.identifier import IdentifierType
@@ -36,9 +38,27 @@ from requests import Timeout
 )
 @statsd(namespace='tasks')
 def lookup_contact_info(
-    self,
-    notification_id,
+    self: Task,
+    notification_id: str,
 ):
+    """
+    Celery task to look up contact information (email/phone number) for a given notification.
+    If the feature flag, VA_PROFILE_V3_COMBINE_CONTACT_INFO_AND_PERMISSIONS_LOOKUP, is enabled,
+    also check for related communication permissions.
+
+    Args:
+        self (Task): The Celery task instance.
+        notification_id (str): The ID of the notification for which to look up contact information.
+
+    Raises:
+        AutoRetryException: If a retryable exception occurs during the lookup process.
+        NotificationTechnicalFailureException: If the maximum retries have been exceeded.
+        NotificationPermanentFailureException: If the exception indicates a permanent failure.
+        Exception: If an unhandled exception occurs.
+
+    Returns:
+        None
+    """
     current_app.logger.info('Looking up contact information for notification_id: %s.', notification_id)
 
     notification = get_notification_by_id(notification_id)
@@ -47,9 +67,7 @@ def lookup_contact_info(
 
     try:
         if is_feature_enabled(FeatureFlag.VA_PROFILE_V3_COMBINE_CONTACT_INFO_AND_PERMISSIONS_LOOKUP):
-            result = get_profile_result(
-                notification.notification_type, notification_id, recipient_identifier, should_send
-            )
+            result = get_profile_result(notification, recipient_identifier)
             recipient = result.recipient
             should_send = result.communication_allowed
             permission_message = result.permission_message
@@ -60,7 +78,7 @@ def lookup_contact_info(
                 recipient_identifier,
             )
     except Exception as e:
-        handle_lookup_contact_info_exception(self, notification, notification_id, recipient_identifier, should_send, e)
+        handle_lookup_contact_info_exception(self, notification, recipient_identifier, e)
 
     notification.to = recipient
     dao_update_notification(notification)
@@ -97,65 +115,79 @@ def get_recipient(
 
 
 def get_profile_result(
-    notification_type: str,
-    notification_id: str,
+    notification: Notification,
     recipient_identifier: RecipientIdentifier,
-    default_send: bool,
 ) -> VAProfileResult:
     """
     Retrieve the result of looking up contact info from VA Profile.
 
     Args:
-        notification_type (str): The type of contact info requested.
-        notification_id (str): The notification ID associated with this request.
+        notification (Notification): The Notification object to get contact info and permissions for.
         recipient_identifier (RecipientIdentifier): The VA profile ID to retrieve the profile for.
-        # communication_item_id_for_permission_check (str): The communication_item_id to use for checking permissions.
 
     Returns:
         VAProfileResult: The contact info result from VA Profile.
     """
-    if notification_type == EMAIL_TYPE:
-        return va_profile_client.get_email_with_permission(recipient_identifier, default_send)
-    elif notification_type == SMS_TYPE:
-        return va_profile_client.get_telephone_with_permission(recipient_identifier, default_send)
+    if notification.type == EMAIL_TYPE:
+        return va_profile_client.get_email_with_permission(recipient_identifier, notification.default_send)
+    elif notification.type == SMS_TYPE:
+        return va_profile_client.get_telephone_with_permission(recipient_identifier, notification.default_send)
     else:
         raise NotImplementedError(
-            f'The task lookup_contact_info failed for notification {notification_id}. '
-            f'{notification_type} is not supported'
+            f'The task lookup_contact_info failed for notification {notification.id}. '
+            f'{notification.type} is not supported'
         )
 
 
 def handle_lookup_contact_info_exception(
-    lookup_task, notification, notification_id, recipient_identifier, default_send_flag, e
+    lookup_task: Task, notification: Notification, recipient_identifier: RecipientIdentifier, e: Exception
 ):
+    """
+    Handles exceptions that occur during the lookup of contact information.
+
+    Args:
+        lookup_task (Task): The task object that is performing the lookup.
+        notification (Notification): The notification object associated with the lookup.
+        recipient_identifier (RecipientIdentifier): The identifier of the recipient.
+        e (Exception): The exception that was raised during the lookup.
+
+    Raises:
+        AutoRetryException: If the exception is retryable and the task can be retried.
+        NotificationTechnicalFailureException: If the maximum retries have been exceeded.
+        NotificationPermanentFailureException: If the exception indicates a permanent failure.
+        Exception: If an unhandled exception occurs.
+
+    Returns:
+        str or None: A message indicating the result of the exception handling, or None if no action is needed.
+    """
     if isinstance(e, (Timeout, VAProfileRetryableException)):
-        if can_retry(lookup_task.request.retries, lookup_task.max_retries, notification_id):
-            current_app.logger.warning('Unable to get contact info for notification id: %s, retrying', notification_id)
+        if can_retry(lookup_task.request.retries, lookup_task.max_retries, notification.id):
+            current_app.logger.warning('Unable to get contact info for notification id: %s, retrying', notification.id)
             raise AutoRetryException(f'Found {type(e).__name__}, autoretrying...', e, e.args)
         else:
-            msg = handle_max_retries_exceeded(notification_id, 'lookup_contact_info')
+            msg = handle_max_retries_exceeded(notification.id, 'lookup_contact_info')
             check_and_queue_callback_task(notification)
             raise NotificationTechnicalFailureException(msg)
     elif isinstance(e, NoContactInfoException):
         message = (
-            f"Can't proceed after querying VA Profile for contact information for {notification_id}. "
+            f"Can't proceed after querying VA Profile for contact information for {notification.id}. "
             'Stopping execution of following tasks. Notification has been updated to permanent-failure.'
         )
         current_app.logger.warning('%s - %s:  %s', e.__class__.__name__, str(e), message)
 
         update_notification_status_by_id(
-            notification_id, NOTIFICATION_PERMANENT_FAILURE, status_reason=e.failure_reason
+            notification.id, NOTIFICATION_PERMANENT_FAILURE, status_reason=e.failure_reason
         )
         check_and_queue_callback_task(notification)
         raise NotificationPermanentFailureException(message) from e
     elif isinstance(e, (VAProfileIDNotFoundException, VAProfileNonRetryableException)):
         current_app.logger.exception(e)
         message = (
-            f'The task lookup_contact_info failed for notification {notification_id}. '
+            f'The task lookup_contact_info failed for notification {notification.id}. '
             'Notification has been updated to permanent-failure'
         )
         update_notification_status_by_id(
-            notification_id, NOTIFICATION_PERMANENT_FAILURE, status_reason=e.failure_reason
+            notification.id, NOTIFICATION_PERMANENT_FAILURE, status_reason=e.failure_reason
         )
         check_and_queue_callback_task(notification)
         raise NotificationPermanentFailureException(message) from e
@@ -163,28 +195,40 @@ def handle_lookup_contact_info_exception(
         current_app.logger.info(
             'Communication item for recipient %s not found on notification %s',
             recipient_identifier.id_value,
-            notification_id,
+            notification.id,
         )
 
-        return None if default_send_flag else 'No recipient opt-in found for explicit preference'
+        return None if notification.default_send else 'No recipient opt-in found for explicit preference'
     else:
-        current_app.logger.exception(f'Unhandled exception for notification {notification_id}: {e}')
+        current_app.logger.exception(f'Unhandled exception for notification {notification.id}: {e}')
         raise e
 
 
-def handle_communication_not_allowed(notification, recipient_identifier, permission_message):
+def handle_communication_not_allowed(
+    notification: Notification, recipient_identifier: RecipientIdentifier, permission_message: str
+):
+    """
+    Handles the scenario where communication is not allowed for a given notification.
+
+    Args:
+        notification (Notification): The notification object associated with the communication.
+        recipient_identifier (RecipientIdentifier): The identifier of the recipient.
+        permission_message (str): The message indicating the reason for permission denial.
+
+    Raises:
+        NotificationPermanentFailureException: If the recipient has declined permission to receive notifications.
+    """
     if is_feature_enabled(FeatureFlag.VA_PROFILE_V3_COMBINE_CONTACT_INFO_AND_PERMISSIONS_LOOKUP):
         current_app.logger.info(
             'Permission denied for recipient %s for notification %s',
             recipient_identifier.id_value,
             notification.id,
         )
-        if permission_message is not None:
-            reason = permission_message
-        else:
-            reason = 'Contact preferences set to false'
+        reason = permission_message if permission_message is not None else 'Contact preferences set to false'
         update_notification_status_by_id(notification.id, NOTIFICATION_PREFERENCES_DECLINED, status_reason=reason)
+
         message = f'The recipient for notification {notification.id} has declined permission to receive notifications.'
         current_app.logger.info(message)
+
         check_and_queue_callback_task(notification)
         raise NotificationPermanentFailureException(message)
