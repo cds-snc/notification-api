69244c659323db1af993fcd15a78a070df529742
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2024-09-23 15:08:11 +0000

    fix

--- app/celery/reporting_tasks.py
+++ app/celery/reporting_tasks.py
@@ -83,7 +83,7 @@ def create_nightly_notification_status_for_day(process_day):
     """
     process_day = datetime.strptime(process_day, "%Y-%m-%d").date()
     service_ids = [x.id for x in Service.query.all()]
-    chunk_size = 100
+    chunk_size = 20
     iter_service_ids = iter(service_ids)
 
     while True:
