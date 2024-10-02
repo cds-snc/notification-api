1e9e1545023ddb59860ab0206e7628c403013e05
Jimmy Royer <jimleroyer@gmail.com>
2024-10-02 18:15:43 -0400

    Reduced # of gunicorn workers

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,7 +7,7 @@ import newrelic.agent  # See https://bit.ly/2xBVKBH
 
 newrelic.agent.initialize()  # noqa: E402
 
-workers = 4
+workers = 1
 worker_class = "gevent"
 worker_connections = 256
 bind = "0.0.0.0:{}".format(os.getenv("PORT"))
