4f67c2223faae071b41d4b50b5bbe81f247f17d0
Andrew Leith <andrew.leith@cds-snc.ca>
2024-11-08 12:41:27 +0000

    feat(get_monthly_notification_stats): add option to exclude today's data, so that admin can cache the result

--- app/service/rest.py
+++ app/service/rest.py
@@ -622,6 +622,11 @@ def get_monthly_notification_stats(service_id):
         year = int(request.args.get("year", "NaN"))
     except ValueError:
         raise InvalidRequest("Year must be a number", status_code=400)
+    
+    try:
+        exclude_today = request.args.get("exclude_today", "false").lower() in ("true", "1", "yes")
+    except ValueError:
+        raise InvalidRequest("exclude_today must be a bool", status_code=400)
 
     start_date, end_date = get_financial_year(year)
 
@@ -631,9 +636,11 @@ def get_monthly_notification_stats(service_id):
     statistics.add_monthly_notification_status_stats(data, stats)
 
     now = datetime.utcnow()
-    if end_date > now:
-        todays_deltas = fetch_notification_status_for_service_for_day(convert_utc_to_local_timezone(now), service_id=service_id)
-        statistics.add_monthly_notification_status_stats(data, todays_deltas)
+    
+    if not exclude_today:
+        if end_date > now:
+            todays_deltas = fetch_notification_status_for_service_for_day(convert_utc_to_local_timezone(now), service_id=service_id)
+            statistics.add_monthly_notification_status_stats(data, todays_deltas)
 
     return jsonify(data=data)
 
