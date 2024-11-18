bcb270a6d65289887504deab2dd0a9897454d8d6
Stephen Astels <sastels@gmail.com>
2024-11-18 14:40:16 -0500

    another file but the same as the first so easy peasy

--- tests/app/test_sms_fragment_utils.py
+++ tests/app/test_sms_fragment_utils.py
@@ -21,9 +21,10 @@ def test_fetch_todays_requested_sms_count(client, mocker, sample_service, redis_
 
         assert actual_result == expected_result
         if redis_value is None:
-            assert mocked_set.called_once_with(
+            mocked_set.assert_called_once_with(
                 cache_key,
                 db_value,
+                ex=7200,
             )
         else:
             mocked_set.assert_not_called()
@@ -33,17 +34,9 @@ def test_fetch_todays_requested_sms_count(client, mocker, sample_service, redis_
 def test_increment_todays_requested_sms_count(mocker, sample_service, redis_value, db_value, increment_by):
     cache_key = sms_daily_count_cache_key(sample_service.id)
     mocker.patch("app.redis_store.get", lambda x: redis_value if x == cache_key else None)
-    mocked_set = mocker.patch("app.redis_store.set")
     mocked_incrby = mocker.patch("app.redis_store.incrby")
     mocker.patch("app.sms_fragment_utils.fetch_todays_requested_sms_count", return_value=db_value)
 
     increment_todays_requested_sms_count(sample_service.id, increment_by)
 
-    assert mocked_incrby.called_once_with(cache_key, increment_by)
-    if redis_value is None:
-        assert mocked_set.called_once_with(
-            cache_key,
-            db_value,
-        )
-    else:
-        mocked_set.assert_not_called()
+    mocked_incrby.assert_called_once_with(cache_key, increment_by)
