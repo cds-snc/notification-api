b23997f774a4636cf55616bf0084b0b3a8b137b8
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2024-12-19 11:08:06 -0500

    HOTFIX - Update Comp and Pen Cron due to Holidays (#2192)

--- app/config.py
+++ app/config.py
@@ -319,7 +319,7 @@ class Config(object):
             'send-scheduled-comp-and-pen-sms': {
                 'task': 'send-scheduled-comp-and-pen-sms',
                 # Every 1 minute past every hour from 13 through 21 on every day-of-month from 22 through end-of-month
-                'schedule': crontab(hour='13-21', day_of_month='22-31', minute='*/1'),
+                'schedule': crontab(hour='13-21', day_of_month='23-31', minute='*/1'),
                 'options': {'queue': QueueNames.PERIODIC},
             },
             'update-twilio-status': {
