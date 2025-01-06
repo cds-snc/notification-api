bf9f4028d95d4a3cbcfd1261eee8d01cc09f37ba
David Kalbfleisch <1.21e9W@protonmail.com>
2025-01-06 14:11:51 -0500

    #1748 Remove the update_service_inbound_api_schema and associated tests. (#2225)

--- app/service/service_callback_api_schema.py
+++ app/service/service_callback_api_schema.py
@@ -1,20 +1,6 @@
 from app.constants import NOTIFICATION_STATUS_TYPES_COMPLETED, CALLBACK_CHANNEL_TYPES, SERVICE_CALLBACK_TYPES
 from app.schema_validation.definitions import uuid, https_url
 
-update_service_inbound_api_schema = {
-    '$schema': 'http://json-schema.org/draft-04/schema#',
-    'description': 'POST service callback/inbound api schema',
-    'type': 'object',
-    'title': 'Create service callback/inbound api',
-    'properties': {
-        'url': https_url,
-        'bearer_token': {'type': 'string', 'minLength': 10},
-        'notification_statuses': {'type': 'array', 'items': {'enum': NOTIFICATION_STATUS_TYPES_COMPLETED}},
-        'updated_by_id': uuid,
-    },
-    'required': ['updated_by_id'],
-}
-
 create_service_callback_api_request_schema = {
     '$schema': 'http://json-schema.org/draft-04/schema#',
     'description': 'POST service callback/inbound api schema',
