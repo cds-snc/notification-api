29beb9c34da1e5380f78371e2dd9357a7434402e
Stephen Astels <sastels@gmail.com>
2025-01-16 16:06:05 -0500

    add celery receipts script

---
+++ scripts/run_celery_receipts.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# Runs celery with only the delivery-receipts queue.
+
+echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
+
+celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency="${CELERY_CONCURRENCY-4}" -Q delivery-receipts
