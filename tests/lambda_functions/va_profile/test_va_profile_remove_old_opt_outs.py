b2f2d319c97ad363b340badf281f6e0d926b73c9
Mack Halliday <Mackenzie.Halliday@gmail.com>
2025-01-28 09:13:15 -0500

    #1636 - BUG remove all opt-out unit tests failures (#2270)

--- tests/lambda_functions/va_profile/test_va_profile_remove_old_opt_outs.py
+++ tests/lambda_functions/va_profile/test_va_profile_remove_old_opt_outs.py
@@ -40,7 +40,8 @@ def test_remove_opted_out_records_query(notify_db_session, sample_va_profile_loc
         # This tests the lambda handler, which calls the stored procedure.
         va_profile_remove_old_opt_outs_handler()
 
-    # TODO 1636 - This should not be commented out.
-    # assert notify_db_session.session.get(VAProfileLocalCache, opt_out_id) is None
+    notify_db_session.session.expire_all()
+
+    assert notify_db_session.session.get(VAProfileLocalCache, opt_out_id) is None
     assert notify_db_session.session.get(VAProfileLocalCache, opt_out_newer.id) is not None
     assert notify_db_session.session.get(VAProfileLocalCache, opt_in.id) is not None
