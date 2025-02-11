29bdeb09129e4e9d76f856da551bbb7f585a5c8f
wbanks <william.banks@cds-snc.ca>
2025-02-11 15:50:00 -0500

    Rename pinpoint-failure status to provider-failure

---
+++ migrations/versions/0475_change_notification_status.py
@@ -0,0 +1,24 @@
+"""
+
+Revision ID: 0475_change_notification_status
+Revises: 0474_add_feedback_reason_column
+Create Date: 2025-02-11 15:37:00 EST
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = "0475_change_notification_status"
+down_revision = "0474_add_feedback_reason_column"
+
+new_notification_status = "pinpoint-failure"
+
+
+def upgrade():
+    op.execute("UPDATE notification_status_types set name = 'provider-failure' where name = 'pinpoint-failure'")
+    pass
+
+
+def downgrade():
+    pass
