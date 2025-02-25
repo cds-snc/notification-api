53895cf30a435254fe08bc980824062e75a6f4f3
Stephen McMurtry <smcmurtry@gmail.com>
2025-02-25 20:58:02 +0000

    format

--- app/dao/api_key_dao.py
+++ app/dao/api_key_dao.py
@@ -73,15 +73,12 @@ def update_last_used_api_key(api_key_id, last_used=None) -> None:
     """
     Update the last_used_timestamp of an API key using a direct SQLAlchemy update.
     Using update() directly is more efficient than loading the model instance.
-    Setting `synchronize_session=False` improves performance and can be used since we 
+    Setting `synchronize_session=False` improves performance and can be used since we
     don't need to access the updated value in the same session.
     """
     timestamp = last_used if last_used else datetime.utcnow()
-    
-    ApiKey.query.filter_by(id=api_key_id).update(
-        {"last_used_timestamp": timestamp},
-        synchronize_session=False
-    )
+
+    ApiKey.query.filter_by(id=api_key_id).update({"last_used_timestamp": timestamp}, synchronize_session=False)
 
 
 @transactional
