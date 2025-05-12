b69ca7bfce251046dd8a446e659aeec633ec4116
Stephen McMurtry <smcmurtry@gmail.com>
2025-05-12 19:21:57 +0000

    moved logic out of process_pinpoint_receipts

--- app/annual_limit_utils.py
+++ app/annual_limit_utils.py
@@ -16,27 +16,56 @@ from app.models import EMAIL_TYPE, SMS_TYPE
 from app.utils import get_fiscal_year, prepare_notification_counts_for_seeding
 
 
+def seed_annual_limit_counts(service_id: UUID) -> None:
+    """
+    Seed the annual limit counts for a service. This is called when the service is created or updated.
+    """
+    today = datetime.now(timezone.utc)
+    annual_data_sms = fetch_notification_status_totals_for_service_by_fiscal_year(
+        service_id, get_fiscal_year(today), notification_type=SMS_TYPE
+    )
+    annual_data_email = fetch_notification_status_totals_for_service_by_fiscal_year(
+        service_id, get_fiscal_year(today), notification_type=EMAIL_TYPE
+    )
+    data = prepare_notification_counts_for_seeding(
+        fetch_notification_status_for_service_for_day(
+            datetime.now(timezone.utc),
+            service_id=service_id,
+        )
+    )
+    data[TOTAL_SMS_FISCAL_YEAR_TO_YESTERDAY] = annual_data_sms
+    data[TOTAL_EMAIL_FISCAL_YEAR_TO_YESTERDAY] = annual_data_email
+
+    # The below function will also set the SEEDED_AT key for notifications_v2 in redis
+    annual_limit_client.seed_annual_limit_notifications(service_id, data)
+    return data
+
+
 @requires_feature("REDIS_ENABLED")
 def get_annual_limit_notifications_v2(service_id: UUID) -> dict:
     if not annual_limit_client.was_seeded_today(service_id):
-        today = datetime.now(timezone.utc)
-        annual_data_sms = fetch_notification_status_totals_for_service_by_fiscal_year(
-            service_id, get_fiscal_year(today), notification_type=SMS_TYPE
-        )
-        annual_data_email = fetch_notification_status_totals_for_service_by_fiscal_year(
-            service_id, get_fiscal_year(today), notification_type=EMAIL_TYPE
-        )
-        data = prepare_notification_counts_for_seeding(
-            fetch_notification_status_for_service_for_day(
-                datetime.now(timezone.utc),
-                service_id=service_id,
-            )
-        )
-        data[TOTAL_SMS_FISCAL_YEAR_TO_YESTERDAY] = annual_data_sms
-        data[TOTAL_EMAIL_FISCAL_YEAR_TO_YESTERDAY] = annual_data_email
-
-        # The below function will also set the SEEDED_AT key for notifications_v2 in redis
-        annual_limit_client.seed_annual_limit_notifications(service_id, data)
-        return data
+        seed_annual_limit_counts(service_id)
     else:
         return annual_limit_client.get_all_notification_counts(service_id)
+
+
+@requires_feature("REDIS_ENABLED")
+def increment_notification_failed(service_id: UUID, notification_type) -> None:
+    if not annual_limit_client.was_seeded_today(service_id):
+        seed_annual_limit_counts(service_id)
+    else:
+        if notification_type == EMAIL_TYPE:
+            annual_limit_client.increment_email_failed(service_id)
+        elif notification_type == SMS_TYPE:
+            annual_limit_client.increment_sms_failed(service_id)
+
+
+@requires_feature("REDIS_ENABLED")
+def increment_notification_delivered(service_id: UUID, notification_type) -> None:
+    if not annual_limit_client.was_seeded_today(service_id):
+        seed_annual_limit_counts(service_id)
+    else:
+        if notification_type == EMAIL_TYPE:
+            annual_limit_client.increment_email_delivered(service_id)
+        elif notification_type == SMS_TYPE:
+            annual_limit_client.increment_sms_delivered(service_id)
