96ec627683bf2edcaa81d72b621b72dfbba6d087
David Kalbfleisch <1.21e9W@protonmail.com>
2025-06-06 15:58:20 -0400

    #2371 Communication item dao unit testing (#2485)

--- app/dao/communication_item_dao.py
+++ app/dao/communication_item_dao.py
@@ -1,19 +1,7 @@
-import uuid
-from typing import List
-
 from sqlalchemy import select
 
 from app import db
 from app.models import CommunicationItem
-
-
-def dao_create_communication_item(communication_item: CommunicationItem):
-    communication_item.id = communication_item.id if communication_item.id else uuid.uuid4()
-    db.session.add(communication_item)
-
-
-def get_communication_items() -> List[CommunicationItem]:
-    return db.session.scalars(select(CommunicationItem)).all()
 
 
 def get_communication_item(communication_item_id) -> CommunicationItem:
--- tests/app/dao/test_communication_item_dao.py
+++ tests/app/dao/test_communication_item_dao.py
@@ -1,3 +1,17 @@
-class TestGetCommunicationItems:
-    # Issue #2371
-    ...
+from uuid import uuid4
+
+import pytest
+from sqlalchemy.orm.exc import NoResultFound
+
+from app.dao.communication_item_dao import get_communication_item
+
+
+def test_get_communication_item(sample_communication_item):
+    communication_item = sample_communication_item()
+    communication_item_from_dao = get_communication_item(str(communication_item.id))
+    assert communication_item.id == communication_item_from_dao.id
+
+
+def test_get_communication_item_not_found():
+    with pytest.raises(NoResultFound):
+        get_communication_item(str(uuid4()))
