7cab61a29ffcd5bad345b88d19396f68fb7e48d2
Stephen McMurtry <smcmurtry@gmail.com>
2025-06-09 20:30:10 +0000

    add migration

---
+++ migrations/versions/0483_index_jobs.py
@@ -0,0 +1,33 @@
+"""
+
+Revision ID: 0483_index_jobs
+Revises: 0482_index_facts
+Create Date: 2025-06-09 20:01:02.943393
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '0483_index_jobs'
+down_revision = '0482_index_facts'
+
+def index_exists(name):
+    connection = op.get_bind()
+    result = connection.execute(
+        "SELECT exists(SELECT 1 from pg_indexes where indexname = '{}') as ix_exists;".format(name)
+    ).first()
+    return result.ix_exists
+
+def upgrade():
+    if not index_exists("ix_jobs_created_at"):
+        op.create_index(op.f('ix_jobs_created_at'), 'jobs', ['created_at'], unique=False)
+    if not index_exists("ix_jobs_processing_started"):
+        op.create_index(op.f('ix_jobs_processing_started'), 'jobs', ['processing_started'], unique=False)
+
+
+def downgrade():
+    if index_exists("ix_jobs_processing_started"):
+        op.drop_index(op.f('ix_jobs_processing_started'), table_name='jobs')
+    if index_exists("ix_jobs_created_at"):
+        op.drop_index(op.f('ix_jobs_created_at'), table_name='jobs')
