a780d6e47bbcf668e0921ae0c904c978073b4444
William B <7444334+whabanks@users.noreply.github.com>
2025-06-10 15:43:50 -0300

    Add v2 API endpoint to fetch the openAPI spec (#2554)
    * Add v2 API endpoint to fetch the openAPI spec
    
    - Supports EN and FR versions of the spec by passing in the lang query
      param
    
    * Split and rename endpoints
    
    * The endpoints are now `/v2/openapi-{en|fr}` instead of using a query
      string
    * The spec endpoint no longer requires auth
    
    * Make route paths consistent

---
+++ app/v2/api_spec/get_api_spec.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Blueprint, current_app, send_file
+
+v2_api_spec_blueprint = Blueprint("v2_api_spec", __name__, url_prefix="/v2")
+
+
+@v2_api_spec_blueprint.route("/openapi-en", methods=["GET"])
+def get_v2_api_spec_en():
+    """
+    Returns the English OpenAPI specification for the v2 Notifications API.
+    """
+    spec_path = os.path.join(current_app.root_path, "openapi/v2-notifications-api-en.yaml")
+    spec_path = os.path.abspath(spec_path)
+
+    return send_file(spec_path, mimetype="application/yaml", as_attachment=False, download_name="v2-notifications-api.yaml")
+
+
+@v2_api_spec_blueprint.route("/openapi-fr", methods=["GET"])
+def get_v2_api_spec_fr():
+    """
+    Returns the French OpenAPI specification for the v2 Notifications API.
+    """
+    spec_path = os.path.join(current_app.root_path, "openapi/v2-notifications-api-fr.yaml")
+    spec_path = os.path.abspath(spec_path)
+
+    return send_file(spec_path, mimetype="application/yaml", as_attachment=False, download_name="v2-notifications-api.yaml")
