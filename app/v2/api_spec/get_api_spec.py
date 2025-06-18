10e75ecf6e1c14ff21d446f8f38f1a60d6692b56
wbanks <william.banks@cds-snc.ca>
2025-06-18 16:45:52 -0400

    Exclude application/yaml from binary content types

--- app/v2/api_spec/get_api_spec.py
+++ app/v2/api_spec/get_api_spec.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Blueprint, current_app, send_file
+import yaml
+from flask import Blueprint, Response, current_app
 
 v2_api_spec_blueprint = Blueprint("v2_api_spec", __name__, url_prefix="/v2")
 
@@ -13,7 +14,12 @@ def get_v2_api_spec_en():
     spec_path = os.path.join(current_app.root_path, "openapi/v2-notifications-api-en.yaml")
     spec_path = os.path.abspath(spec_path)
 
-    return send_file(spec_path, mimetype="application/yaml", as_attachment=False, download_name="v2-notifications-api.yaml")
+    with open(spec_path, "r") as f:
+        yaml_data = yaml.safe_load(f)
+
+    yaml_string = yaml.dump(yaml_data, default_flow_style=False)
+    response = Response(yaml_string, mimetype="application/yaml")
+    return response
 
 
 @v2_api_spec_blueprint.route("/openapi-fr", methods=["GET"])
@@ -24,4 +30,8 @@ def get_v2_api_spec_fr():
     spec_path = os.path.join(current_app.root_path, "openapi/v2-notifications-api-fr.yaml")
     spec_path = os.path.abspath(spec_path)
 
-    return send_file(spec_path, mimetype="application/yaml", as_attachment=False, download_name="v2-notifications-api.yaml")
+    with open(spec_path, "r") as f:
+        yaml_data = f.read()
+
+    response = Response(yaml_data, mimetype="application/yaml")
+    return response
