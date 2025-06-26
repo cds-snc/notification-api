e5532f6efb5e784eb8dba327c6a97365ed6237c8
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2025-06-26 21:11:18 +0000

    start

--- app/models.py
+++ app/models.py
@@ -168,6 +168,7 @@ class User(BaseModel):
     blocked = db.Column(db.Boolean, nullable=False, default=False)
     additional_information = db.Column(JSONB(none_as_null=True), nullable=True, default={})
     password_expired = db.Column(db.Boolean, nullable=False, default=False)
+    verified_phonenumber = db.Column(db.Boolean, nullable=True, default=False)
 
     # either email auth or a mobile number must be provided
     CheckConstraint("auth_type = 'email_auth' or mobile_number is not null")
@@ -224,6 +225,7 @@ class User(BaseModel):
             "blocked": self.blocked,
             "additional_information": self.additional_information,
             "password_expired": self.password_expired,
+            "verified_phonenumber": self.verified_phonenumber,
         }
 
     def serialize_for_users_list(self) -> dict:
@@ -232,6 +234,7 @@ class User(BaseModel):
             "name": self.name,
             "email_address": self.email_address,
             "mobile_number": self.mobile_number,
+            "verified_phonenumber": self.verified_phonenumber,
         }
 
 
