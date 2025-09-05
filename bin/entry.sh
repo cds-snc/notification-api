0ef8dc67e7bd111e1c90a9e42fe23622a8d352b7
Michael Pond <mikepond11@gmail.com>
2025-09-05 09:35:44 -0400

    removing the scripts and docker references to env var handling

--- bin/entry.sh
+++ bin/entry.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
+
+# Minimal Lambda entrypoint: runs RIE locally, awslambdaric in Lambda.
+# No environment dumping (SSM vars load later inside Python).
+
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-    echo "ENTRY.SH: Running locally"
-    exec /usr/bin/aws-lambda-rie $(which python) -m awslambdaric $1
+    exec /usr/bin/aws-lambda-rie $(which python) -m awslambdaric "$1"
 else
-    echo "ENTRY.SH: Running in AWS Lambda"
-    . /sync_lambda_envs.sh
-    echo "All environment variable names:"
-    env | cut -d '=' -f 1
-    exec $(which python) -m awslambdaric $1
+    exec $(which python) -m awslambdaric "$1"
 fi
