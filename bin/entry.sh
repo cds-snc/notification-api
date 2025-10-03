5aed313878995eb3c79b0d6d3d943bd05f870b6a
Michael Pond <mikepond11@gmail.com>
2025-10-02 22:04:38 -0400

    just keeping the default in. case we ever did want to override the newrelic.ini

--- bin/entry.sh
+++ bin/entry.sh
@@ -6,13 +6,13 @@ else
     RUNTIME_CMD="$(which python)"
 fi
 
+## IMPORTANT: NEW RELIC CONFIGURATION -- WE ALWAYS WANT TO USE APM MODE!
 # Force classic New Relic agent mode for Lambda so data flows to APM instead of the serverless extension
 unset NEW_RELIC_LAMBDA_EXTENSION_ENABLED
 unset NEW_RELIC_LAMBDA_HANDLER
 unset NEW_RELIC_EXTENSION_LOGS_ENABLED
 unset NEW_RELIC_EXTENSION_SEND_FUNCTION_LOGS
-
-export NEW_RELIC_SERVERLESS_MODE_ENABLED=${NEW_RELIC_SERVERLESS_MODE_ENABLED:-false}
+export NEW_RELIC_SERVERLESS_MODE_ENABLED=false
 export NEW_RELIC_CONFIG_FILE=${NEW_RELIC_CONFIG_FILE:-/app/newrelic.ini}
 
 exec ${RUNTIME_CMD} -m awslambdaric $1
