c73579231752b5876ca66217a06a996ba9fcfa73
Mike Pond <32133001+P0NDER0SA@users.noreply.github.com>
2025-10-06 08:52:25 -0400

    New Relic Cold Start Initialization/APM Variables and Handler (#2655)
    * attempting to initialize new relic on cold start rather than each invocation.  This is why we have 400+ lambdas in NR
    
    * adding default name
    
    * formatting
    
    * fixing
    
    * removing old files, forcing some env vars to force new relic into APM mode
    
    * just keeping the default in. case we ever did want to override the newrelic.ini
    
    * bringing docker files in line except for the new relic layer to keep them as consistent as possible
    
    * adding a check to make sure the initialization only happens once for new relic
    
    * moving new relic agent import under runtime if statement
    
    * updating new relic version
    
    * updating lock
    
    * ruff formatting

--- bin/entry.sh
+++ bin/entry.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-    exec /usr/bin/aws-lambda-rie $(which python) -m awslambdaric $1
+    RUNTIME_CMD="/usr/bin/aws-lambda-rie $(which python)"
 else
     . /sync_lambda_envs.sh # Retrieve .env from parameter store and remove currently set environement variables
-    exec $(which python) -m awslambdaric $1
+    RUNTIME_CMD="$(which python)"
 fi
+
+## IMPORTANT: NEW RELIC CONFIGURATION -- WE ALWAYS WANT TO USE APM MODE!
+# Force classic New Relic agent mode for Lambda so data flows to APM instead of the serverless extension
+unset NEW_RELIC_LAMBDA_EXTENSION_ENABLED
+unset NEW_RELIC_LAMBDA_HANDLER
+unset NEW_RELIC_EXTENSION_LOGS_ENABLED
+unset NEW_RELIC_EXTENSION_SEND_FUNCTION_LOGS
+export NEW_RELIC_SERVERLESS_MODE_ENABLED=false
+export NEW_RELIC_CONFIG_FILE=${NEW_RELIC_CONFIG_FILE:-/app/newrelic.ini}
+
+exec ${RUNTIME_CMD} -m awslambdaric $1
