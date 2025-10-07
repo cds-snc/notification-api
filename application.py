9f045e0e13f34dc54d68a79705424be19f84355e
Michael Pond <mikepond11@gmail.com>
2025-10-07 12:48:08 -0400

    just some formatting and moving the lambda handler into only lambda territory

--- application.py
+++ application.py
@@ -58,10 +58,11 @@ if os.environ.get("AWS_LAMBDA_RUNTIME_API"):
     newrelic.agent.initialize(environment=app.config["NOTIFY_ENVIRONMENT"])  # noqa: E402
     app.wsgi_app = newrelic.agent.WSGIApplicationWrapper(app.wsgi_app, name="Lambda API")
     newrelic.agent.register_application(timeout=20.0)
-
-apig_wsgi_handler = make_lambda_handler(
-    app, binary_support=True, non_binary_content_type_prefixes=["application/yaml", "application/json"]
-)
+    apig_wsgi_handler = make_lambda_handler(
+        app,
+        binary_support=True,
+        non_binary_content_type_prefixes=["application/yaml", "application/json"],
+    )
 
 if os.environ.get("USE_LOCAL_JINJA_TEMPLATES") == "True":
     print("")
