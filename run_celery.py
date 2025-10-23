679e8352a16a9ee7873765f3c4be088d168c1ad1
Ben Larabie <benlarabie@gmail.com>
2025-10-23 11:44:44 -0400

    New Relic enable/disable (#2677)
    * if statements
    
    * formatting
    
    ---------
    
    Co-authored-by: Ben Larabie <ben.larabie@blarabie-jtfk.home>

--- run_celery.py
+++ run_celery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 
-import newrelic.agent  # See https://bit.ly/2xBVKBH
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from dotenv import load_dotenv
@@ -10,7 +9,14 @@ from flask import Flask
 from app.aws.xray.context import NotifyContext
 
 environment = os.environ.get("NOTIFY_ENVIRONMENT")
-newrelic.agent.initialize(environment=environment)  # noqa: E402
+enable_newrelic = os.getenv("ENABLE_NEW_RELIC", "False").lower() == "true"
+
+print("enable_newrelic =", enable_newrelic)
+
+if enable_newrelic:
+    import newrelic.agent
+
+    newrelic.agent.initialize(environment=environment)  # noqa: E402
 
 # notify_celery is referenced from manifest_delivery_base.yml, and cannot be removed
 from app import create_app, notify_celery  # noqa
