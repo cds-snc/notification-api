c5748c0075dce3a75ac546b6ebccaa8e9d1cbfc5
Ben Larabie <benlarabie@gmail.com>
2025-11-18 15:09:57 -0500

    fixing sync lambda (#2698)
    Co-authored-by: Ben Larabie <ben.larabie@blarabie-jtfk.home>

--- bin/sync_lambda_envs.sh
+++ bin/sync_lambda_envs.sh
@@ -29,7 +29,7 @@ load_non_existing_envs() {
     value=$(echo "$line" | cut -d '=' -f 2-)
 
     if [ -z $(var_expand $key) ]; then # Check if environment variable doesn't exist
-      export "${key}=${value}"
+      eval "export ${key}='${value}'"
     fi
     
   done < $TMP_ENV_FILE
