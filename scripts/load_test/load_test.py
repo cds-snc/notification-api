d21e4b2cbbeebdc30b1039a11b8ff1af53d1e164
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2026-01-21 10:59:53 -0500

    realistic load test

--- scripts/load_test/load_test.py
+++ scripts/load_test/load_test.py
@@ -5,6 +5,7 @@ from io import StringIO
 from typing import Iterator, List
 
 from dotenv import load_dotenv
+
 from locust import HttpUser, constant_pacing, task
 
 load_dotenv()
@@ -19,6 +20,7 @@ def rows_to_csv(rows: List[List[str]]):
 
 def job_lines(data: str, number_of_lines: int) -> Iterator[List[str]]:
     return map(lambda n: [data], range(0, number_of_lines))
+
 
 
 class NotifyApiUser(HttpUser):
@@ -60,14 +62,6 @@ class NotifyApiUser(HttpUser):
         json = {"phone_number": self.phone_number, "template_id": self.high_priority_sms_template}
         self.client.post("/v2/notifications/sms", json=json, headers=self.headers)
 
-    @task(2)  # about every 5 minutes
-    def send_medium_priority_sms(self):
-        self.send_bulk_sms(self.medium_priority_sms_template, 199)
-
-    @task(1)  # about every 10 minutes
-    def send_low_priority_sms(self):
-        self.send_bulk_sms(self.low_priority_sms_template, 1000)
-
     # Email Tasks
 
     @task(120)  # about every 5 seconds
@@ -75,16 +69,50 @@ class NotifyApiUser(HttpUser):
         json = {"email_address": self.email_address, "template_id": self.high_priority_email_template}
         self.client.post("/v2/notifications/email", json=json, headers=self.headers)
 
-    @task(2)  # about every 5 minutes
-    def send_medium_priority_email(self):
-        self.send_bulk_email(self.medium_priority_email_template, 199)
-
-    @task(1)  # about every 10 minutes
-    def send_low_priority_emails(self):
-        self.send_bulk_email(self.low_priority_email_template, 10000)
-
     # Do nothing task
 
     @task(600 - 120 - 2 - 1 - 120 - 2 - 1)
     def do_nothing(self):
         pass
+
+
+class BulkOperationsUser(HttpUser):
+    """Single user instance for periodic bulk operations"""
+    weight = 1
+    wait_time = constant_pacing(300)  # do something every 5 minutes
+
+    def __init__(self, *args, **kwargs):
+        super(BulkOperationsUser, self).__init__(*args, **kwargs)
+
+        self.headers = {
+            "Authorization": f"apikey-v1 {os.getenv('API_KEY')}",
+            "waf-secret": os.getenv('WAF_SECRET')
+        }
+        self.email_address = "success@simulator.amazonses.com"
+        self.phone_number = "16135550123"
+        self.low_priority_email_template = os.getenv("LOW_PRIORITY_EMAIL_TEMPLATE_ID")
+        self.low_priority_sms_template = os.getenv("LOW_PRIORITY_SMS_TEMPLATE_ID")
+
+    def send_bulk_email(self, template: str, count: int):
+        json = {
+            "name": f"bulk emails {datetime.utcnow().isoformat()}",
+            "template_id": template,
+            "csv": rows_to_csv([["email address"], *job_lines(self.email_address, count)]),
+        }
+        self.client.post("/v2/notifications/bulk", json=json, headers=self.headers, timeout=60)
+
+    def send_bulk_sms(self, template: str, count: int):
+        json = {
+            "name": f"bulk sms {datetime.utcnow().isoformat()}",
+            "template_id": template,
+            "csv": rows_to_csv([["phone_number"], *job_lines(self.phone_number, count)]),
+        }
+        self.client.post("/v2/notifications/bulk", json=json, headers=self.headers, timeout=60)
+
+    @task(1)  # runs every 5 minutes (due to wait_time)
+    def send_low_priority_emails(self):
+        self.send_bulk_email(self.low_priority_email_template, 10000)
+
+    @task(1)  # runs every 5 minutes (due to wait_time)
+    def send_low_priority_sms(self):
+        self.send_bulk_sms(self.low_priority_sms_template, 1000)
