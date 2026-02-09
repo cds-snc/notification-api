f1c3bbc600b4b01949e42ab0d95f93338e34444d
William B <7444334+whabanks@users.noreply.github.com>
2026-02-09 12:03:58 -0400

    Use python variable naming convention
    I spent too much time writing JS recently.

--- app/user/rest.py
+++ app/user/rest.py
@@ -136,7 +136,7 @@ def update_user_attribute(user_id):
     else:
         updated_by = None
 
-    isEditorDefaultUpdate = "default_editor_is_rte" in req_json
+    is_editor_default_update = "default_editor_is_rte" in req_json
 
     update_dct = user_update_schema_load_json.load(req_json)
 
@@ -148,7 +148,7 @@ def update_user_attribute(user_id):
     user_alert_dct = update_dct.copy()
     user_alert_dct.pop("blocked", None)
     user_alert_dct.pop("current_session_id", None)
-    if not updated_by and user_alert_dct and not isEditorDefaultUpdate:
+    if not updated_by and user_alert_dct and not is_editor_default_update:
         _update_alert(user_to_update, user_alert_dct)
 
     # Alert that team member edit user
