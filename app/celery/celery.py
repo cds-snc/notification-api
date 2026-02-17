fe88b8198e38542cc946d871a3de5dbf83be751f
Jimmy Royer <jimleroyer@gmail.com>
2026-02-17 17:44:48 -0500

    Removed older code now that tests are done.

--- app/celery/celery.py
+++ app/celery/celery.py
@@ -22,11 +22,6 @@ def make_task(app):
             elapsed_time = time.time() - self.start
             app.logger.info("{task_name} took {time}".format(task_name=self.name, time="{0:.4f}".format(elapsed_time)))
 
-        # def on_failure(self, exc, task_id, args, kwargs, einfo):
-        #     # ensure task will log exceptions to correct handlers
-        #     app.logger.exception("Celery task: {} failed".format(self.name))
-        #     super().on_failure(exc, task_id, args, kwargs, einfo)
-
         def on_failure(self, exc, task_id, args, kwargs, einfo):
             # Classify the error to determine logging level and marker
             category = classify_error(exc)
