724021b18afd363de76a3ec5656e1466b463c580
Jimmy Royer <jimleroyer@gmail.com>
2026-03-06 14:33:42 -0500

    Added two db upgrades to add rcs notification type and twilio as a rcs provider

---
+++ migrations/versions/0507_add_rcs_notification_type.py
@@ -0,0 +1,29 @@
+"""
+Revision ID: 0507_add_rcs_notification_type
+Revises: 0506_update_ft_billing
+Create Date: 2026-03-05 00:00:00
+
+Add a new notification type enumeration value for RCS.
+"""
+from alembic import op
+
+revision = "0507_add_rcs_notification_type"
+down_revision = "0506_update_ft_billing"
+
+
+def upgrade():
+    # prevent from being executed in a transaction block
+    op.execute("COMMIT")
+
+    # Add a new notification type enumeration value for RCS
+    op.execute("ALTER TYPE notification_type ADD VALUE 'rcs'")
+
+
+def downgrade():
+    # Remove the RCS notification type enumeration value
+    sql = f"""DELETE FROM pg_enum
+            WHERE enumlabel = 'rcs'
+            AND enumtypid = (
+              SELECT oid FROM pg_type WHERE typname = 'notification_type'
+            )"""
+    op.execute(sql)
