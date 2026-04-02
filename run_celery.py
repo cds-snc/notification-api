a05d420cfa1f9990e0c344969976dfe3493e3da3
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2026-04-02 13:13:15 -0400

    Format

--- run_celery.py
+++ run_celery.py
@@ -8,14 +8,6 @@ from flask import Flask
 environment = os.environ.get("NOTIFY_ENVIRONMENT")
 env = Env()
 ff_enable_otel = env.bool("FF_ENABLE_OTEL", default=False)
-enable_newrelic = env.bool("ENABLE_NEW_RELIC", default=False) and not ff_enable_otel
-
-print("enable_newrelic =", enable_newrelic)
-
-if enable_newrelic:
-    import newrelic.agent
-
-    newrelic.agent.initialize(environment=environment)  # noqa: E402
 
 # notify_celery is referenced from manifest_delivery_base.yml, and cannot be removed
 from app import create_app, notify_celery  # noqa
