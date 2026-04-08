008cac7a4ce9ea91a3ba84ac8e6a470dea16038c
wbanks <william.banks@cds-snc.ca>
2026-04-08 14:13:01 -0400

    Improve `dao_fetch_sms_cost_for_service_in_range`
    Previously `dao_fetch_sms_cost_for_service_in_range` was only fetching
    data from the `ft_billing` table which does not include data for today.
    Now if the `end_date` parameter includes today, then we query the
    `notifications` table using `sms_total_carrier_fee` and
    `sms_total_message_price` to calculate today's sms costs

--- app/dao/fact_billing_dao.py
+++ app/dao/fact_billing_dao.py
@@ -38,6 +38,14 @@ def dao_fetch_sms_cost_for_service_in_range(service_id, start_date, end_date):
     FactBilling is populated by a nightly task, so it is always ~1 day behind.
     For the current day we fall back to the Notification table and use the
     carrier-reported costs (sms_total_carrier_fee + sms_total_message_price).
+
+    Minor edge case:
+    If this API is called at 00:15 EST and prior to `create_nightly_billing_for_day` completing, then
+    this endpoint could return inaccurate data.
+    
+    Future improvements could include:
+    1. Check if `ft_billing` has been populated for the requested date range and if not, fall back to the `notifications` table
+    2. Always use the `notifications` table for today and yesterday's data, and `ft_billing` for anything older
     """
     today = convert_utc_to_local_timezone(datetime.utcnow()).date()
 
