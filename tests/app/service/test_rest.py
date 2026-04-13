61bd97e3f04bead417d3e81dec62137734fd6c77
wbanks <william.banks@cds-snc.ca>
2026-04-13 11:22:55 -0400

    Fix annual limit reached email not sent bug
    `ANNUAL_LIMIT_SMS_OVER_NEAR_STATUS_FIELDS` had `near_email_limit` in the
    list of values instead of `over_sms_limit`. This caused the
    `over_sms_limit` flag to never be cleared when a service's annual limit
    was increased, resulting in the email never being sent

--- tests/app/service/test_rest.py
+++ tests/app/service/test_rest.py
@@ -1043,11 +1043,11 @@ def test_update_service_annual_limits(
     assert getattr(sample_service, limit_field) == limit_value
     if limit_field == "sms_annual_limit":
         assert redis_delete.call_args_list == [
-            call(f"annual-limit:{sample_service.id}:status", ["near_sms_limit", "near_email_limit"]),
+            call(f"annual-limit:{sample_service.id}:status", ["near_sms_limit", "over_sms_limit"]),
         ]
     if limit_field == "email_annual_limit":
         assert redis_delete.call_args_list == [
-            call(f"annual-limit:{sample_service.id}:status", ["over_email_limit", "near_email_limit"]),
+            call(f"annual-limit:{sample_service.id}:status", ["near_email_limit", "over_email_limit"]),
         ]
 
 
