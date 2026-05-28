b935674e369e1258f21748afe1836969f864632d
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2026-05-28 20:42:32 +0000

    fix

--- app/v3/template/__init__.py
+++ app/v3/template/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
-from app.errors import register_errors
+from app.v2.errors import register_errors
 
 v3_template_blueprint = Blueprint("v3_template", __name__, url_prefix="/v3/template")
 
-register_errors(v3_template_blueprint)
+register_errors(v3_template_blueprint)  # noqa — registers routes on the blueprint
