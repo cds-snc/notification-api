72a693bdf8a74b32484fc2db5daf00eb5f702f81
Jimmy Royer <jimleroyer@gmail.com>
2026-06-02 14:48:04 -0400

    Isolate long-running nightly delete tasks to a dedicated worker (#2875)
    * Creating nightly queue and moving nightly task into it
    
    * Fixed tests around queue testing
    
    * adding periodic script and adjusting the other scripts accordingly so that the period script manages any k8s related periodic-tasks and job-tasks
    
    * Better comment msg
    
    * Reverting periodic removal changes for the run_celery_no_sms_sending script
    
    ---------
    
    Co-authored-by: William B <7444334+whabanks@users.noreply.github.com>
    Co-authored-by: Michael Pond <mikepond11@gmail.com>
    Co-authored-by: Mike Pond <32133001+P0NDER0SA@users.noreply.github.com>

--- scripts/run_celery_core_tasks.sh
+++ scripts/run_celery_core_tasks.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Runs celery with all celery queues except send-throttled-sms-tasks, 
-# send-sms-* and send-email-*.
+# Runs celery with all celery queues except send-throttled-sms-tasks,
+# send-sms-*, send-email-*, periodic-tasks, and job-tasks.
 
 echo "Start celery, concurrency: ${CELERY_CONCURRENCY-4}"
 
-celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency="${CELERY_CONCURRENCY-4}" -Q -priority-database-tasks.fifo,-normal-database-tasks,-bulk-database-tasks,job-tasks,notify-internal-tasks,periodic-tasks,priority-tasks,normal-tasks,bulk-tasks,reporting-tasks,research-mode-tasks,retry-tasks,service-callbacks,service-callbacks-retry,delivery-receipts
+celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency="${CELERY_CONCURRENCY-4}" -Q -priority-database-tasks.fifo,-normal-database-tasks,-bulk-database-tasks,notify-internal-tasks,priority-tasks,normal-tasks,bulk-tasks,reporting-tasks,research-mode-tasks,retry-tasks,service-callbacks,service-callbacks-retry,delivery-receipts
