a43d9e09a4c48b4b1cb23b03d5de75107d7dc2be
wbanks <william.banks@cds-snc.ca>
2026-06-10 14:12:49 -0400

    Add created_by_id when creating new File objects
    Also added small fix to the migration

--- migrations/versions/0515_add_created_by_id_to_files.py
+++ migrations/versions/0515_add_created_by_id_to_files.py
@@ -16,10 +16,10 @@ down_revision = "0514_add_files_table"
 def upgrade():
     op.add_column("files", sa.Column("created_by_id", postgresql.UUID(as_uuid=True), nullable=True))
     op.create_foreign_key("files_created_by_id_fkey", "files", "users", ["created_by_id"], ["id"])
-    op.create_index("ix_files_created_by_id", "files", ["created_by_id"])
+    op.create_index(op.f("ix_files_created_by_id"), "files", ["created_by_id"])
 
 
 def downgrade():
-    op.drop_index("ix_files_created_by_id", table_name="files")
+    op.drop_index(op.f("ix_files_created_by_id"), table_name="files")
     op.drop_constraint("files_created_by_id_fkey", "files", type_="foreignkey")
     op.drop_column("files", "created_by_id")
