58b30d6b07d9a90a1da4d6e3bbf74b9c1adde6dc
Stephen McMurtry <smcmurtry@gmail.com>
2026-07-08 15:11:50 +0000

    add migration and model change

---
+++ migrations/versions/0517_add_archived_to_files.py
@@ -0,0 +1,22 @@
+"""
+Revision ID: 0517_add_archived_to_files
+Revises: 0516_backfill_rte_default
+Create Date: 2026-07-08
+
+Add archived column to files table.
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "0517_add_archived_to_files"
+down_revision = "0516_backfill_rte_default"
+
+
+def upgrade():
+    op.add_column("files", sa.Column("archived", sa.Boolean(), nullable=False, server_default=sa.false()))
+    op.alter_column("files", "archived", server_default=None)
+
+
+def downgrade():
+    op.drop_column("files", "archived")
