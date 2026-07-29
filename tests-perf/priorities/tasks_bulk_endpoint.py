c3b29d4b3a0053cc6ece815df6963af03db5d0b8
Jimmy Royer <jimleroyer@gmail.com>
2026-07-29 10:35:29 -0400

    Added SMS support to tests-perf/priorities bulk test (#2980)
    * Updating priorities test to latest api key required format
    
    * Added support for waf-secret http header
    
    * Modified the bulk test of the priorities folder to include all priority lanes and notification types
    
    * Set the priority email job size to 10, same as SMS
    
    * Refactored the logic for a data driven config loop approach
    
    * Updated README instructions of priorities/bulk test aligned with latest changes

--- tests-perf/priorities/tasks_bulk_endpoint.py
+++ tests-perf/priorities/tasks_bulk_endpoint.py
@@ -8,21 +8,68 @@ from utils import api_headers, job_line, rows_to_csv
 
 """
 Usage:
-runs the bulk upload twice
+Run the following command to execute the load test for the bulk endpoint:
 
 locust -f ./locust_bulk_endpoint.py --headless --stop-timeout=30 --run-time=15s --host=https://api-k8s.staging.notification.cdssandbox.xyz --users=1 --html=locust.html
+
+This will send bulk requests for all priorities of both email and SMS, with a total of 6 requests per run (1 for each priority and type).
 """
 
 
 class ApiUser(HttpUser):
-    wait_time = constant_pacing(10)  # run once every 10 second
+    wait_time = constant_pacing(60)  # run all tasks once every 60 seconds
     host = Config.API_HOST_NAME
 
+    BULK_SEND_CONFIG = [
+        {
+            "template": Config.BULK_EMAIL_TEMPLATE,
+            "row_name": "email address",
+            "recipient": Config.EMAIL_TO,
+            "job_size": Config.JOB_SIZE,
+        },
+        {
+            "template": Config.NORMAL_EMAIL_TEMPLATE,
+            "row_name": "email address",
+            "recipient": Config.EMAIL_TO,
+            "job_size": Config.JOB_SIZE,
+        },
+        {"template": Config.PRIORITY_EMAIL_TEMPLATE, "row_name": "email address", "recipient": Config.EMAIL_TO, "job_size": 10},
+        {
+            "template": Config.BULK_SMS_TEMPLATE,
+            "row_name": "phone number",
+            "recipient": Config.SMS_TO,
+            "job_size": Config.JOB_SIZE,
+        },
+        {
+            "template": Config.NORMAL_SMS_TEMPLATE,
+            "row_name": "phone number",
+            "recipient": Config.SMS_TO,
+            "job_size": Config.JOB_SIZE,
+        },
+        {"template": Config.PRIORITY_SMS_TEMPLATE, "row_name": "phone number", "recipient": Config.SMS_TO, "job_size": 10},
+    ]
+
+    URL = "/v2/notifications/bulk"
+
     @task
-    def send_bulk(self):
-        json = {
-            "name": f"{datetime.utcnow().isoformat()} {self.environment.parsed_options.ref}",
-            "template_id": Config.BULK_EMAIL_TEMPLATE,
-            "csv": rows_to_csv([["email address"], *job_line(Config.EMAIL_TO, Config.JOB_SIZE)]),
-        }
-        self.client.post("/v2/notifications/bulk", json=json, headers=api_headers(Config.API_KEY))
+    def send_bulk_email_sms_all_priorities(self):
+        ref = self.environment.parsed_options.ref
+
+        for config in self.BULK_SEND_CONFIG:
+            self.client.post(
+                self.URL,
+                json={
+                    "name": f"{datetime.utcnow().isoformat()} {ref}",
+                    "template_id": config["template"],
+                    "csv": rows_to_csv(
+                        [
+                            [config["row_name"]],
+                            *job_line(
+                                config["recipient"],
+                                config["job_size"],
+                            ),
+                        ]
+                    ),
+                },
+                headers=api_headers(Config.API_KEY, Config.WAF_SECRET),
+            )
