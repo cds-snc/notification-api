0da9c63ba7dbab376ecdfa92bb60d44cb89292af
Steve McMurtry <smcmurtry@gmail.com>
2026-07-29 13:58:56 -0400

    [EXPERIMENT] Generate the en and fr open api spec files from a single file with translations (#2971)
    This PR is an experiment to see if we can generate the en and fr open api spec files using a single yaml file: v2-notifications-api.yaml.j2. This is actually a jinja file that contains expressions like {{ t("english string") }} that then get replaced with translations from fr.csv, similar to how the translation process works in notification-admin.
    
    I also added a check to the make test scripts that will run make generate-openapi and verify there is no change in the file content to v2-notifications-api-en.yaml and v2-notifications-api-fr.yaml. That is to ensure that these files are up to date.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -20,6 +20,15 @@ function display_result {
   fi
 }
 
+make generate-openapi
+OPENAPI_FILES=("openapi/v2-notifications-api-en.yaml" "openapi/v2-notifications-api-fr.yaml")
+if ! git diff --exit-code -- "${OPENAPI_FILES[@]}" > /dev/null 2>&1; then
+  echo -e "\033[31mOpenAPI files are out of date. Run 'make generate-openapi' and commit the changes.\033[0m"
+  git diff -- "${OPENAPI_FILES[@]}"
+  exit 1
+fi
+display_result 0 1 "OpenAPI files up to date check"
+
 make test-requirements
 display_result $? 1 "Requirements check"
 
