a1183f6e441eb5c6bc283680a41264450c4df0e8
wbanks <william.banks@cds-snc.ca>
2026-09-09 15:30:11 -0400

    Add logging for cache failures
    Only register session events when FF is turned on

--- app/__init__.py
+++ app/__init__.py
@@ -156,9 +156,10 @@ def create_app(application, config=None):
     redis_store.init_app(application)
     bounce_rate_client.init_app(application)
     init_dogpile_cache(application)
-    from app.cache.cache_events import register_cache_orm_events
+    if application.config.get("FF_USE_DOGPILE_CACHING", False) is True:
+        from app.cache.cache_events import register_cache_orm_events
 
-    register_cache_orm_events()
+        register_cache_orm_events()
 
     sms_bulk_publish.init_app(flask_cache_ops, metrics_logger)
     sms_normal_publish.init_app(flask_cache_ops, metrics_logger)
